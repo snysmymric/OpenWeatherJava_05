c370d0e5939f9ee7bb1ff100f626ebe17a6cf8d4
Tatiana-SH-V <115119026+Tatiana-SH-V@users.noreply.github.com>
2022-11-08 22:48:28 -0500

    testGuideTitle() added (#185)
    Co-authored-by: Irina Z Java <tester@test.com>

--- src/test/java/Tatiana_SH_V_Test.java
+++ src/test/java/Tatiana_SH_V_Test.java
@@ -41,4 +41,26 @@ public class Tatiana_SH_V_Test extends BaseTest {
         Assert.assertEquals(actualResult, expectedResult);
 
     }
+
+    @Test
+    public void testGuideTitle() throws InterruptedException {
+        String url = "https://openweathermap.org/";
+        String expectedResult1 = "https://openweathermap.org/guide";
+        String expectedResult2 = "OpenWeatherMap API guide - OpenWeatherMap";
+
+        getDriver().get(url);
+        Thread.sleep(7000);
+
+        WebElement guideSearch = getDriver().findElement(
+                By.xpath("//div/ul/li/a[@href = '/guide']")
+        );
+        guideSearch.click();
+        Thread.sleep(2000);
+
+        String actualResult1 = getDriver().getCurrentUrl();
+        Assert.assertEquals(actualResult1, expectedResult1);
+
+        String actualResult2 = getDriver().getTitle();
+        Assert.assertEquals(actualResult2, expectedResult2);
+    }
 }
