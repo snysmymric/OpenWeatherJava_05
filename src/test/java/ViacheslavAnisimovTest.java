62bc48bf0ae4177e0648fcedac4019d3cddd9475
Viacheslav Anisimov <forvsn@gmail.com>
2022-11-09 11:16:53 +0700

    testHamburgerMenuAndGuidePageTitle added (#191)

--- src/test/java/ViacheslavAnisimovTest.java
+++ src/test/java/ViacheslavAnisimovTest.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -44,5 +45,34 @@ public class ViacheslavAnisimovTest extends BaseTest {
         String actualResult = h2CityCountryHeader.getText();
 
         Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testHamburgerMenuAndGuidePageTitle() throws InterruptedException {
+
+        String url = "https://openweathermap.org/";
+        String expectedResult1 = "https://openweathermap.org/guide";
+        String expectedResult2 = "OpenWeatherMap API guide - OpenWeatherMap";
+
+        getDriver().manage().window().setSize(new Dimension(1024, 970));
+        getDriver().get(url);
+        Thread.sleep(7000);
+
+        WebElement HamburgerMenu = getDriver().findElement(
+                By.id("hamburger")
+        );
+
+        HamburgerMenu.click();
+
+        WebElement linkToGuidePage = getDriver().findElement(
+                By.xpath("//ul[@id='mobile-menu']//a[@href='/guide']")
+        );
+        linkToGuidePage.click();
+
+        String actualResult1 = getDriver().getCurrentUrl();
+        String actualResult2 = getDriver().getTitle();
+
+        Assert.assertEquals(actualResult1, expectedResult1);
+        Assert.assertEquals(actualResult2, expectedResult2);
     }
 }
