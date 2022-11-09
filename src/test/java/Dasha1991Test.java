7ca8ccdd2422bc1dc1b2335759c851f4896c00a9
Dasha1991 <72041132+Dasha1991@users.noreply.github.com>
2022-11-08 21:57:00 -0600

    Dasha1991 (#163)
    * testMenuSupport() added
    
    * delete 62 row
    
    Co-authored-by: Dashechka-blond <d.shevchenka@gmail.com>

--- src/test/java/Dasha1991Test.java
+++ src/test/java/Dasha1991Test.java
@@ -10,7 +10,6 @@ public class Dasha1991Test extends BaseTest {
     @Test
     public void testButtonGuide_OpenWeather() throws InterruptedException {
 
-
         String url = "https://openweathermap.org/";
         String expectedResult1 = "https://openweathermap.org/guide";
         String expectedResult2 = "OpenWeatherMap API guide - OpenWeatherMap";
@@ -50,5 +49,37 @@ public class Dasha1991Test extends BaseTest {
         boolean actualResult = imageFTemperature.getText().contains("°F");
         Assert.assertTrue(actualResult);
 
+    }
+    @Test
+    public void testMenuSupport() throws InterruptedException {
+
+        String url = "https://openweathermap.org/";
+        String expectedResult1 = "FAQ";
+        String expectedResult2 = "How to start";
+        String expectedResult3 = "Ask a question";
+
+        getDriver().get(url);
+        Thread.sleep(7000);
+
+        WebElement findSupportButton = getDriver().findElement(By.xpath("//div[@id='support-dropdown']"));
+
+        findSupportButton.click();
+        Thread.sleep(2000);
+
+        WebElement buttonFAQ = getDriver().findElement(By.xpath
+                ("//ul[@class = 'dropdown-menu dropdown-visible']//li/a[@href = '/faq']"));
+        WebElement buttonHowToStart = getDriver().findElement(By.xpath
+                ("//ul[@class = 'dropdown-menu dropdown-visible']/li[2]//a"));
+        WebElement buttonAskAQuestion = getDriver().findElement(By.xpath
+                ("//ul[@class = 'dropdown-menu dropdown-visible']//li[3]/a"));
+        Thread.sleep(2000);
+        String actualResult1 = buttonFAQ.getText();
+        Assert.assertEquals(actualResult1, expectedResult1);
+
+        String actualResult2 = buttonHowToStart.getText();
+        Assert.assertEquals(actualResult2, expectedResult2);
+
+        String actualResult3 = buttonAskAQuestion.getText();
+        Assert.assertEquals(actualResult3, expectedResult3);
     }
 }
