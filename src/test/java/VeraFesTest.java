8656a8bc9960953ab9c24ce97633680ee062ab17
Vera Fesianava <vera@Veras-MacBook-Air.local>
2022-11-08 18:52:37 -0800

    testConfirmLinkAndTitleOnGuidePage_OpenWeatherMap() added

--- src/test/java/VeraFesTest.java
+++ src/test/java/VeraFesTest.java
@@ -41,4 +41,26 @@ public class VeraFesTest extends BaseTest {
         Assert.assertEquals(actualResult,expectedResult);
 
     }
+
+    @Test
+    public void testConfirmLinkAndTitleOnGuidePage_OpenWeatherMap() throws InterruptedException {
+        String url = "https://openweathermap.org/";
+        String guideMenuText = "Guide";
+        String expectedResult1 = "https://openweathermap.org/guide";
+
+        String expectedResult2 = "OpenWeatherMap API guide - OpenWeatherMap";
+
+        getDriver().get(url);
+        Thread.sleep(5000);
+
+        WebElement guideMenu = getDriver().findElement(By.linkText(guideMenuText));
+        guideMenu.click();
+
+        String actualResult1 = getDriver().getCurrentUrl();
+        String actualResult2 = getDriver().getTitle();
+
+        Assert.assertEquals(actualResult1, expectedResult1);
+        Assert.assertEquals(actualResult2, expectedResult2);
+    }
+
 }
