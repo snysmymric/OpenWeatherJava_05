690de935d4a76de74595a0513a31374368292751
Yermek <yermek.sultangaleyev@gmail.com>
2022-11-09 17:51:30 -0500

    CookiesTest_Yermek

--- src/test/java/YermeksTest.java
+++ src/test/java/YermeksTest.java
@@ -46,5 +46,44 @@ public class YermeksTest extends BaseTest {
         Assert.assertEquals(actualResult, expectedResult);
     }
 
+    @Test
+    public void testPanelWithTextAndTwoButtons_WhenOpenTheBaseURL() throws InterruptedException {
+
+
+        String url = "https://openweathermap.org/";
+        String expectedResultPanelWithText = "We use cookies which are essential for the site to work. " +
+                "We also use non-essential cookies to help us improve our services. " +
+                "Any data collected is anonymised. You can allow all cookies or manage them individually.";
+        String expectedResultButtonAllowAll = "Allow all";
+        String expectedResultButtonManageCookies = "Manage cookies";
+
+        getDriver().get(url);
+        ;
+        Thread.sleep(8000);
+
+        Assert.assertTrue(getDriver().findElement(By.className(
+                "stick-footer-panel__container")).isDisplayed());
+
+        WebElement panelWithText = getDriver().findElement(By.className(
+                "stick-footer-panel__description"));
+
+        WebElement buttonAllowAll = getDriver().findElement(By.xpath(
+                "//div//button[text()='Allow all']"));
+
+        WebElement buttonManageCookies = getDriver().findElement
+                (By.xpath("//div[@class='stick-footer-panel__btn-container'"
+                        + "]/*[text()=' Manage cookies ']"));
+
+        String actualResultPanelWithText = panelWithText.getText();
+        String actualResultButtonAllowAll = buttonAllowAll.getText();
+        String actualResultButtonManageCookies = buttonManageCookies.getText();
+
+        Assert.assertEquals(actualResultPanelWithText, expectedResultPanelWithText);
+        Assert.assertEquals(actualResultButtonAllowAll, expectedResultButtonAllowAll);
+        Assert.assertEquals(actualResultButtonManageCookies, expectedResultButtonManageCookies);
+        Assert.assertTrue(buttonAllowAll.isDisplayed());
+        Assert.assertTrue(buttonManageCookies.isDisplayed());
+
+    }
 
 }
