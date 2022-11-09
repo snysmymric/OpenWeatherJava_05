aa9c2bc19009f98b74779fc26a42277beb8d24f2
Yermek <yermek.sultangaleyev@gmail.com>
2022-11-09 00:54:36 -0500

    Yermek_Sultangaleyev_New_Class_Commit

--- src/test/java/YermeksTest.java
+++ src/test/java/YermeksTest.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class YermeksTest {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+import java.util.ArrayList;
+
+public class YermeksTest extends BaseTest {
+
+    @Test
+    public void testH2TagText_WhenSearchingCityCountry() throws InterruptedException {
+
+        String url = "https://openweathermap.org/";
+        String cityName = "Paris";
+        String expectedResult = "Paris, FR";
+
+        getDriver().get(url);
+        Thread.sleep(7000);
+
+        WebElement searchCityField = getDriver().findElement(
+                By.xpath("//div[@id = 'weather-widget']//input[@placeholder = 'Search city']")
+        );
+        searchCityField.click();
+        searchCityField.sendKeys(cityName);
+
+        WebElement searchButton = getDriver().findElement(
+                By.xpath("//div[@id = 'weather-widget']//button[@type = 'submit']")
+        );
+        searchButton.click();
+        Thread.sleep(7000);
+
+        WebElement parisFRChoiceInDropdownMenu = getDriver().findElement(
+                By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Paris, FR ']")
+        );
+        parisFRChoiceInDropdownMenu.click();
+
+        WebElement h2CityCountryHeader = getDriver().findElement(
+                By.xpath("//div[@id = 'weather-widget']//h2")
+        );
+        Thread.sleep(7000);
+        String actualResult = h2CityCountryHeader.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+
 }
