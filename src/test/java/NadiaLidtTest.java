367813da00c5ca7597495d24a12402bc99b51477
Nadia Ludanik <nadialidt@gmail.com>
2022-11-10 09:55:20 +0300

    testH2TagText added (#267)

---
+++ src/test/java/NadiaLidtTest.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class NadiaLidtTest extends BaseTest {
+
+    @Test
+    public void testH2TagTextWhenSearchingCityCountry() throws InterruptedException {
+        String url = "https://openweathermap.org/";
+        String cityName = "Paris";
+        String expectedResult = "Paris, FR";
+
+        getDriver().get(url);
+        Thread.sleep(8000);
+
+        WebElement searchCityField = getDriver().findElement(
+                By.xpath("//div[@id='weather-widget']//input[@placeholder='Search city']"));
+        Thread.sleep(1000);
+        searchCityField.sendKeys(cityName);
+
+        WebElement searchButton = getDriver().findElement(
+                By.xpath("//div[@id='weather-widget']//button[@type='submit']"));
+        searchButton.click();
+        Thread.sleep(1000);
+
+        WebElement parisFrChoiInDropDownMenu = getDriver().findElement(
+                By.xpath("//ul[@class='search-dropdown-menu']/li/span[text()='Paris, FR ']"));
+        parisFrChoiInDropDownMenu.click();
+
+        WebElement h2CityNameHeader = getDriver().findElement(By.xpath("//div[@id='weather-widget']//h2"));
+        Thread.sleep(2000);
+
+        String actualResultat = h2CityNameHeader.getText();
+
+        Assert.assertEquals(actualResultat, expectedResult);
+    }
+}
