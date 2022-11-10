c9c3ae0ef76011f2f0a46a4bc65c6d1fcf55cfac
verafes <verafes@gmail.com>
2022-11-09 22:46:04 -0800

    FahrenheitUnits_IfChooseF test added

--- src/test/java/VeraFesTest.java
+++ src/test/java/VeraFesTest.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
@@ -14,19 +16,19 @@ public class VeraFesTest extends BaseTest {
         String expectedResult = "Paris, FR";
 
         getDriver().get(url);
-        Thread.sleep(9000);
+        Thread.sleep(10000);
 
         WebElement searchCityField = getDriver().findElement(
                 By.xpath("//div[@id='weather-widget']//input[@placeholder='Search city']")
         );
         searchCityField.click();
         searchCityField.sendKeys(cityName);
-        Thread.sleep(7000);
+        Thread.sleep(10000);
 
         WebElement searchButton = getDriver().findElement(By.xpath("//button[@type='submit']"));
         searchButton.click();
 
-        Thread.sleep(7000);
+        Thread.sleep(10000);
 
         WebElement parisFRChoiceInDropdownMenu = getDriver().findElement(
                 By.xpath("//ul[@class='search-dropdown-menu']/li/span[text()='Paris, FR ']"));
@@ -34,11 +36,38 @@ public class VeraFesTest extends BaseTest {
 
         WebElement h2CityCountryHeader  = getDriver().findElement(By.xpath("//div[@id='weather-widget']//h2"));
 
-        Thread.sleep(7000);
+        Thread.sleep(10000);
 
         String actualResult = h2CityCountryHeader.getText();
 
         Assert.assertEquals(actualResult,expectedResult);
     }
 
+    @Test
+    public void test_FahrenheitUnits_IfChooseF_11_02() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+
+        String url = "https://openweathermap.org/";
+        String menuOption = "Imperial: °F, mph";
+        boolean expectedResult = true;
+        String fTempSymbol = "F";
+
+        getDriver().get(url);
+        Thread.sleep(10000);
+
+        WebElement findTempFSwitcher = driver.findElement(By.xpath(
+                "//div[@class='switch-container']//div[contains(text(), '" +menuOption+ "')]"));
+        findTempFSwitcher.click();
+
+        WebElement findTempIndicator = driver.findElement(By.xpath(
+                "//div[@class='current-temp']/span"));
+
+        String tempIndidcatorF = findTempIndicator.getText();
+        String actualResult = tempIndidcatorF.substring(tempIndidcatorF.length() - 2);
+
+        Assert.assertTrue(findTempIndicator.getText().contains(fTempSymbol));
+        Assert.assertTrue(expectedResult, actualResult);
+
+        driver.quit();
+    }
 }
