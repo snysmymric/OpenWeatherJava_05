dccfe356290f26fc599e9840d774d290047baa4e
Ivan Tretyakov <ivtretyakov56@gmail.com>
2022-11-11 10:45:12 +0500

    testTemperatureForCity_HappyPath_Test() added

--- src/test/java/CordedWareTest.java
+++ src/test/java/CordedWareTest.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
@@ -31,6 +33,39 @@ public class CordedWareTest extends BaseTest {
         // Assert
         Assert.assertEquals(actualResultUrl, expectedResultUrl);
         Assert.assertEquals(actualResultTitle, expectedResultTitle);
+
+        getDriver().quit();
+    }
+
+    @Test
+    public void testTemperatureForCity_HappyPath_Test() throws InterruptedException {
+
+        // AAA
+        // Arrange
+        String url = "https://openweathermap.org/";
+        String fSymbol = "°F";
+        String expectedResult = "°F";
+
+        // Act
+        getDriver().get(url);
+        Thread.sleep(10000);
+
+        WebElement searchButtonWithTemperature = getDriver().findElement(
+                By.xpath("//div[3][@class = 'option']")
+        );
+        searchButtonWithTemperature.click();
+        Thread.sleep(10000);
+
+        WebElement checkTemperatureHeader = getDriver().findElement(
+                By.xpath("//div[@class = 'current-container mobile-padding']/div[2]/div/span[@class = 'heading']")
+        );
+        checkTemperatureHeader.click();
+        Thread.sleep(10000);
+
+        String actualResult = checkTemperatureHeader.getText().substring(checkTemperatureHeader.getText().length()-2);
+
+        // Assert
+        Assert.assertEquals(actualResult, expectedResult);
 
         getDriver().quit();
     }
