547390bf54472a0216576f607c0d5f5ab1467fb7
Ivan Tretyakov <ivtretyakov56@gmail.com>
2022-11-11 09:04:19 +0500

    testTittleAndUrlWhenGuideMenu_HappyPath_Test() fixed comment & added

--- src/test/java/CordedWareTest.java
+++ src/test/java/CordedWareTest.java
@@ -1,7 +1,5 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
@@ -18,17 +16,17 @@ public class CordedWareTest extends BaseTest {
         String expectedResultTitle = "OpenWeatherMap API guide - OpenWeatherMap";
 
         // Act
-        getDriver().get(url); // 1. открыть ссылку по url
+        getDriver().get(url);
         Thread.sleep(10000);
 
-        WebElement selectButtonMenu = getDriver().findElement( // 2.  Нажать на пункт меню Guide
+        WebElement selectButtonMenu = getDriver().findElement(
                 By.xpath("//a[@href = '/guide']")
         );
         selectButtonMenu.click();
         Thread.sleep(10000);
 
-        String actualResultUrl = getDriver().getCurrentUrl(); // 3.1 Подтвердить, что перешли по ссылке
-        String actualResultTitle = getDriver().getTitle(); // 3.2 Подтвердить, что заголовок изменился
+        String actualResultUrl = getDriver().getCurrentUrl();
+        String actualResultTitle = getDriver().getTitle();
 
         // Assert
         Assert.assertEquals(actualResultUrl, expectedResultUrl);
