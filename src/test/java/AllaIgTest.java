7110525f41be3f4c8e825d6bf3f95f8f81ba74b0
AllaIg <115178653+AllaIg@users.noreply.github.com>
2022-11-11 09:45:59 -0600

    testUrlAndTitle_WhenGuidePress() added (#362)

--- src/test/java/AllaIgTest.java
+++ src/test/java/AllaIgTest.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
@@ -40,6 +42,32 @@ public class AllaIgTest extends BaseTest {
         );
         Thread.sleep(5000);
         String actualResult = h2CityCountryHeader.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+    @Test
+    public void testUrlAndTitle_WhenGuidePress() throws InterruptedException {
+
+        String url = "https://openweathermap.org/";
+
+        String expectedResult = "OpenWeatherMap API guide - OpenWeatherMap";
+
+        getDriver().get(url);
+
+        Thread.sleep(10000);
+
+        WebElement guideButton = getDriver().findElement(
+                By.xpath("//div[@id = 'desktop-menu']/ul/li/a[@href = '/guide']")
+        );
+        guideButton.click();
+
+        String actualResult = getDriver().getTitle();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+        String expectedCurrentUrl = "https://openweathermap.org/guide";
+
+        String actualCurrentUrl = getDriver().getCurrentUrl();
 
         Assert.assertEquals(actualResult, expectedResult);
     }
