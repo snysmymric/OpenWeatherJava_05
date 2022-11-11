c894fd67d7b625f3f9d79c613b20ac2a17430bea
elviraKh1 <115122344+elviraKh1@users.noreply.github.com>
2022-11-11 12:44:19 -0500

    upd tc_ 11_04 with JavascriptExecutor click (#294)

--- src/test/java/ElviraKh1Test.java
+++ src/test/java/ElviraKh1Test.java
@@ -1,9 +1,11 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
+import java.time.Duration;
 import java.util.List;
 
 public class ElviraKh1Test extends BaseTest {
@@ -15,12 +17,18 @@ public class ElviraKh1Test extends BaseTest {
         final String expectedSupportMenu = "Support";
         final String[] expectedSupportSubMenus = {"FAQ", "How to start", "Ask a question"};
 
+//        getDriver().get(BASE_URL);
+//        Thread.sleep(10000);
+//
+//        WebElement supportMenu = getDriver().findElement(By.xpath("//div[@id='support-dropdown']"));
+//        supportMenu.click();
+//        Thread.sleep(7000);
+        getDriver().manage().timeouts().implicitlyWait( Duration.ofSeconds(30));
         getDriver().get(BASE_URL);
-        Thread.sleep(10000);
-
         WebElement supportMenu = getDriver().findElement(By.xpath("//div[@id='support-dropdown']"));
-        supportMenu.click();
-        Thread.sleep(7000);
+        JavascriptExecutor js = (JavascriptExecutor)getDriver();
+        js.executeScript("arguments[0].click()", supportMenu);
+
         List<WebElement> subSupportMenus = getDriver().findElements(By.xpath("//ul[@id='support-dropdown-menu']//li"));
 
         Assert.assertEquals(supportMenu.getText(), expectedSupportMenu);
