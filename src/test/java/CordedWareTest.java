07e2268c1eb7789e026a9afe4fa3ea8c26f45fe3
Ivan Tretyakov <ivtretyakov56@gmail.com>
2022-11-12 17:17:44 +0500

    refactoring testTittleAndUrlWhenGuideMenu_HappyPath_Test() & testTemperatureForCity_HappyPath_Test()

--- src/test/java/CordedWareTest.java
+++ src/test/java/CordedWareTest.java
@@ -1,72 +1,48 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class CordedWareTest extends BaseTest {
 
+    private final String WEATHER_PAGE_URL = "https://openweathermap.org/";
     @Test
     public void testTittleAndUrlWhenGuideMenu_HappyPath_Test() throws InterruptedException{
 
-        // AAA
-        // Arrange
-        String url = "https://openweathermap.org/";
         String expectedResultUrl = "https://openweathermap.org/guide";
         String expectedResultTitle = "OpenWeatherMap API guide - OpenWeatherMap";
 
-        // Act
-        getDriver().get(url);
+        getDriver().get(WEATHER_PAGE_URL);
         Thread.sleep(10000);
 
-        WebElement selectButtonMenu = getDriver().findElement(
-                By.xpath("//a[@href = '/guide']")
-        );
-        selectButtonMenu.click();
-        Thread.sleep(10000);
+        getDriver().findElement(By.xpath("//a[@href = '/guide']")).click();
 
         String actualResultUrl = getDriver().getCurrentUrl();
         String actualResultTitle = getDriver().getTitle();
 
-        // Assert
         Assert.assertEquals(actualResultUrl, expectedResultUrl);
         Assert.assertEquals(actualResultTitle, expectedResultTitle);
-
-        getDriver().quit();
     }
 
     @Test
     public void testTemperatureForCity_HappyPath_Test() throws InterruptedException {
 
-        // AAA
-        // Arrange
-        String url = "https://openweathermap.org/";
         String fSymbol = "°F";
         String expectedResult = "°F";
 
-        // Act
-        getDriver().get(url);
+        getDriver().get(WEATHER_PAGE_URL);
         Thread.sleep(10000);
 
-        WebElement searchButtonWithTemperature = getDriver().findElement(
-                By.xpath("//div[3][@class = 'option']")
-        );
-        searchButtonWithTemperature.click();
+        getDriver().findElement(By.xpath("//div[3][@class = 'option']")).click();
         Thread.sleep(10000);
 
-        WebElement checkTemperatureHeader = getDriver().findElement(
-                By.xpath("//div[@class = 'current-container mobile-padding']/div[2]/div/span[@class = 'heading']")
-        );
-        checkTemperatureHeader.click();
-        Thread.sleep(10000);
+        WebElement checkTemperatureHeader = getDriver().findElement(By.xpath("//div[@class = " +
+                "'current-container mobile-padding']/div[2]/div/span[@class = 'heading']"));
+        checkTemperatureHeader.getText();
 
         String actualResult = checkTemperatureHeader.getText().substring(checkTemperatureHeader.getText().length()-2);
 
-        // Assert
         Assert.assertEquals(actualResult, expectedResult);
-
-        getDriver().quit();
     }
 }
