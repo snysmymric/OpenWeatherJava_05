6cb30454a5d4dc087ed03abcaef9140ee3ae8c16
LolitaMn <87144131+LolitaMn@users.noreply.github.com>
2022-11-13 00:59:48 +0400

    testCheckTextInTheBottomOfThePage() added (#448)
    * testCheckTextInTheBottomOfThePage()
    
    * removed priority and space

--- src/test/java/LolitaMnTest.java
+++ src/test/java/LolitaMnTest.java
@@ -10,7 +10,7 @@ import runner.BaseTest;
 public class LolitaMnTest extends BaseTest {
 
 
-    @Test(priority = 0)
+    @Test
     public void testH2TagText_WhenSearchingCtyCountry() throws InterruptedException {
 
         String url = "https://openweathermap.org/";
@@ -52,7 +52,7 @@ public class LolitaMnTest extends BaseTest {
 
     }
 
-    @Test(priority = 1)
+    @Test
     public void testCheckPageTitle_WhenClickOnGuide() throws InterruptedException {
 
         String url = "https://openweathermap.org/";
@@ -79,7 +79,7 @@ public class LolitaMnTest extends BaseTest {
 
     }
 
-    @Test(priority = 2)
+    @Test
     public void testCheck_F_TemperatureMeasurement() throws InterruptedException {
         String url = "https://openweathermap.org/";
         String fSymbol = "°F";
@@ -106,4 +106,39 @@ public class LolitaMnTest extends BaseTest {
 
     }
 
+    @Test
+    public void testCheckTextInTheBottomOfThePage() throws InterruptedException {
+        String url = "https://openweathermap.org/";
+        String expectedResult = "We use cookies which are essential for the site to work. We also use non-essential "
+                + "cookies to help us improve our services. Any data collected is anonymised. You can allow all cookies"
+                + " or manage them individually.";
+        String allowAll = "Allow all";
+        String manageCookies = " Manage cookies ";
+
+        getDriver().get(url);
+        Thread.sleep(10000);
+
+        WebElement bottomPanel = getDriver().findElement(
+                By.xpath("//div[@class='stick-footer-panel__container']")
+        );
+
+        WebElement textPanel = getDriver().findElement(
+                By.xpath("//p[@class='stick-footer-panel__description']")
+        );
+
+        String actualResultTextPanel = textPanel.getText();
+
+        WebElement allowAllButton = getDriver().findElement(
+                By.xpath("//button[@type='button' and text()='" + allowAll + "']")
+        );
+
+        WebElement manageButton = getDriver().findElement(
+                By.xpath("//a[@href='/cookies-settings' and text()='" + manageCookies + "']")
+        );
+        Assert.assertTrue(bottomPanel.isDisplayed());
+        Assert.assertTrue(allowAllButton.isDisplayed());
+        Assert.assertTrue(manageButton.isDisplayed());
+        Assert.assertEquals(actualResultTextPanel, expectedResult);
+
+    }
 }
