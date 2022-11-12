49f71765f5dd70e81f0cee85cc9718bc7c6e7159
Viktoriya D <115054456+ViktoriyaED@users.noreply.github.com>
2022-11-12 09:47:18 -0500

    Codes refactoring (#437)

--- src/test/java/ViktoriyaEDTest.java
+++ src/test/java/ViktoriyaEDTest.java
@@ -1,7 +1,5 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -15,52 +13,42 @@ import java.util.List;
 
 public class ViktoriyaEDTest extends BaseTest {
 
+    private final String WEATHER_URL = "https://openweathermap.org/";
+
     @Test
     public void testH2TagText_WhenSearchingCityCountry() throws InterruptedException {
 
-        String url = "https://openweathermap.org/";
         String cityName = "Paris";
         String expectedResult = "Paris, FR";
 
-        getDriver().get(url);
+        getDriver().get(WEATHER_URL);
         Thread.sleep(10000);
 
-        WebElement searchCityField = getDriver().findElement(
-                By.xpath("//div[@id = 'weather-widget']//input[@placeholder = 'Search city']")
-        );
-        searchCityField.click();
-        searchCityField.sendKeys(cityName);
+        getDriver().findElement(
+                By.xpath("//div[@id = 'weather-widget']//input[@placeholder = 'Search city']")).sendKeys(cityName);
 
-        WebElement searchButton = getDriver().findElement(
-                By.xpath("//div[@id='weather-widget']//button[@type='submit']\n")
-        );
-        searchButton.click();
+        getDriver().findElement(By.xpath("//div[@id='weather-widget']//button[@type='submit']\n")).click();
 
         Thread.sleep(2000);
 
-        WebElement parisFrChoiceInDropDownMenu = getDriver().findElement(
-                By.xpath("//ul[@class=\"search-dropdown-menu\"]//li//span[text() = 'Paris, FR ']")
-        );
-        parisFrChoiceInDropDownMenu.click();
+        getDriver().findElement(
+                By.xpath("//ul[@class=\"search-dropdown-menu\"]//li//span[text() = 'Paris, FR ']")).click();
 
         WebElement h2CityNameHeader = getDriver().findElement(By.xpath("//div[@id = 'weather-widget']//h2"));
 
         Thread.sleep(2000);
 
-        String actualResult = h2CityNameHeader.getText();
-
-        Assert.assertEquals(actualResult, expectedResult);
+        Assert.assertEquals(h2CityNameHeader.getText(), expectedResult);
     }
 
     @Test
     public void test_TitleText_WhenChooseMenuGuide() throws InterruptedException {
 
-        String url = "https://openweathermap.org/";
         String expectedResult = "https://openweathermap.org/guide";
         String expectedResult1 = "OpenWeatherMap API guide - OpenWeatherMap";
 
-        getDriver().get(url);
-        getDriver().manage().window().maximize();
+        getDriver().get(WEATHER_URL);
+//        getDriver().manage().window().maximize();
         Thread.sleep(10000);
         getDriver().findElement(By.xpath("//a[@href = '/guide']")).click();
 
@@ -71,10 +59,9 @@ public class ViktoriyaEDTest extends BaseTest {
     @Test
     public void test_CheckWeather_WhenChooseFahrenheit() throws InterruptedException {
 
-        String url = "https://openweathermap.org/";
         String expectedResult = "°F";
 
-        getDriver().get(url);
+        getDriver().get(WEATHER_URL);
         Thread.sleep(10000);
 
         getDriver().findElement(By.xpath("//div[@class='switch-container']/div[3]")).click();
@@ -87,25 +74,22 @@ public class ViktoriyaEDTest extends BaseTest {
     @Test
     public void test_CheckCookiesText() throws InterruptedException {
 
-        String url = "https://openweathermap.org/";
         String expectedResult = "We use cookies which are essential for the site to work. We also use non-essential " +
                 "cookies to help us improve our services. Any data collected is anonymised. You can allow all cookies " +
                 "or manage them individually.";
         String expectedResult1 = "Allow all";
         String expectedResult3 = "Manage cookies";
 
-        getDriver().get(url);
+        getDriver().get(WEATHER_URL);
         Thread.sleep(10000);
 
         Assert.assertTrue(getDriver().findElement(By.className("stick-footer-panel__container")).isDisplayed());
 
         WebElement cookiesText = getDriver().findElement(By.xpath("//div[@id='stick-footer-panel']//p"));
         WebElement allowAllButton = getDriver().findElement(
-                By.xpath("//div[@id='stick-footer-panel']//button")
-        );
+                By.xpath("//div[@id='stick-footer-panel']//button"));
         WebElement manageCookiesButton = getDriver().findElement(
-                By.xpath("//div[@id='stick-footer-panel']//a[@href='/cookies-settings']")
-        );
+                By.xpath("//div[@id='stick-footer-panel']//a[@href='/cookies-settings']"));
 
         Assert.assertEquals(cookiesText.getText(), expectedResult);
         Assert.assertEquals(allowAllButton.getText(), expectedResult1);
@@ -115,28 +99,22 @@ public class ViktoriyaEDTest extends BaseTest {
     @Test
     public void testSupportDropDownMenu() throws InterruptedException {
 
-        String url = "https://openweathermap.org/";
-
         String expectedResult = "FAQ";
         String expectedResult1 = "How to start";
         String expectedResult2 = "Ask a question";
 
-        getDriver().get(url);
-        getDriver().manage().window().maximize();
+        getDriver().get(WEATHER_URL);
+//        getDriver().manage().window().maximize();
         Thread.sleep(10000);
 
-        WebElement menuSupportDropDown = getDriver().findElement(By.xpath("//div[@id='support-dropdown']"));
-        menuSupportDropDown.click();
+        getDriver().findElement(By.xpath("//div[@id='support-dropdown']")).click();
 
         WebElement FAQ = getDriver().findElement(
-                By.xpath("//ul[@id='support-dropdown-menu']//a[@href='/faq']")
-        );
+                By.xpath("//ul[@id='support-dropdown-menu']//a[@href='/faq']"));
         WebElement HowToStart = getDriver().findElement(
-                By.xpath("//ul[@id='support-dropdown-menu']//a[@href='/appid']")
-        );
+                By.xpath("//ul[@id='support-dropdown-menu']//a[@href='/appid']"));
         WebElement AskAQuestion = getDriver().findElement(
-                By.xpath("//ul[@id='support-dropdown-menu']//a[@target='_blank']")
-        );
+                By.xpath("//ul[@id='support-dropdown-menu']//a[@target='_blank']"));
 
         Assert.assertEquals(FAQ.getText(), expectedResult);
         Assert.assertEquals(HowToStart.getText(), expectedResult1);
@@ -146,14 +124,12 @@ public class ViktoriyaEDTest extends BaseTest {
     @Test
     public void test_ClickSubmitWithoutCaptcha() throws InterruptedException {
 
-        String url = "https://openweathermap.org/";
-
         String emailTest = "tester@test.com";
         String message = "This is test";
         String expectedResult = "reCAPTCHA verification failed, please try again.";
 
-        getDriver().get(url);
-        getDriver().manage().window().maximize();
+        getDriver().get(WEATHER_URL);
+//        getDriver().manage().window().maximize();
         Thread.sleep(10000);
 
         getDriver().findElement(By.xpath("//div[@id='support-dropdown']")).click();
@@ -167,7 +143,8 @@ public class ViktoriyaEDTest extends BaseTest {
 
         getDriver().findElement(By.id("question_form_email")).sendKeys(emailTest);
         getDriver().findElement(By.xpath("//select[@id='question_form_subject']")).click();
-        getDriver().findElement(By.xpath("//select[@id='question_form_subject']//option[text()='Other']")).click();
+        getDriver().findElement(
+                By.xpath("//select[@id='question_form_subject']//option[text()='Other']")).click();
         getDriver().findElement(By.xpath("//textarea[@id='question_form_message']")).sendKeys(message);
         getDriver().findElement(By.xpath("//input[@data-disable-with='Create Question form']")).click();
         String actualResult = getDriver().findElement(By.xpath("//div[@class='help-block']")).getText();
@@ -178,9 +155,7 @@ public class ViktoriyaEDTest extends BaseTest {
     @Test
     public void test_SwitchFahrenheitToCelsius() throws InterruptedException {
 
-        String url = "https://openweathermap.org/";
-
-        getDriver().get(url);
+        getDriver().get(WEATHER_URL);
         Thread.sleep(10000);
 
         getDriver().findElement(By.xpath("//div[@id='weather-widget']//div[text()='Imperial: °F, mph']")).click();
@@ -196,11 +171,10 @@ public class ViktoriyaEDTest extends BaseTest {
     @Test
     public void test_FindRome() throws InterruptedException {
 
-        String url = "https://openweathermap.org/";
         String city = "Rome";
         boolean expectedResult = true;
 
-        getDriver().get(url);
+        getDriver().get(WEATHER_URL);
         Thread.sleep(10000);
 
         getDriver().findElement(
@@ -215,14 +189,12 @@ public class ViktoriyaEDTest extends BaseTest {
     @Test
     public void test_APICheck30Buttons() throws InterruptedException {
 
-        String url = "https://openweathermap.org/";
         int expectedResult = 30;
 
-        getDriver().get(url);
+        getDriver().get(WEATHER_URL);
         Thread.sleep(10000);
 
-        WebElement aPIButton = getDriver().findElement(By.xpath("//div[@id='desktop-menu']//a[@href='/api']"));
-        aPIButton.click();
+        getDriver().findElement(By.xpath("//div[@id='desktop-menu']//a[@href='/api']")).click();
 
         List<WebElement> orangeButtons29 = getDriver().findElements(By.xpath("//a[@class='btn_block orange round']"));
         List<WebElement> orangeButton1 = getDriver().findElements(By.xpath("//a[@class='ow-btn round btn-orange']"));
