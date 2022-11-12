09007d82ae8413d13a1a4713338224199ae46d09
BugorDmitriy <116024901+BugorDmitriy@users.noreply.github.com>
2022-11-12 08:28:39 -0500

    BugorDmitriy (#454)
    * testConfirmText()
    
    * testMenuSupport()
    
    * testMenuSupport() added

--- src/test/java/BugorDmitriyTest.java
+++ src/test/java/BugorDmitriyTest.java
@@ -5,43 +5,44 @@ import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class BugorDmitriyTest extends BaseTest {
-    @Test
-    public void testH2TextWhenSearchingCityCountry() throws InterruptedException {
-        String url = "https://openweathermap.org/";
-        String cityName = "Paris";
-        String expectedResult = "Paris, FR";
+        @Test
+        public void testH2TextWhenSearchingCityCountry() throws InterruptedException {
+            String url = "https://openweathermap.org/";
+            String cityName = "Paris";
+            String expectedResult = "Paris, FR";
 
-        getDriver().get(url);
-        getDriver().manage().deleteAllCookies();
-        getDriver().manage().window().maximize();
+            getDriver().get(url);
+            getDriver().manage().deleteAllCookies();
+            getDriver().manage().window().maximize();
 
-        Thread.sleep(10000);
+            Thread.sleep(10000);
 
-        WebElement searchCityField = getDriver().findElement(
-                By.xpath("//div[@id='weather-widget']//input[@placeholder='Search city']")
-        );
-        searchCityField.click();
-        searchCityField.sendKeys(cityName);
+            WebElement searchCityField = getDriver().findElement(
+                   By.xpath("//div[@id='weather-widget']//input[@placeholder='Search city']")
+            );
+            searchCityField.click();
+            searchCityField.sendKeys(cityName);
 
-        WebElement searchButton = getDriver().findElement(
-                By.xpath("//div[@id = 'weather-widget']//button[@type = 'submit']")
-        );
-        searchButton.click();
+            WebElement searchButton = getDriver().findElement(
+                   By.xpath("//div[@id = 'weather-widget']//button[@type = 'submit']")
+            );
+            searchButton.click();
 
-        Thread.sleep(1000);
-        WebElement parisFRChoiceDropdownMenu = getDriver().findElement(
-                By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Paris, FR ']")
-        );
-        parisFRChoiceDropdownMenu.click();
+            Thread.sleep(3000);
+            WebElement parisFRChoiceDropdownMenu = getDriver().findElement(
+                   By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Paris, FR ']")
+            );
+            parisFRChoiceDropdownMenu.click();
 
-        WebElement h2CityCountryHeader = getDriver().findElement(
-                By.xpath("//div[@id = 'weather-widget']//h2")
-        );
-        Thread.sleep(1000);
-        String actualResult = h2CityCountryHeader.getText();
+            WebElement h2CityCountryHeader = getDriver().findElement(
+                   By.xpath("//div[@id = 'weather-widget']//h2")
+            );
+            Thread.sleep(3000);
+            String actualResult = h2CityCountryHeader.getText();
 
-        Assert.assertEquals(actualResult, expectedResult);
+            Assert.assertEquals(actualResult, expectedResult);
     }
+
             @Test
         public void testConfirmText() throws InterruptedException {
             String url = "https://openweathermap.org/";
@@ -67,5 +68,37 @@ public class BugorDmitriyTest extends BaseTest {
             Assert.assertEquals(actualResult1, expectedResult1);
             Assert.assertEquals(actualResultAllowAll, expectedResult2);
             Assert.assertEquals(actualResultManageCookies, expectedResult3);
+    }
+
+        @Test
+        public void testMenuSupport() throws InterruptedException {
+            String url = "https://openweathermap.org/";
+            String expectedResult1 = "FAQ";
+            String expectedResult2 = "How to start";
+            String expectedResult3 = "Ask a question";
+
+            getDriver().get(url);
+            Thread.sleep(10000);
+
+            WebElement headingSupportDropdown = getDriver().findElement(
+                By.xpath("//div[@id='support-dropdown']")
+            );
+            headingSupportDropdown.click();
+            Thread.sleep(5000);
+
+            Assert.assertEquals(
+            getDriver().findElements(By.xpath("//ul[@id='support-dropdown-menu']/*")).size(), 3
+            );
+
+            String actualResultFAQ = getDriver().findElement(
+                By.xpath("//ul[@class='dropdown-menu dropdown-visible']//a [@href='/faq']")).getText();
+            String actualResultHowToStart = getDriver().findElement(
+                By.xpath("//ul[@class='dropdown-menu dropdown-visible']//li/a[@href='/appid']")).getText();
+            String actualResultAskAQuestion = getDriver().findElement(
+                By.xpath("//ul[@class='dropdown-menu dropdown-visible']//a[text()='Ask a question']")).getText();
+
+            Assert.assertEquals(actualResultFAQ, expectedResult1);
+            Assert.assertEquals(actualResultHowToStart, expectedResult2);
+            Assert.assertEquals(actualResultAskAQuestion, expectedResult3);
     }
    }
