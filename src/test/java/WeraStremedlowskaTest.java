032ee54acfb7f5ac53f76fe39dc62332bcea873e
Wera <stremedlowska@gmail.com>
2022-11-13 00:19:19 +0100

    WeraStremedlowska3 (#482)
    * testMenuSupportHasThreeSubmenus()

--- src/test/java/WeraStremedlowskaTest.java
+++ src/test/java/WeraStremedlowskaTest.java
@@ -2,9 +2,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class WeraStremedlowskaTest extends BaseTest {
 
@@ -63,29 +68,58 @@ public class WeraStremedlowskaTest extends BaseTest {
     }
 
     @Test
-    public void testTempFahrenheitChangesToCelsius() throws InterruptedException {
+    public void testTempFahrenheitChangesToCelsius(){
         String url = "https://openweathermap.org/";
         String expectedResult = "°C";
 
         getDriver().get(url);
-        Thread.sleep(10000);
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("owm-loader-container")));
+        getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         getDriver().findElement(
                         By.xpath("//div[@id='weather-widget']/div[1]/div/div/div[1]/div[2]/div[text()='Imperial: °F, mph']"))
                 .click();
-        Thread.sleep(5000);
+
         getDriver().findElement(By.xpath("//div[@id='weather-widget']/div/div/div/div/div/div[2]")).click();
 
-        WebElement fahrenheit = getDriver().findElement(
+        WebElement celsius = getDriver().findElement(
                 By.xpath("//div[@id='weather-widget']//div[2]/div/div/div/div/span")
         );
 
-        Thread.sleep(3000);
 
-        String actualResult = fahrenheit.getText();
+        String actualResult = celsius.getText();
         actualResult = actualResult.substring(actualResult.length() - 2);
 
         Assert.assertEquals(actualResult, expectedResult);
+
+    }
+
+    @Test
+    public void testMenuSupportHasThreeSubmenus()  {
+        String baseUrl = "https://openweathermap.org/";
+        String expectedResultSubmenu1 = "FAQ";
+        String expectedResultSubmenu2 = "How to start";
+        String expectedResultSubmenu3 = "Ask a question";
+
+        getDriver().get(baseUrl);
+
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("owm-loader-container")));
+        getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        getDriver().findElement(By.id("support-dropdown")).click();
+
+        Assert.assertEquals(getDriver().findElements(
+                By.xpath("//ul[@id='support-dropdown-menu']/li")).size(), 3);
+
+        String actualResult1 = getDriver().findElement(By.xpath("//body/nav/ul[1]/div/ul/li[12]/ul/li[1]/a")).getText();
+        String actualResult2 = getDriver().findElement(By.xpath("//body/nav/ul[1]/div/ul/li[12]/ul/li[2]/a")).getText();
+        String actualResult3 = getDriver().findElement(By.xpath("//body/nav/ul[1]/div/ul/li[12]/ul/li[3]/a")).getText();
+
+        Assert.assertEquals(actualResult1, expectedResultSubmenu1);
+        Assert.assertEquals(actualResult2, expectedResultSubmenu2);
+        Assert.assertEquals(actualResult3, expectedResultSubmenu3);
 
     }
 }
