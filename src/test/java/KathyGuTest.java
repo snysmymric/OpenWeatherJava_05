9751425b2f66b3b01ca9ab366b44aeef9373e88e
Kate <97336394+KathyGU@users.noreply.github.com>
2022-11-14 15:46:07 +0300

    [RF][Main][Navbar][Menu][Support]KathyGuTest / testFaqHowToStartAskQuestion_InSupportMenu_first () deleted<KathyGU> (#512)
    * testFaqHowToStartAskQuestion_InSupportMenu_first_deleted
    
    * testFaqHowToStartAskQuestion_InSupportMenu_first_deleted
    
    * testFaqHowToStartAskQuestion_InSupportMenu_first_deleted

--- src/test/java/KathyGuTest.java
+++ src/test/java/KathyGuTest.java
@@ -7,44 +7,31 @@ import runner.BaseTest;
 
 @Ignore
 public class KathyGuTest extends BaseTest {
-    @Test
 
-    public void testFaqHowToStartAskQuestion_InSupportMenu_first () throws InterruptedException {
 
-        String url = "https://openweathermap.org";
-        String textFaq = "FAQ";
-        String textHowToStart = "How to start";
-        String textAskQuestion = "Ask a question";
-
-        getDriver().get(url);
-        Thread.sleep(10000);
-
-        WebElement menuSupport = getDriver().findElement(By.id("support-dropdown"));
-        menuSupport.click();
-
-        Assert.assertEquals(
-                getDriver().findElements(By.xpath("//ul[@id = 'support-dropdown-menu']/li"))
-                        .size(), 3);
-
-        Assert.assertTrue(
-                getDriver().findElement(
-                        By.xpath("//ul[@id = 'support-dropdown-menu']/li/a[text() = '" + textFaq + "']"
-                        )
-                ).isDisplayed()
-        );
-
-        Assert.assertTrue(
-                getDriver().findElement(
-                        By.xpath("//ul[@id = 'support-dropdown-menu']/li/a[text() = '" + textHowToStart + "']"
-                        )
-                ).isDisplayed()
-        );
-
-        Assert.assertTrue(
-                getDriver().findElement(
-                        By.xpath("//ul[@id = 'support-dropdown-menu']/li/a[text() = '" + textAskQuestion + "']"
-                        )
-                ).isDisplayed()
-        );
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
