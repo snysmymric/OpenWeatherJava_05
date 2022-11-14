930376b4dd1fa6307d1f6d4c4dd91375c4a71ff4
Dima_Zadrutski <dzadruckiy@gmail.com>
2022-11-14 12:05:10 +0200

    [RF][Main][NavBar][Menu][Search] DimaZadrutsiyTest / testSearchLineCityRome() DELETED <DimaZadrutsiy> (#514)
    [RF][Start][Weather][Current Weather][h2] DimaZadrutsiyTest / testH2TagText_WhenSearchingCitiCountry() DELETED <DimaZadrutsiy>
    [RF][Main][NavBar][Menu][API] DimaZadrutsiyTest / testOrangeBatton() DELETED <DimaZadrutsiy>
    Delete 3 tests

--- src/test/java/DimaZadrutsiyTest.java
+++ src/test/java/DimaZadrutsiyTest.java
@@ -6,43 +6,10 @@ import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-@Ignore
+
 public class DimaZadrutsiyTest extends BaseTest {
-    @Test
-    public void testH2TagText_WhenSearchingCitiCountry() throws InterruptedException {
 
-        String url = "https://openweathermap.org/";
-        String cityName = "Paris";
-        String expectedResult = "Paris, FR";
-
-        getDriver().get(url);
-        Thread.sleep(10000);
-        WebElement searchCityField = getDriver().findElement(
-                By.xpath("//div[@id = 'weather-widget']//input[@placeholder = 'Search city']"));
-        searchCityField.click();
-        searchCityField.sendKeys(cityName);
-
-
-        WebElement searchButton = getDriver().findElement(
-                By.xpath("//div[@id='weather-widget']//button[@type ='submit']"));
-        searchButton.click();
-
-        Thread.sleep(3000);
-        WebElement firstChoiesInDropdownMenu = getDriver().findElement(
-                By.xpath("//div[@id='weather-widget']//ul[@class='search-dropdown-menu']/li/span[text()"
-                        + "='Paris, FR ']"));
-        firstChoiesInDropdownMenu.click();
-
-        Thread.sleep(4000);
-        WebElement h2CityCountryHeader = getDriver().findElement(
-                By.xpath("//div[@id='weather-widget']//h2"));
-
-        Thread.sleep(3000);
-        String actualResult = h2CityCountryHeader.getText();
-
-        Assert.assertEquals(actualResult, expectedResult);
-    }
-
+    @Ignore
     @Test
     public void testPageReload() throws InterruptedException {
 
@@ -61,7 +28,7 @@ public class DimaZadrutsiyTest extends BaseTest {
 
         Assert.assertEquals(actualResult, expectedResult);
     }
-
+    @Ignore
     @Test
     public void testIconCurrentLocation() throws InterruptedException {
 
@@ -92,48 +59,5 @@ public class DimaZadrutsiyTest extends BaseTest {
         String actualResult = actualLocation.getText();
 
         Assert.assertEquals(actualResult, expectedResult);
-    }
-
-    @Test
-    public void testOrangeBatton() throws InterruptedException {
-
-        String url = "https://openweathermap.org/";
-        int expectedResult = 30;
-
-        getDriver().get(url);
-
-        Thread.sleep(10000);
-        WebElement linkAPI = getDriver().findElement(By.xpath("//a[@href='/api']"));
-        linkAPI.click();
-
-        int actualResult = getDriver().findElements(By.xpath("//a[contains(@class, 'orange')]")).size();
-
-        Assert.assertEquals(actualResult, expectedResult);
-    }
-
-    @Test
-    public void testSearchLineCityRome() throws InterruptedException {
-        String url = "https://openweathermap.org/";
-        String cityName = "Rome";
-        String find = "find";
-
-        String expectedResult = "Rome";
-
-        getDriver().get(url);
-
-        Thread.sleep(10000);
-        WebElement searchLine = getDriver().findElement(By.name("q"));
-        searchLine.click();
-        searchLine.sendKeys(cityName);
-        searchLine.sendKeys(Keys.ENTER);
-
-        boolean actualResult = getDriver().getCurrentUrl().contains(find) && getDriver().getCurrentUrl().contains(cityName);
-
-        WebElement cityInTheSearchBar = getDriver().findElement(By.id("search_str"));
-
-        String actualResult2 = cityInTheSearchBar.getAttribute("value");
-
-        Assert.assertTrue(actualResult);
-        Assert.assertEquals(actualResult2, expectedResult);
     }
 }
