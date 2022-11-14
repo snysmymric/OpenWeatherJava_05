89d3f276cc008d89fa43774c5f331134baf2528a
Viktoriia Huk <viktoriaguk1986@gmail.com>
2022-11-14 16:34:15 +0200

    [RF][Main][NavBar][API] ViktoriiaHukTest/testWenPageApiHas30OrangeButten<ViktoriiaHuk> (#522)

--- src/test/java/ViktoriiaHukTest.java
+++ src/test/java/ViktoriiaHukTest.java
@@ -1,22 +1,38 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-@Ignore
+
 public class ViktoriiaHukTest extends BaseTest {
-    private static final String URL = "https://openweathermap.org/";
+    private static final String BASE_URL = "https://openweathermap.org/";
+    private static final By DESKTOP_MENU_API = By.xpath("//div[@id = 'desktop-menu']/ul/li[2]/a");
+    private static final By ORANGE_BUTTON_IN_API_MENU = By.xpath("//a[contains(@class, 'orange')]");
+
+    private void openBaseURL() {
+        getDriver().get(BASE_URL);
+    }
+
+    private void waitForGrayFrameDisappeared() {
+        getWait20().until(ExpectedConditions.invisibilityOfElementLocated(By.className("owm-loader-container")));
+    }
+
+    private void click(By by, WebDriverWait wait) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
+    }
 
     @Test
-    public void testWenPageApiHas30OrangeButten() throws InterruptedException {
+    public void testWenPageApiHas30OrangeButten() {
         int expectedResult = 30;
 
-        getDriver().get(URL);
-        Thread.sleep(10000);
-        getDriver().findElement(By.xpath("//div[@id = 'desktop-menu']/ul/li[2]/a")).click();
+        openBaseURL();
+        waitForGrayFrameDisappeared();
+        click(DESKTOP_MENU_API, getWait10());
 
-        int actualResult = getDriver().findElements(By.xpath("//a[contains(@class, 'orange')]")).size();
+        int actualResult = getDriver().findElements(ORANGE_BUTTON_IN_API_MENU).size();
 
         Assert.assertEquals(actualResult, expectedResult);
     }
