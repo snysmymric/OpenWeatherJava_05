f9114345d351246b64189453483c47dfd2eb4f92
NataliiaOliver <103964206+NataliiaOliver@users.noreply.github.com>
2022-11-14 13:45:25 -0600

    testH2TagText_WhenSearchingCityCountry() refactored, methods added (#531)

--- src/test/java/NataliiaOliverTest.java
+++ src/test/java/NataliiaOliverTest.java
@@ -1,56 +1,84 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-@Ignore
+
 public class NataliiaOliverTest extends BaseTest {
 
-    private static final String URL = "https://openweathermap.org/";
+    final static String BASE_URL = "https://openweathermap.org/";
+
+    final static By H2_CITY_COUNTRY_HEADER = By.xpath("//div[@id = 'weather-widget']//h2");
+    final static By SEARCH_CITY_FIELD = By.xpath("//div[@id = 'weather-widget']//input[@placeholder = 'Search city']");
+    final static By SEARCH_BUTTON = By.xpath("//div[@id = 'weather-widget']//button[@type = 'submit']");
+    final static By SEARCH_DROPDOWN_MENU = By.className("search-dropdown-menu");
+    final static By PARIS_FR_CHOICE_IN_DROPDOWN_MENU = By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Paris, FR ']");
+
+    private void openBaseURL() {
+        getDriver().get(BASE_URL);
+    }
+
+    private void waitForGrayFrameDisappeared() {
+        getWait20().until(ExpectedConditions.invisibilityOfElementLocated(
+                By.className("owm-loader-container")));
+    }
+
+    private String getText(By by, WebDriver driver) {
+
+        return driver.findElement(by).getText();
+    }
+
+    private void click(By by,  WebDriverWait wait) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
+    }
+
+    private void input(String text, By by, WebDriver driver) {
+        driver.findElement(by).sendKeys(text);
+    }
+
+    private void waitElementToBeVisible(By by, WebDriverWait wait) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+
+    private void waitTextToBeChanged(By by, String text, WebDriver driver, WebDriverWait wait) {
+        wait.until(ExpectedConditions
+                .not(ExpectedConditions.textToBePresentInElement(driver.findElement(by), text)));
+    }
 
     @Test
-    public void testH2TagText_WhenSearchingCityCountry() throws InterruptedException {
+    public void testH2TagText_WhenSearchingCityCountry() {
         String cityName = "Paris";
         String expectedResult = "Paris, FR";
 
-        getDriver().get(URL);
-        Thread.sleep(10000);
+        openBaseURL();
+        waitForGrayFrameDisappeared();
 
-        WebElement searchCityField = getDriver().findElement(
-                By.xpath("//div[@id='weather-widget']//input[@placeholder='Search city']")
-        );
-        searchCityField.click();
-        searchCityField.sendKeys(cityName);
+        String oldH2Header = getText(H2_CITY_COUNTRY_HEADER, getDriver());
 
-        WebElement searchButton = getDriver().findElement(
-                By.xpath("//div[@id='weather-widget']//button[@type='submit']")
-        );
-        searchButton.click();
+        click(SEARCH_CITY_FIELD, getWait5());
+        input(cityName, SEARCH_CITY_FIELD, getDriver());
+        click(SEARCH_BUTTON, getWait5());
+        waitElementToBeVisible(SEARCH_DROPDOWN_MENU, getWait10());
+        click(PARIS_FR_CHOICE_IN_DROPDOWN_MENU, getWait10());
+        waitTextToBeChanged(H2_CITY_COUNTRY_HEADER, oldH2Header, getDriver(), getWait10());
 
-        Thread.sleep(1000);
-
-        WebElement parisFRChoiceInDropdownMenu = getDriver().findElement(
-                By.xpath("//ul[@class='search-dropdown-menu']/li/span[text()='Paris, FR ']")
-        );
-        parisFRChoiceInDropdownMenu.click();
-
-        WebElement h2CityCountryHeader = getDriver().findElement(
-                By.xpath("//div[@id='weather-widget']//h2")
-        );
-
-        Thread.sleep(2000);
-        String actualResult = h2CityCountryHeader.getText();
+        String actualResult = getText(H2_CITY_COUNTRY_HEADER, getDriver());
 
         Assert.assertEquals(actualResult, expectedResult);
     }
 
+    @Ignore
     @Test
     public void testPressImperialButtonF_TempIsdIsDisplayedInF() throws InterruptedException {
         String expectedResult = "°F";
 
-        getDriver().get(URL);
+        getDriver().get(BASE_URL);
         Thread.sleep(10000);
 
         WebElement searchImperial = getDriver().findElement(
@@ -66,6 +94,7 @@ public class NataliiaOliverTest extends BaseTest {
         Assert.assertTrue(tempInF.contains(expectedResult));
     }
 
+    @Ignore
     @Test
     public void testFindingCookiePanelAndTwoButtons() throws InterruptedException {
         String expectedResult1 = "We use cookies which are essential for the site to work. We also use non-essential "
@@ -74,7 +103,7 @@ public class NataliiaOliverTest extends BaseTest {
         String expectedResult2 = "Allow all";
         String expectedResult3 = "Manage cookies";
 
-        getDriver().get(URL);
+        getDriver().get(BASE_URL);
         Thread.sleep(10000);
 
         WebElement footerCookiePanel = getDriver().findElement(
