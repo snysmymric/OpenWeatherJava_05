6592f1d7dbb747880d9ca2ccc7f02fe4f37aa52a
Tatiana P <103320248+dmswlpk@users.noreply.github.com>
2022-11-14 09:39:48 -0600

    [RF]All imports deleted from DmswlpkTest class <dmswlpk> (#526)

--- src/test/java/DmswlpkTest.java
+++ src/test/java/DmswlpkTest.java
@@ -1,14 +1,4 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
-import org.testng.annotations.Ignore;
-import org.testng.annotations.Test;
 import runner.BaseTest;
-
-import java.util.List;
-
-import static java.lang.Thread.sleep;
-import static org.openqa.selenium.Keys.ENTER;
 
 public class DmswlpkTest extends BaseTest {
 
