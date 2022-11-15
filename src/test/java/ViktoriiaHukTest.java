b3d8f1174feb5abb9ce8dbf609e26c693190274b
Viktoriia Huk <viktoriaguk1986@gmail.com>
2022-11-15 16:29:54 +0200

    [AT][Functional][Main][NavBar][Menu][Partners]Search list hyperlinks / testVerifyListHyperlinksOnPartnersPage<Viktoriia Huk> add fixed conflict (#570)

--- src/test/java/ViktoriiaHukTest.java
+++ src/test/java/ViktoriiaHukTest.java
@@ -1,10 +1,13 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ViktoriiaHukTest extends BaseTest {
@@ -17,7 +20,37 @@ public class ViktoriiaHukTest extends BaseTest {
             By.linkText("Java");
     private static final By BUTTON_VIEW_TO_HIT_HUB_IN_JAVA =
             By.xpath("//section [@id='java']/a");
-    private static final By HYPERLINK_HOME_IN_MENU_PARTNERS = By.linkText("Home");
+    private static final By HYPER_LINK_LIST_IN_MENU_PARTNERS =
+            By.xpath("//div[@class = 'doc-container']//ul//a");
+    private List<String> getExpectedListHyperlinksInMenuPartners() {
+        List<String> expectedMenu = new ArrayList<>();
+        expectedMenu.add("Google Weather-Based Campaign Management with OpenWeatherMap API");
+        expectedMenu.add("Google Maps JavaScript API based on OpenWeatherMap API");
+        expectedMenu.add("OpenWeather current weather data in Mozilla's IoT project");
+        expectedMenu.add("Ubuntu");
+        expectedMenu.add("Android");
+        expectedMenu.add("Leaflet");
+        expectedMenu.add("Java");
+        expectedMenu.add("Go (golang)");
+        expectedMenu.add("JavaScript");
+        expectedMenu.add("CMS");
+        expectedMenu.add("Raspberry Pi");
+        expectedMenu.add("Python");
+        expectedMenu.add("PHP");
+        expectedMenu.add("Apache Camel");
+        expectedMenu.add("Desktop");
+        expectedMenu.add("Mobile applications");
+        expectedMenu.add("Big library on GitHub");
+        return expectedMenu;
+    }
+    private List<String> getActualListItemsInMenu(By locator) {
+        List<String> actualListItems = new ArrayList<>();
+        List<WebElement> listItemsInMenu = getDriver().findElements(locator);
+        for (WebElement items : listItemsInMenu) {
+            actualListItems.add(items.getText());
+        }
+        return actualListItems;
+    }
 
     private void openBaseURL() {
         getDriver().get(BASE_URL);
@@ -31,10 +64,11 @@ public class ViktoriiaHukTest extends BaseTest {
         wait.until(ExpectedConditions.visibilityOfElementLocated(by));
         wait.until(ExpectedConditions.elementToBeClickable(by)).click();
     }
-    private void newWindow (){
+
+    private void newWindow() {
         String originalWindow = getDriver().getWindowHandle();
         for (String windowHandle : getDriver().getWindowHandles()) {
-            if(!originalWindow.contentEquals(windowHandle)) {
+            if (!originalWindow.contentEquals(windowHandle)) {
                 getDriver().switchTo().window(windowHandle);
                 break;
             }
@@ -61,22 +95,23 @@ public class ViktoriiaHukTest extends BaseTest {
         openBaseURL();
         waitForGrayFrameDisappeared();
         click(DESKTOP_MENU_PARTNERS, getWait5());
-        click(HYPER_LINK_JAVA_IN_MENU_PARTNERS,getWait5());
+        click(HYPER_LINK_JAVA_IN_MENU_PARTNERS, getWait5());
         click(BUTTON_VIEW_TO_HIT_HUB_IN_JAVA, getWait20());
         newWindow();
 
         Assert.assertEquals(getDriver().getCurrentUrl(), expectedResult);
     }
+
     @Test
-    public void testVerifyActiveHyperlinkHomeOnPartnersPage(){
-        String expectedResult = "https://openweathermap.org/";
+    public void testVerifyListHyperlinksOnPartnersPage() {
+        List<String> expectedList = getExpectedListHyperlinksInMenuPartners();
 
         openBaseURL();
         waitForGrayFrameDisappeared();
         click(DESKTOP_MENU_PARTNERS, getWait5());
-        click(HYPERLINK_HOME_IN_MENU_PARTNERS, getWait5());
-        waitForGrayFrameDisappeared();
 
-        Assert.assertEquals(getDriver().getCurrentUrl(),expectedResult);
+        List<String> actualList = getActualListItemsInMenu(HYPER_LINK_LIST_IN_MENU_PARTNERS);
+
+        Assert.assertEquals(actualList, expectedList);
     }
 }
