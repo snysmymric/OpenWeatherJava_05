b8087b86f06da13bacac5f28aba82f3547f80e0b
Kristina Pereselkina <kristinapereselkina@gmail.com>
2022-11-15 13:10:20 +0100

    [RF][Main][NavBar][API] testRedirectionToApiGuidePage() deleted <KristinaPereselkina> (#566)

--- src/test/java/KristinaPereselkinaTest.java
+++ src/test/java/KristinaPereselkinaTest.java
@@ -1,35 +1,7 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.testng.annotations.Ignore;
-import org.testng.annotations.Test;
 import runner.BaseTest;
 
-import static org.testng.Assert.assertEquals;
 
-@Ignore
 public class KristinaPereselkinaTest extends BaseTest {
 
-    @Test
-    public void testRedirectionToApiGuidePage() throws InterruptedException {
 
-        String baseUrl = "https://openweathermap.org/";
-
-        String expectedResultLink = "https://openweathermap.org/guide";
-        String expectedResultTab = "OpenWeatherMap API guide - OpenWeatherMap";
-
-        getDriver().get(baseUrl);
-        getDriver().manage().window().maximize();
-        Thread.sleep(5000);
-
-        WebElement searcGuideLink = getDriver().findElement(By.xpath("//div[@id='desktop-menu']/ul/li[1]"));
-        searcGuideLink.click();
-
-        Thread.sleep(2000);
-
-        String actualResultTab = getDriver().getTitle();
-        String actualResultLink = getDriver().getCurrentUrl();
-
-        assertEquals(actualResultTab, expectedResultTab);
-        assertEquals(actualResultLink, expectedResultLink);
-    }
 }
