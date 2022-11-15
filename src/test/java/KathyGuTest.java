3f16297fbfc4f883de46ded46ab1bcde3cc46c04
Kate <97336394+KathyGU@users.noreply.github.com>
2022-11-15 23:49:51 +0300

    testStartPageTitleAndURL_WhenOpenBaseUrl() addded (#590)

--- src/test/java/KathyGuTest.java
+++ src/test/java/KathyGuTest.java
@@ -1,15 +1,37 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-@Ignore
 public class KathyGuTest extends BaseTest {
-
-
+    final static String BASE_URL = "https://openweathermap.org/";
+    private void openBaseUrl() {
+        getDriver().get(BASE_URL);
     }
+    private void waitForGreyFrameDisappeared() {
+        getWait20().until(ExpectedConditions.invisibilityOfElementLocated(
+            By.className("owm-loader-container")));
+    }
+    private String getCurrentUrl() {
+       return getDriver().getCurrentUrl();
+    }
+    private String getTitle() {
+        return getDriver().getTitle();
+    }
+    @Test
+
+    public void testStartPageTitleAndURL_WhenOpenBaseUrl() {
+        String expectedTitle = "Сurrent weather and forecast - OpenWeatherMap";
+        String expectedUrl = "https://openweathermap.org/";
+
+        openBaseUrl();
+        waitForGreyFrameDisappeared();
+
+        Assert.assertEquals(getTitle(), expectedTitle);
+        Assert.assertEquals(getCurrentUrl(), expectedUrl);
+    }
+}
 
 
 
