aff092b9c415c507bca68061739ac11a81edc761
Lara Bee <115852420+larisabratukhina@users.noreply.github.com>
2022-11-14 17:08:12 -0800

    [RF][Weather][GuideMenu] testTitle_WhenGoingToGuideMenu() deleted <larisabratukhina> (#548)
    * testH2TagText_WhenSearchingCityCountry() deleted
    
    * testTitle_WhenGoingToGuideMenu() was deleted

--- src/test/java/LarisaBratukhinaTest.java
+++ src/test/java/LarisaBratukhinaTest.java
@@ -1,30 +1,5 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
-import org.testng.annotations.Ignore;
-import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class LarisaBratukhinaTest extends BaseTest {
 
-    @Ignore
-    @Test
-    public void testTitle_WhenGoingToGuideMenu() throws InterruptedException {
-        String url = "https://openweathermap.org/";
-        String expectedResultUrl = "https://openweathermap.org/guide";
-        String expectedResultTitle = "OpenWeatherMap API guide - OpenWeatherMap";
-
-        getDriver().get(url);
-        Thread.sleep(10000);
-
-        WebElement guideMenu = getDriver().findElement(By.xpath("//a[@href='/guide']"));
-        guideMenu.click();
-        Thread.sleep(2000);
-
-        String actualResultUrl = getDriver().getCurrentUrl();
-        String actualResultTitle = getDriver().getTitle();
-
-        Assert.assertEquals(actualResultUrl, expectedResultUrl);
-        Assert.assertEquals(actualResultTitle, expectedResultTitle);
-    }
 }
