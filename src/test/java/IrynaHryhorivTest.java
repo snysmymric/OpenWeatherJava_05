511eb804517541e731d1c39f9ce9c32805142f51
IrynaHryhoriv <114322325+IrynaHryhoriv@users.noreply.github.com>
2022-11-16 13:51:52 -0800

    ATVerifyChangingTempUnitInHeading_WhenSwitchTempUnitButton() added, method added (#636)

--- src/test/java/IrynaHryhorivTest.java
+++ src/test/java/IrynaHryhorivTest.java
@@ -1,22 +1,25 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
+
 public class IrynaHryhorivTest extends BaseTest {
     final static String BASE_URL = "https://openweathermap.org/";
-
     final static By H2_CITY_COUNTRY_HEADER = By.xpath("//div[@id = 'weather-widget']//h2");
     final static By SEARCH_CITY_FIELD = By.xpath("//div[@id = 'weather-widget']//input[@placeholder = 'Search city']");
     final static By SEARCH_BUTTON = By.xpath("//div[@id = 'weather-widget']//button[@type = 'submit']");
     final static By SEARCH_DROPDOWN_MENU = By.className("search-dropdown-menu");
     final static By PARIS_FR_CHOICE_IN_DROPDOWN_MENU = By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Paris, FR ']");
     final static By SEARCH_GUIDE_BUTTON = By.xpath("//a[@href='/guide']");
-
-    final static By CONFIRM_API = (By.xpath("//*[@id='desktop-menu']/ul/li[2]/a"));
+    final static By CONFIRM_API = By.xpath("//*[@id='desktop-menu']/ul/li[2]/a");
+    final static String TEMP_IMPERIAL_F = "Imperial: °F, mph";
+    final static String SYMBOL_TEMP = "°F";
+    final static By APPROVE_DEGREES = By.xpath("//div[@id='weather-widget']//span[@class='heading']");
 
 
     private void openBaseURL() {
@@ -52,6 +55,14 @@ public class IrynaHryhorivTest extends BaseTest {
     }
     private void waitURLToBeChanged(String url2, WebDriver driver, WebDriverWait wait) {
         wait.until(ExpectedConditions.urlToBe(url2));
+    }
+
+    private boolean find_element_by_xpath(WebDriver driver, String temp, String symbolTemp) {
+        WebElement tempValue = driver.findElement(By.xpath(String.format("//div[text()='%s']", temp)));
+        tempValue.click();
+        waitForGrayFrameDisappeared();
+
+        return getText(APPROVE_DEGREES, getDriver()).contains(symbolTemp);
     }
     @Test
     public void testH2TagText_WhenSearchingCityCountry() {
@@ -102,5 +113,13 @@ public class IrynaHryhorivTest extends BaseTest {
         waitURLToBeChanged(expectedResult, getDriver(), getWait10());
 
         Assert.assertEquals(getDriver().getCurrentUrl(), expectedResult);
+    }
+    @Test
+    public void VerifyChangingTempUnitInHeading_WhenSwitchTempUnitButton() {
+
+        openBaseURL();
+        waitForGrayFrameDisappeared();
+
+        Assert.assertTrue(find_element_by_xpath(getDriver(), TEMP_IMPERIAL_F, SYMBOL_TEMP));
     }
 }
