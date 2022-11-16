bcb7354949cebfbc2991c4b7364dfc8d76b094b9
Maksim Meleshkin <116033547+Maksim2404@users.noreply.github.com>
2022-11-15 17:51:43 -0800

    testH2TextWhenSearchingCityCountry() was changed (#600)

--- src/test/java/Maksim2404Test.java
+++ src/test/java/Maksim2404Test.java
@@ -1,47 +1,65 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-@Ignore
 public class Maksim2404Test extends BaseTest {
+    final static String Base_URL = "https://openweathermap.org/";
+    final static By H2_CITY_COUNTRY_HEADER = By.xpath("//div[@id='weather-widget']//h2");
+    final static By SEARCH_CITY_FIELD = By.xpath("//div[@id='weather-widget']//input[@placeholder='Search city']");
+    final static By SEARCH_BUTTON = By.xpath("//div[@id='weather-widget']//button[@type='submit']");
+    final static By SEARCH_DROPDOWN_MENU = By.className("search-dropdown-menu");
+    final static By PARIS_CHOICE_IN_DROP_DOWN_MENU = By.xpath(
+            "//ul[@class ='search-dropdown-menu']/li/span[text() = 'Paris, FR ']");
+    private void openBaseURL(){
+        getDriver().get(Base_URL);
+    }
+    private void waitForGrayFrameDisappeared(){
+        getWait20().until(ExpectedConditions.invisibilityOfElementLocated(By.className("owm-loader-container")));
+    }
+    private String getText(By by, WebDriver driver){
+        return driver.findElement(by).getText();
+    }
+    private void click(By by, WebDriver driver){
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(by));
+        getWait5().until(ExpectedConditions.elementToBeClickable(by)).click();
+    }
+    private void input(String text, By by, WebDriver driver){
+        driver.findElement(by).sendKeys(text);
+    }
+    private void waitElementToBeVisible(By by, WebDriverWait wait){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+    private void waitTextToBeChanged(By by, String text, WebDriver driver, WebDriverWait wait){
+        wait.until(ExpectedConditions
+                .not(ExpectedConditions.textToBePresentInElement(driver.findElement(by), text)));
+    }
     @Test
     public void testH2TextWhenSearchingCityCountry() throws InterruptedException {
-        String url = "https://openweathermap.org/";
         String cityName = "Paris";
         String expectedResult = "Paris, FR";
 
-        getDriver().get(url);
-        Thread.sleep(10000);
+        openBaseURL();
+        waitForGrayFrameDisappeared();
 
-        WebElement searchCityField = getDriver().findElement(
-                By.xpath("//div[@id = 'weather-widget']//input[@placeholder = 'Search city']"));
-        searchCityField.click();
-        searchCityField.sendKeys(cityName);
+        String oldH2Header = getText(H2_CITY_COUNTRY_HEADER, getDriver());
 
-        WebElement searchButton = getDriver().findElement(
-                By.xpath("//button[@type = 'submit']")
-        );
-        searchButton.click();
-        Thread.sleep(1000);
-
-        WebElement parisFRChoiceInDropdownMenu = getDriver().findElement(
-                By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Paris, FR ']")
-        );
-        parisFRChoiceInDropdownMenu.click();
-
-        WebElement h2CityCountryHeader = getDriver().findElement(
-                By.xpath("//div[@id = 'weather-widget']//h2")
-        );
-
-        Thread.sleep(2000);
-        String actualResult = h2CityCountryHeader.getText();
+        click(SEARCH_CITY_FIELD, getDriver());
+        input(cityName, SEARCH_CITY_FIELD, getDriver());
+        click(SEARCH_BUTTON, getDriver());
+        waitElementToBeVisible(SEARCH_DROPDOWN_MENU, getWait10());
+        click(PARIS_CHOICE_IN_DROP_DOWN_MENU, getDriver());
+        waitTextToBeChanged(H2_CITY_COUNTRY_HEADER, oldH2Header, getDriver(), getWait10());
+        String actualResult = getText(H2_CITY_COUNTRY_HEADER, getDriver());
 
         Assert.assertEquals(actualResult, expectedResult);
     }
-
+    @Ignore
     @Test
     public void testConfirmThatSupportHave3Types() throws InterruptedException {
         getDriver().manage().window().maximize();
@@ -86,6 +104,7 @@ public class Maksim2404Test extends BaseTest {
         Assert.assertEquals(actualResult3, expectedResult3);
     }
 
+    @Ignore
     @Test
     public void testconfirmSite() throws InterruptedException {
 
