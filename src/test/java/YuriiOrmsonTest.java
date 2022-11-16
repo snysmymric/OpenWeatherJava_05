0f8a6cfb96355913932e7a66352e6db30eda5ca6
Yurii_Ormson <Seraph2401@gmail.com>
2022-11-16 20:50:53 +0200

    testBtnBlock_WhenOpenPricingPage added (#626)

--- src/test/java/YuriiOrmsonTest.java
+++ src/test/java/YuriiOrmsonTest.java
@@ -6,16 +6,13 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-
 public class YuriiOrmsonTest extends BaseTest {
     final static String BASE_URL = "https://openweathermap.org/";
-    final static String PRICING_URL = "https://openweathermap.org/price";
     final static By A_HREF_PRICE = By.xpath("//div[@id = 'desktop-menu']//a[@href = '/price']");
     final static By A_HREF_CURRENT_PROFESSIONAL_SUBSCRIPTIONS = By.xpath(
             "//main//p//a[@href ='#current' and contains(text(), 'professional subscriptions')]");
     final static By H1 = By.xpath("//h1");
-    final static By PROFFESSIONAL_COLLECTION_SECTION = By.xpath(
-            "//div[@class = 'col-sm-12']//h2[contains(text(),'Current weather and forecasts collection')]");
+    final static By BTN_BLOCK_TRANSPARENT_ROUND = By.xpath("//a[@class = 'btn_block transparent round']");
     private void openBaseURL() {
         getDriver().get(BASE_URL);
     }
@@ -29,6 +26,9 @@ public class YuriiOrmsonTest extends BaseTest {
     }
     private String getText(By by, WebDriver driver) {
         return driver.findElement(by).getText();
+    }
+    private int countElements(By by, WebDriver driver) {
+        return driver.findElements(by).size();
     }
     @Test
     public void testH1BreadcrumbTitle_WhenOpenPricingPage() {
@@ -80,6 +80,19 @@ public class YuriiOrmsonTest extends BaseTest {
         click(A_HREF_CURRENT_PROFESSIONAL_SUBSCRIPTIONS,getWait5());
 
         String actualResult = getDriver().getCurrentUrl();
+
+        Assert.assertEquals(actualResult,expectedResult);
+    }
+    @Test
+    public void testBtnBlock_WhenOpenPricingPage() {
+        int expectedResult = 19;
+
+        openBaseURL();
+        waitForGrayFrameDisappeared();
+        click(A_HREF_PRICE,getWait5());
+        waitForGrayFrameDisappeared();
+        click(A_HREF_CURRENT_PROFESSIONAL_SUBSCRIPTIONS,getWait5());
+        int actualResult = countElements(BTN_BLOCK_TRANSPARENT_ROUND,getDriver());
 
         Assert.assertEquals(actualResult,expectedResult);
     }
