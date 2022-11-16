2c952bc4904c79d980415851af6bf143c99e20c9
Elena_S <sosnitsky.e@gmail.com>
2022-11-16 11:49:29 +0200

    [AT][Start][URL][Title] testOpenWeatherStartPageOpened <Elena_S> (#608)
    [US][Functional][Page][URL][Title]Verify Start pageTitetle and URL IrinaDud
    [TC][Start][URL][Title] Verify that Start page is opened <Elena_S>
    [AT][Start][URL][Title] testOpenWeatherStartPageOpened <Elena_S>

--- src/test/java/Elena_STest.java
+++ src/test/java/Elena_STest.java
@@ -1,7 +1,24 @@
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class Elena_STest extends BaseTest {
+    final static String BASE_URL ="https://openweathermap.org/";
+    private void openBaseUrl(){
+        getDriver().get(BASE_URL);
+    }
+    @Test
+    public void testOpenWeatherStartPageOpened(){
+        String expectedResultTitle = "Сurrent weather and forecast - OpenWeatherMap";
+        String expectedResultUrl = "https://openweathermap.org/";
+        openBaseUrl();
+        String actualResultTitle = getDriver().getTitle();
+        String actualResultUrl = getDriver().getCurrentUrl();
+
+        Assert.assertEquals(actualResultTitle,expectedResultTitle);
+        Assert.assertEquals(actualResultUrl,expectedResultUrl);
+    }
 
 }
 
