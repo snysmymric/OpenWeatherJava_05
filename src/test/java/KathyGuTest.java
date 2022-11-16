2f06ad09af3a34117e0ecece0341ef68e9863cb0
Kate <97336394+KathyGU@users.noreply.github.com>
2022-11-16 12:56:06 +0300

    testClickOnLogoRedirectUserToStartPage added (#612)

--- src/test/java/KathyGuTest.java
+++ src/test/java/KathyGuTest.java
@@ -1,11 +1,16 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class KathyGuTest extends BaseTest {
     final static String BASE_URL = "https://openweathermap.org/";
+    final static By GUIDE_TAB = By.xpath("//div[@id = 'desktop-menu']//a[@href = '/guide']");
+    final static By LOGO = By.className("logo");
+
     private void openBaseUrl() {
         getDriver().get(BASE_URL);
     }
@@ -19,6 +24,10 @@ public class KathyGuTest extends BaseTest {
     private String getTitle() {
         return getDriver().getTitle();
     }
+    private void click (By by, WebDriverWait wait) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
+    }
     @Test
 
     public void testStartPageTitleAndURL_WhenOpenBaseUrl() {
@@ -31,7 +40,25 @@ public class KathyGuTest extends BaseTest {
         Assert.assertEquals(getTitle(), expectedTitle);
         Assert.assertEquals(getCurrentUrl(), expectedUrl);
     }
+    @Test
+
+    public void testClickOnLogoRedirectUserToStartPage() {
+        String startPage = "https://openweathermap.org/";
+
+        openBaseUrl();
+        waitForGreyFrameDisappeared();
+        click(GUIDE_TAB, getWait5());
+        click(LOGO, getWait5());
+
+        Assert.assertEquals(getCurrentUrl(),startPage);
+    }
 }
+
+
+
+
+
+
 
 
 
