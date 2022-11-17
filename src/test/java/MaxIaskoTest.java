6d6d67fe57c87bcc9458605ad8a6e6c5ad45d4dc
Max Iasko <115347581+maksymiasko@users.noreply.github.com>
2022-11-17 17:54:09 +0000

    Refactoring testSearch30OrangeButtons() (#680)
    Rest methods deleted
    getWait() added

--- src/test/java/MaxIaskoTest.java
+++ src/test/java/MaxIaskoTest.java
@@ -1,78 +1,42 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-@Ignore
 public class MaxIaskoTest extends BaseTest {
+    final static String BASE_URL = "https://openweathermap.org/";
+    final static By SEARCH_API_PAGE = By.xpath("//div[@id = 'desktop-menu']//a[text()= 'API']");
+    final static By SEARCH_ORANGE_ELEMENT = By.xpath("//a[contains(@class, 'btn_block orange round') " +
+            "or contains(@class, 'ow-btn round btn-orange')]");
+
+    private void click(By by, WebDriverWait wait) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
+        getDriver().findElement(by).click();
+    }
+
+    private void waitGrayFrameDisappear() {
+        getWait20().until(ExpectedConditions.invisibilityOfElementLocated(
+                By.className("owm-loader-container")));
+    }
+
+    private int size(By by, WebDriverWait wait) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        return getDriver().findElements(by).size();
+    }
 
     @Test
-    public void testSearch30OrangeButtons() throws InterruptedException {
-
-        final String url = "https://openweathermap.org/";
+    public void testSearch30OrangeButtons() {
         final int expectedResult = 30;
-        getDriver().get(url);
 
-        Thread.sleep(10000);
-        WebElement searchApiPage = getDriver().findElement(
-                By.xpath("//div[@id = 'desktop-menu']//a[text()= 'API']"));
-        searchApiPage.click();
+        getDriver().get(BASE_URL);
 
-        int actualResult = getDriver().findElements(
-                By.xpath("//a[contains(@class, 'btn_block orange round') " +
-                        "or contains(@class, 'ow-btn round btn-orange')]")).size();
+        waitGrayFrameDisappear();
+        click(SEARCH_API_PAGE, getWait5());
 
-        Assert.assertEquals(actualResult, expectedResult);
-    }
-
-    @Test
-    public void testConfirmLinkNoChangeByLogoClick() throws InterruptedException {
-
-        final String url = "https://openweathermap.org/";
-
-        getDriver().get(url);
-        Thread.sleep(10000);
-
-        WebElement searchLogoField = getDriver().findElement(By.xpath("//a[@href ='/']"));
-        searchLogoField.click();
-
-        String actualResult = getDriver().getCurrentUrl();
-        Assert.assertEquals(actualResult, url);
-    }
-
-    @Test
-    public void testConfirmTextInLink() throws InterruptedException {
-        final String url = "http://openweathermap.org/";
-        final String expectedResult0 = "Rome";
-        final String expectedResult1 = "find";
-
-        getDriver().get(url);
-        Thread.sleep(7000);
-
-        WebElement searchWhetherInYourCityField = getDriver().findElement(
-                By.xpath("//div[@id='desktop-menu']/form/input[@type ='text']"));
-        searchWhetherInYourCityField.click();
-        searchWhetherInYourCityField.sendKeys(expectedResult0);
-        searchWhetherInYourCityField.sendKeys(Keys.ENTER);
-
-        String currentUrl = getDriver().getCurrentUrl();
-        Assert.assertEquals(currentUrl.contains(expectedResult0), currentUrl.contains(expectedResult1));
-
-        String mainWindows = getDriver().getWindowHandle();
-        for (String windowsHandle : getDriver().getWindowHandles()) {
-            if (!mainWindows.contentEquals(windowsHandle)) {
-                getDriver().switchTo().window(windowsHandle);
-                break;
-            }
-        }
-        WebElement searchFieldText = getDriver().findElement(
-                By.xpath("//div/input[@id='search_str']"));
-
-        String actualResult = searchFieldText.getAttribute("value");
-        Assert.assertEquals(expectedResult0, actualResult);
+        Assert.assertEquals(size(SEARCH_ORANGE_ELEMENT, getWait5()), expectedResult);
     }
 }
 
