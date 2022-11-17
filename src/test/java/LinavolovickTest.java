5feaf9c3a9452c8fb6f893ec7b3bf59dadbf982e
Galina Sedunova <109212327+linavolovick@users.noreply.github.com>
2022-11-18 02:15:30 +0300

    testVerifyCurrentAndForecastAPIsLinkExist_etc() added (#702)
    Co-authored-by: Galina Sedunova <sgedunova@gmail.com>

--- src/test/java/LinavolovickTest.java
+++ src/test/java/LinavolovickTest.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,6 +11,7 @@ import runner.BaseTest;
 
 public class LinavolovickTest extends BaseTest {
     final static String BASE_URL = "https://openweathermap.org/";
+    final static String WEATHER_API_URL = "https://openweathermap.org/api#current";
 
     final static By H_2_CITY_COUNTRY_HEADER = By.xpath("//div[@id = 'weather-widget']//h2");
     final static By SEARCH_CITY_FIELD = By.xpath("//div[@id = 'weather-widget']//input[@placeholder = 'Search city']");
@@ -17,8 +19,9 @@ public class LinavolovickTest extends BaseTest {
     final static By SEARCH_DROPDOWN_MENU = By.className("search-dropdown-menu");
     final static By PARIS_FR_CHOICE_IN_DROPDOWN_MENU = By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Paris, FR ']");
     final static By LOGO = By.xpath("//li[@class='logo']/descendant::img");
+    final static By CURRENT_AND_FORECAST_APIS_LINK = By.xpath("//li/a[@href='/api#current']");
 
-    private void openBaseURL() {
+    private void openBaseURL(){
         getDriver().get(BASE_URL);
     }
 
@@ -60,6 +63,16 @@ public class LinavolovickTest extends BaseTest {
                 .not(ExpectedConditions.textToBePresentInElement(driver.findElement(by), text)));
     }
 
+    private void useJavascriptExecutorToPerfomClick() {
+        JavascriptExecutor executor = (JavascriptExecutor)getDriver();
+        executor.executeScript("arguments[0].click();", getDriver().findElement(CURRENT_AND_FORECAST_APIS_LINK));
+    }
+
+    private void scrollDown () {
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+    }
+
     @Test
     public void testH2TagText_WhenSearchingCityCountry() {
         String cityName = "Paris";
@@ -89,6 +102,17 @@ public class LinavolovickTest extends BaseTest {
         openBaseURL();
         waitForGreyFrameDisapearred();
         click(LOGO, getWait5());
+
+        Assert.assertEquals(getCurrentUrl(), expectedResult);
+    }
+
+    @Test
+    public void testVerifyCurrentAndForecastAPIsLinkExistsAndRedirectsToWeatherAPIPage() {
+        String expectedResult = WEATHER_API_URL;
+
+        openBaseURL();
+        waitForGreyFrameDisapearred();
+        useJavascriptExecutorToPerfomClick();
 
         Assert.assertEquals(getCurrentUrl(), expectedResult);
     }
