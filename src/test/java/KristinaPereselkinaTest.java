52aa6a545f375350c33f86747093e1fad003b385
Kristina Pereselkina <kristinapereselkina@gmail.com>
2022-11-17 10:07:28 +0100

    [AT][Start][NavBar][Menu][Pricing] testListFreeCollection<KristinaPereselkina> (#637)
    * [AT][Start][NavBar][Menu][Pricing] testListFreeCollection<KristinaPereselkina>
    
    * [RF][Start][NavBar][Menu][Pricing] testListFreeCollection<KristinaPereselkina>

--- src/test/java/KristinaPereselkinaTest.java
+++ src/test/java/KristinaPereselkinaTest.java
@@ -19,6 +19,49 @@ public class KristinaPereselkinaTest extends BaseTest {
     final static By H2 = By.xpath("//h2[text()='Professional collections']");
     final static By PROFFESSIONAL_COLLECTION_TABLE_HEAD = By.xpath("//h3/b");
     final static By H2_HEADER_NAME_PROFESSIONAL_COLLECTION = By.xpath("//h2[text()= 'Current weather and forecasts collection']");
+    final static By COLLECTION_PRODUCTS = By.xpath("//div/table//td[1]/p");
+
+    private List<String> getExpectedListProfessionalCollectionTableHead() {
+        List<String> expectedList = new ArrayList<>();
+        expectedList.add("Free");
+        expectedList.add("Startup");
+        expectedList.add("Developer");
+        expectedList.add("Professional");
+        expectedList.add("Enterprise");
+
+        return expectedList;
+    }
+
+    private List<String> getExpectedProductList() {
+        List<String> expectedList = new ArrayList<>();
+        expectedList.add("Current Weather");
+        expectedList.add("3-hour Forecast 5 days");
+        expectedList.add("Hourly Forecast 4 days");
+        expectedList.add("Daily Forecast 16 days");
+        expectedList.add("Climatic Forecast 30 days");
+        expectedList.add("Bulk Download");
+        expectedList.add("Basic weather maps");
+        expectedList.add("Historical maps");
+        expectedList.add("Global Precipitation Map - Historical data");
+        expectedList.add("Weather Dashboard");
+        expectedList.add("Road Risk API (basic configuration)");
+        expectedList.add("Air Pollution API");
+        expectedList.add("Geocoding API");
+        expectedList.add("Weather widgets");
+        expectedList.add("Uptime 95%");
+
+        return expectedList;
+    }
+
+    private List<String> getActualList(By locator) {
+        List<String> actualListItems = new ArrayList<>();
+        List<WebElement> listItemsInMenu = getDriver().findElements(locator);
+        for (WebElement items : listItemsInMenu) {
+            actualListItems.add(items.getText());
+        }
+
+        return actualListItems;
+    }
 
     private void openBaseURL() {
         getDriver().get(BASE_URL);
@@ -36,27 +79,6 @@ public class KristinaPereselkinaTest extends BaseTest {
     private String getText(By by, WebDriver driver) {
 
         return driver.findElement(by).getText();
-    }
-
-    private List<String> getExpectedListProfessionalCollectionTableHead() {
-        List<String> expectedList = new ArrayList<>();
-        expectedList.add("Free");
-        expectedList.add("Startup");
-        expectedList.add("Developer");
-        expectedList.add("Professional");
-        expectedList.add("Enterprise");
-
-        return expectedList;
-    }
-
-    private List<String> getActualListProfessionalCollectionTableHead(By locator) {
-        List<String> actualListItems = new ArrayList<>();
-        List<WebElement> listItemsInMenu = getDriver().findElements(locator);
-        for (WebElement items : listItemsInMenu) {
-            actualListItems.add(items.getText());
-        }
-
-        return actualListItems;
     }
 
 
@@ -97,7 +119,21 @@ public class KristinaPereselkinaTest extends BaseTest {
         waitForGrayFrameDisappeared();
         click(A_HREF_PRICE, getWait5());
 
-        List<String> actualList = getActualListProfessionalCollectionTableHead(PROFFESSIONAL_COLLECTION_TABLE_HEAD);
+        List<String> actualList = getActualList(PROFFESSIONAL_COLLECTION_TABLE_HEAD);
+
+        Assert.assertEquals(actualList, expectedList);
+    }
+
+    @Test
+    public void testListOfProductsForProfessionalCollection() {
+
+        List<String> expectedList = getExpectedProductList();
+
+        openBaseURL();
+        waitForGrayFrameDisappeared();
+        click(A_HREF_PRICE, getWait5());
+
+        List<String> actualList = getActualList(COLLECTION_PRODUCTS);
 
         Assert.assertEquals(actualList, expectedList);
     }
