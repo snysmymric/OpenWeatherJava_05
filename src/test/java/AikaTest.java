465359b446245242c36f9fd40a717c6ccdaaca7b
a20l <aidaytyn@gmail.com>
2022-11-16 23:30:24 -0600

    refactored code by adding and removing extra spaces, removed exception

--- src/test/java/AikaTest.java
+++ src/test/java/AikaTest.java
@@ -14,38 +14,39 @@ public class AikaTest extends BaseTest {
     final static By SUPPORT_MENU = By.id("support-dropdown");
     final static By SUPPORT_DROPDOWN = By.xpath("//ul[@id='support-dropdown-menu']");
 
-    private void openBaseURL(){
+    private void openBaseURL() {
         getDriver().get(BASE_URL);
     }
 
-    private void waitForGrayFrameDisappeared(){
+    private void waitForGrayFrameDisappeared() {
         getWait20().until(ExpectedConditions.invisibilityOfElementLocated(
                 By.className("owm-loader-container")));
     }
 
-    private void click(By by, WebDriver driver){
+    private void click(By by, WebDriver driver) {
         driver.findElement(by).click();
     }
 
     @Test
-    public void testSupportMenuAndDropdownThreeSubmenuExist() throws InterruptedException {
+    public void testSupportMenuAndDropdownThreeSubmenuExist() {
         String dropdownVisibility = "dropdown-visible";
         int expectedDropdownSize = 3;
 
         openBaseURL();
         waitForGrayFrameDisappeared();
-
         click(SUPPORT_MENU, getDriver());
+
         Assert.assertTrue(getDriver().findElement(SUPPORT_DROPDOWN)
                 .getAttribute("class").contains(dropdownVisibility));
 
         List<WebElement> dropdownSize = getDriver().findElements(
                 By.xpath("//ul[@id='support-dropdown-menu']/li"));
+
         Assert.assertEquals(dropdownSize.size(), expectedDropdownSize);
 
         click(SUPPORT_MENU, getDriver());
+
         Assert.assertFalse(getDriver().findElement(SUPPORT_DROPDOWN)
                 .getAttribute("class").contains(dropdownVisibility));
-
     }
 }
