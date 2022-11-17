dff8f6c29628ae032391339be8e15c72f49d5270
YuliaMatusevich <115679291+YuliaMatusevich@users.noreply.github.com>
2022-11-16 22:14:31 -0600

    testAllSocialMediaIconsVisibleAndClickable added (#655)

--- src/test/java/YuliaMatusevichTest.java
+++ src/test/java/YuliaMatusevichTest.java
@@ -1,7 +1,43 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import runner.BaseTest;
+
+import java.util.List;
+
 public class YuliaMatusevichTest extends BaseTest {
 
+    final static String BASE_URL = "https://openweathermap.org/";
 
+    public void openBaseUrl(){
+        getDriver().get(BASE_URL);
+    }
+    private void waitForGrayFrameDisappeared() {
+        getWait20().until(ExpectedConditions.invisibilityOfElementLocated(
+                By.className("owm-loader-container")));
+    }
+    public int findAllVisibleElements(String xpath, WebDriverWait wait) {
+        wait.until(ExpectedConditions.visibilityOfAllElements(getDriver().findElement(By.xpath(xpath))));
+        List<WebElement> elements = getDriver().findElements(By.xpath(xpath));
+        for (WebElement element : elements) {
+            wait.until(ExpectedConditions.elementToBeClickable(element));
+        }
+        return elements.size();
+    }
+    @Test
+    public void testAllSocialMediaIconsVisibleAndClickable (){
+        int expectedResult = 6;
+
+        openBaseUrl();
+        waitForGrayFrameDisappeared();
+
+        int actualResult = findAllVisibleElements("//div[@class = 'social']/a",getWait5());
+
+        Assert.assertEquals(actualResult,expectedResult);
+    }
 }
 
 
