c306e733b2f7f04e453ff26b5ebd53cfe291638c
LolitaMn <87144131+LolitaMn@users.noreply.github.com>
2022-11-18 02:35:31 +0400

    testCheckTextInTheBottomOfThePage() refactored (#687)

--- src/test/java/LolitaMnTest.java
+++ src/test/java/LolitaMnTest.java
@@ -1,49 +1,62 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-@Ignore
+
 public class LolitaMnTest extends BaseTest {
+
+    final static String URL = "https://openweathermap.org/";
+
+    final static By FOOTER_PANEL_CONTAINER = By.xpath("//div[@class='stick-footer-panel__container']");
+    final static By BOTTOM_PANEL = By.xpath("//div[@class='stick-footer-panel__container']");
+    final static By TEXT_PANEL =  By.xpath("//p[@class='stick-footer-panel__description']");
+    final static By ALLOW_ALL_BUTTON = By.xpath("//button[@type='button' and text()='Allow all']");
+    final static By MANAGE_BUTTON = By.xpath("//a[@href='/cookies-settings' and text()=' Manage cookies ']");
+
+
+    private void openBaseURL() {
+        getDriver().get(URL);
+
+    }
+
+    private void waitForGrayFrameDisappeared() {
+        getWait20().until(ExpectedConditions.invisibilityOfElementLocated(By.className("owm-loader-container")));
+    }
+
+    private void waitForElementGetVisible(By by, WebDriverWait wait) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+
+    private String getText(By by, WebDriver driver) {
+
+        return driver.findElement(by).getText();
+    }
+
+
 
 
     @Test
-    public void testCheckTextInTheBottomOfThePage() throws InterruptedException {
-        String url = "https://openweathermap.org/";
+    public void testCheckTextInTheBottomOfThePage() {
         String expectedResult = "We use cookies which are essential for the site to work. We also use non-essential "
                 + "cookies to help us improve our services. Any data collected is anonymised. You can allow all cookies"
                 + " or manage them individually.";
-        String allowAll = "Allow all";
-        String manageCookies = " Manage cookies ";
 
-        getDriver().get(url);
-        Thread.sleep(10000);
 
-        WebElement bottomPanel = getDriver().findElement(
-                By.xpath("//div[@class='stick-footer-panel__container']")
-        );
+        openBaseURL();
+        waitForGrayFrameDisappeared();
+        waitForElementGetVisible(FOOTER_PANEL_CONTAINER, getWait5());
+        waitForElementGetVisible(BOTTOM_PANEL, getWait5());
+        waitForElementGetVisible(ALLOW_ALL_BUTTON, getWait5());
+        waitForElementGetVisible(MANAGE_BUTTON, getWait5());
 
-        WebElement textPanel = getDriver().findElement(
-                By.xpath("//p[@class='stick-footer-panel__description']")
-        );
+        String actualResultTextPanel = getText(TEXT_PANEL, getDriver());
 
-        String actualResultTextPanel = textPanel.getText();
-
-        WebElement allowAllButton = getDriver().findElement(
-                By.xpath("//button[@type='button' and text()='" + allowAll + "']")
-        );
-
-        WebElement manageButton = getDriver().findElement(
-                By.xpath("//a[@href='/cookies-settings' and text()='" + manageCookies + "']")
-        );
-        Assert.assertTrue(bottomPanel.isDisplayed());
-        Assert.assertTrue(allowAllButton.isDisplayed());
-        Assert.assertTrue(manageButton.isDisplayed());
         Assert.assertEquals(actualResultTextPanel, expectedResult);
 
     }
