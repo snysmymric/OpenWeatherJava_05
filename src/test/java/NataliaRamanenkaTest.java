82958fbc76a36fd27a367561be2da8b9582cb3d3
NataliaRamanenka <4yzheuskaya@gmail.com>
2022-11-17 21:34:58 +0300

    testAboutUsVisibleAndClickableIsAdded (#678)

--- src/test/java/NataliaRamanenkaTest.java
+++ src/test/java/NataliaRamanenkaTest.java
@@ -1,13 +1,12 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
-import java.util.List;
+import java.util.ArrayList;
 
 public class NataliaRamanenkaTest extends BaseTest {
     final static String BASE_URL = "https://openweathermap.org/";
@@ -15,6 +14,7 @@ public class NataliaRamanenkaTest extends BaseTest {
             "//div[@id = 'desktop-menu']/ul/li/a[@href = '/guide']");
     final static By SEARCH_HEAD_OF_PAGE_GUIDE = By.xpath(
             "//div[@class='col-sm-7']/h1");
+    final static By ABOUT_US = By.xpath("//a[@href='/about-us']");
 
     private void openBaseURL() {
         getDriver().get(BASE_URL);
@@ -32,7 +32,11 @@ public class NataliaRamanenkaTest extends BaseTest {
     private void waitElementToBeVisible(By by, WebDriverWait wait) {
         wait.until(ExpectedConditions.visibilityOfElementLocated(by));
     }
-
+    public void scrollToElement(By by){
+        WebElement Element = getDriver().findElement(by);
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
+        js.executeScript("arguments[0].scrollIntoView();", Element);
+    }
     private String actualUrl() {
 
         return getDriver().getCurrentUrl();
@@ -44,7 +48,7 @@ public class NataliaRamanenkaTest extends BaseTest {
     }
 
     @Test
-    public void testGuideLinkAndTitle() throws InterruptedException {
+    public void testGuideLinkAndTitle()  {
         String expectedResultUrl = "https://openweathermap.org/guide";
         String expectedResultTitle = "OpenWeatherMap API guide - OpenWeatherMap";
 
@@ -56,6 +60,27 @@ public class NataliaRamanenkaTest extends BaseTest {
 
         Assert.assertEquals(actualUrl(), expectedResultUrl);
         Assert.assertEquals(actualTitle(), expectedResultTitle);
+    }
+    @Test
+    public void testAboutUsVisibleAndClickable(){
+        String expectedResult = "https://openweathermap.org/about-us";
+
+        openBaseURL();
+        waitForGrayFrameDisappeared();
+
+        scrollToElement(ABOUT_US);
+        click(ABOUT_US, getWait5());
+
+        String originalWindow = getDriver().getWindowHandle();
+        ArrayList tabs = new ArrayList(getDriver().getWindowHandles());
+        for(int i = 1; i < tabs.size(); i++) {
+            if (!tabs.get(i).equals(originalWindow)) {
+                getDriver().switchTo().window((String) tabs.get(i));
+            }
+        }
+        String actualResult = actualUrl();
+
+        Assert.assertEquals(actualResult,expectedResult);
     }
 
 }
