f4abde9a2fff7ad7befc8c1b2f4a6e2787704165
Kater1naG <117737989+Kater1naG@users.noreply.github.com>
2022-11-16 23:00:23 -0500

    updated code (#653)

--- src/test/java/Kater1naGTest.java
+++ src/test/java/Kater1naGTest.java
@@ -1,51 +1,67 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-@Ignore
+
 public class Kater1naGTest extends BaseTest {
+    final static String BASE_URL = "https://openweathermap.org/";
+    final static By H2_CITY_COUNTRY_HEADER = By.xpath("//div[@id = 'weather-widget']//h2");
+    final static By SEARCH_CITY_FIELD = By.xpath("//input[@placeholder = 'Search city']");
+    final static By SEARCH_BUTTON = By.xpath("//button[@type = 'submit']");
+    final static By SEARCH_DROPDOWN_MENU = By.className("search-dropdown-menu");
+    final static By CHOICE_PARIS_FR = By.xpath
+            ("//ul[@class = 'search-dropdown-menu']/li/span[text()= 'Paris, FR ']");
+    private void openBaseURL(){
+        getDriver().get(BASE_URL);
+    }
+    private void waitForGreyFrameDisappeared(){
+        getWait20().until(ExpectedConditions.invisibilityOfElementLocated(
+                By.className("owm-loader-container")));
+
+    }
+    private String getText(By by,WebDriver driver){
+        return driver.findElement(by).getText();
+    }
+    private void click(By by, WebDriverWait wait){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
+    }
+    private void input(String text,By by,WebDriver driver){
+        driver.findElement(by).sendKeys(text);
+    }
+    private void waitElementToBeVisible(By by,WebDriverWait wait){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+    private void waitTextChanged(By by, String text, WebDriver driver, WebDriverWait wait){
+        wait.until(ExpectedConditions
+                .not(ExpectedConditions.textToBePresentInElement(driver.findElement(by), text)));
+    }
 
     @Test
-    public void testH2TagText_WhenSearchingCityCountry () throws InterruptedException {
-
-        String url = "https://openweathermap.org/";
+    public void testH2TagText_WhenSearchingCityCountry() {
         String cityName = "Paris";
         String expectedResult = "Paris, FR";
 
-        getDriver().get(url);
-
-        WebElement searchCityField = getDriver().findElement(
-                By.xpath("//input[@placeholder = 'Search city']")
-        );
-        Thread.sleep(10000);
-        searchCityField.click();
-        searchCityField.sendKeys(cityName);
-
-        WebElement searchButton = getDriver().findElement(
-                By.xpath("//button[@type = 'submit']")
-        );
-        searchButton.click();
-        Thread.sleep(5000);
-
-        WebElement choiceParisFr = getDriver().findElement(
-                By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text()= 'Paris, FR ']")
-        );
-        choiceParisFr.click();
-
-        WebElement h2CityCountryHeader = getDriver().findElement(
-                By.xpath("//div[@id = 'weather-widget']//h2")
-        );
-        Thread.sleep(5000);
-        String actualResult = h2CityCountryHeader.getText();
+        openBaseURL();
+        waitForGreyFrameDisappeared();
+        String oldH2Header = getText(H2_CITY_COUNTRY_HEADER, getDriver());
+        click(SEARCH_CITY_FIELD, getWait5());
+        input(cityName,SEARCH_CITY_FIELD,getDriver());
+        click(SEARCH_BUTTON, getWait5());
+        waitElementToBeVisible(SEARCH_DROPDOWN_MENU,getWait10());
+        click(CHOICE_PARIS_FR, getWait10());
+        waitTextChanged(H2_CITY_COUNTRY_HEADER, oldH2Header, getDriver(),getWait10());
+        String actualResult = getText(H2_CITY_COUNTRY_HEADER, getDriver());
 
         Assert.assertEquals(actualResult, expectedResult);
     }
-
+@Ignore
     @Test
     public void testRedirectToGuide() throws InterruptedException {
 
@@ -69,6 +85,7 @@ public class Kater1naGTest extends BaseTest {
 
         Assert.assertEquals(actualResult, expectedResult);
     }
+    @Ignore
     @Test
     public void testImperialF() throws InterruptedException {
 
@@ -101,7 +118,7 @@ public class Kater1naGTest extends BaseTest {
 
         Assert.assertEquals(actualResult, expectedResult);
     }
-
+@Ignore
     @Test
     public void testBelowButtonsAndText() throws InterruptedException {
 
