71098eeb7d5380e0c41be5e312cd1e1049a1864f
Roman T <93357374+Rom-T@users.noreply.github.com>
2022-11-18 08:36:41 -0500

    refactoring testClickabilityOfOneCallAPILink() (#698)

--- src/test/java/RomanTest.java
+++ src/test/java/RomanTest.java
@@ -1,37 +1,48 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-import java.time.Duration;
 
-@Ignore
 public class RomanTest extends BaseTest {
+
+    private static final String BASE_URL = "https://openweathermap.org/";
+
+    private static final By ONE_CALL_API_LINK = By.xpath("//div[@class='section']//h2/a[@href='/api/one-call-3']");
+    private static final By H1_MAIN = By.xpath("//main//h1");
+
+    private void openBaseURL() {
+        getDriver().get(BASE_URL);
+    }
+
+    private void waitForGrayFrameDisappeared() {
+        getWait20().until(ExpectedConditions.invisibilityOfElementLocated(
+                By.className("owm-loader-container")));
+    }
+
+    public void scrollToPage(){
+        JavascriptExecutor executor = (JavascriptExecutor)getDriver();
+        executor.executeScript("window.scrollBy(0,500)", "");
+    }
+
+    private String getText(By by) {
+
+        return getDriver().findElement(by).getText();
+    }
 
     @Test
     public void testClickabilityOfOneCallAPILink() {
+        final String expectedResult = "One Call API 3.0";
 
-        String url = "https://openweathermap.org/";
-        String expectedResult = "One Call API 3.0";
+        openBaseURL();
+        waitForGrayFrameDisappeared();
 
-        getDriver().get(url);
+        scrollToPage();
+        getDriver().findElement(ONE_CALL_API_LINK).click();
 
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(30));
-        wait.until(ExpectedConditions.presenceOfElementLocated(
-                By.xpath("//div[@class='section']//h2/a[@href='/api/one-call-3']")));
-
-        WebElement linkOneCall = getDriver().findElement(
-                By.xpath("//div[@class='section']//h2/a[@href='/api/one-call-3']"));
-        JavascriptExecutor executor = (JavascriptExecutor)getDriver();
-        executor.executeScript("arguments[0].click();", linkOneCall);
-
-        String actualResult = getDriver().findElement(
-                By.xpath("//div[@class='topic']//li[@class='breadcrumb__leaf']")).getText();
+        String actualResult = getText(H1_MAIN);
 
         Assert.assertEquals(actualResult, expectedResult);
     }
