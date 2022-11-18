7c0d955cbc5a9f0cf27bee6ab85d9f1f1ada6b23
Liudmila Plucci <112190395+LiudmilaPlucci@users.noreply.github.com>
2022-11-18 23:16:47 +0000

    RFDelete_OldTest_LiudmilaPlucci (#763)
    Co-authored-by: Liudmila Plucci <112190395+liuda33@users.noreply.github.com>

--- src/test/java/LiudmilaPlucciTest.java
+++ src/test/java/LiudmilaPlucciTest.java
@@ -1,11 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
@@ -71,72 +68,6 @@ public class LiudmilaPlucciTest extends BaseTest {
 
         Assert.assertEquals(actualResult,expectedResult);
 
-    }
-    @Ignore
-    @Test
-    public void testApproveGuidePage() throws InterruptedException {
-        String url = "https://openweathermap.org/";
-        String expectedResult1 = "https://openweathermap.org/guide";
-        String expectedResult2 = "OpenWeatherMap API guide - OpenWeatherMap";
-
-        getDriver().get(url);
-        Thread.sleep(10000);
-
-        WebElement ButtonGuide = getDriver().findElement(
-                By.xpath("//a[@href='/guide']"));
-
-        ButtonGuide.click();
-
-        String actualResult2 = getDriver().getTitle();
-        String actualResult1 = getDriver().getCurrentUrl();
-
-        Assert.assertEquals(actualResult1,expectedResult1);
-        Assert.assertEquals(actualResult2,expectedResult2);
-    }
-    @Ignore
-    @Test
-    public void testApproveTwoButtonsInPanel() throws InterruptedException {
-        String url = "https://openweathermap.org/";
-        String expectedResult = "We use cookies which are essential for the site to work. We also use non-essential cookies to help us improve our services. Any data collected is anonymised. You can allow all cookies or manage them individually.";
-        getDriver().get(url);
-
-        Thread.sleep(10000);
-        WebElement textElement = getDriver().findElement(
-                By.className("stick-footer-panel__description"));
-
-        WebElement buttonAllowAll = getDriver().findElement(By.xpath("//button[text()='Allow all']"));
-        WebElement buttonManageCookies = getDriver().findElement(
-                By.xpath("//a[@href='/cookies-settings']"));
-
-        Assert.assertEquals(buttonAllowAll.getText(),"Allow all");
-        Assert.assertEquals(buttonManageCookies.getText(),"Manage cookies");
-        Assert.assertEquals(textElement.getText(),expectedResult);
-    }
-    @Ignore
-
-    @Test
-    public void testApproveMenuSupportWithThreeSubMenu() throws InterruptedException {
-        String url = "https://openweathermap.org/";
-        String expectedResultFAQ = "FAQ";
-        String expectedResultHowToStart = "How to start";
-        String expectedResultAskAQuestion = "Ask a question";
-
-        getDriver().get(url);
-        Thread.sleep(7000);
-        WebElement supportDropdown = getDriver().findElement(By.xpath("//div[@id='support-dropdown']"));
-        supportDropdown.click();
-        Thread.sleep(3000);
-
-        WebElement checkIfTextFAQIsPresent = getDriver().findElement(
-                By.xpath("//ul[@id='support-dropdown-menu']//a[@href='/faq']"));
-        WebElement checkIfTextHowToStartIsPresent = getDriver().findElement(
-                By.xpath("//ul[@id='support-dropdown-menu']//a[@href='/appid']"));
-        WebElement checkIfTextAskAQuestionIsPresent = getDriver().findElement(By.xpath(
-                "//ul[@id='support-dropdown-menu']//a[@href='https://home.openweathermap.org/questions']"));
-
-        Assert.assertEquals(checkIfTextFAQIsPresent.getText(), expectedResultFAQ);
-        Assert.assertEquals(checkIfTextHowToStartIsPresent.getText(), expectedResultHowToStart);
-        Assert.assertEquals(checkIfTextAskAQuestionIsPresent.getText(), expectedResultAskAQuestion);
     }
     @Test
     public void test_VerifyThePagePricingOpened_WhenClickPricingBtnAtMenu_BreadcrumbsPricing() {
