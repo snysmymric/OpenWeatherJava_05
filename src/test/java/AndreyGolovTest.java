33ed04b40df94451cf0e92f0aec0f8620fcddeee
AndreyGolov <andrey.golov@yahoo.com>
2022-11-18 16:00:54 +0300

    testDifferentWeatherIconsOnPopUp added (#729)

--- src/test/java/AndreyGolovTest.java
+++ src/test/java/AndreyGolovTest.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -11,6 +12,8 @@ public class AndreyGolovTest extends BaseTest {
     final static String BASE_URL = "https://openweathermap.org/";
     final static By DIFFERENT_WEATHER_BUTTON = By.xpath("//div[@class = 'controls']//span");
     final static By BUTTON_X_ON_POPUP = By.xpath("//div[@class = 'pop-up-container']//*[@class='icon-close']");
+    final static By DIFFERENT_WEATHER_ICONS_CONTAINER = By.xpath("//div[@class = 'pop-up-container']//*[@class='icons']//li");
+    final static By WEATHER_ICON_IN_POP_UP = By.xpath("//div[@class = 'pop-up-container']//ul[@class = 'icons']//li[1]");
 
     private void waitForGrayFrameDisappeared() {
         getWait20().until(ExpectedConditions.invisibilityOfElementLocated(
@@ -26,6 +29,21 @@ public class AndreyGolovTest extends BaseTest {
         wait.until(ExpectedConditions.elementToBeClickable(by)).click();
     }
 
+    private By getByWithNumber(By by, int numberInSequence) {
+        String NumberInSequence = String.valueOf(numberInSequence);
+        By newBy = By.xpath(by.toString().substring(10, by.toString().length() - 2) + NumberInSequence + "]");
+        return newBy;
+    }
+
+    private String clickGetAttributeValue(WebDriver driver, By by, String nameOfAttribute, WebDriverWait wait) {
+        driver.findElement(by).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
+        WebElement iconInActiveCondition = getDriver().findElement(by);
+
+        return iconInActiveCondition.getAttribute(nameOfAttribute);
+    }
+
     @Test
     public void testXButtonIsDisplayed() {
         boolean expectedCondition = true;
@@ -36,5 +54,65 @@ public class AndreyGolovTest extends BaseTest {
         WebElement buttonX = getDriver().findElement(BUTTON_X_ON_POPUP);
 
         Assert.assertEquals(buttonX.isDisplayed(), expectedCondition);
+    }
+    @Test
+    public void testDifferentWeatherIconsOnPopUp() {
+
+        String attributeTested = "class";
+        String expectedValueOfAttribute = "activeIcon";
+
+        openBaseURL();
+        waitForGrayFrameDisappeared();
+        click(DIFFERENT_WEATHER_BUTTON, getWait10());
+
+        Assert.assertEquals(
+                getDriver().findElements(DIFFERENT_WEATHER_ICONS_CONTAINER)
+                        .size(), 9
+        );
+
+        Assert.assertEquals(
+                clickGetAttributeValue(getDriver(), getByWithNumber(WEATHER_ICON_IN_POP_UP, 1), attributeTested, getWait5()),
+                expectedValueOfAttribute
+        );
+
+        Assert.assertEquals(
+                clickGetAttributeValue(getDriver(), getByWithNumber(WEATHER_ICON_IN_POP_UP, 2), attributeTested, getWait5()),
+                expectedValueOfAttribute
+        );
+
+        Assert.assertEquals(
+                clickGetAttributeValue(getDriver(), getByWithNumber(WEATHER_ICON_IN_POP_UP, 3), attributeTested, getWait5()),
+                expectedValueOfAttribute
+        );
+
+        Assert.assertEquals(
+                clickGetAttributeValue(getDriver(), getByWithNumber(WEATHER_ICON_IN_POP_UP, 4), attributeTested, getWait5()),
+                expectedValueOfAttribute
+        );
+
+        Assert.assertEquals(
+                clickGetAttributeValue(getDriver(), getByWithNumber(WEATHER_ICON_IN_POP_UP, 5), attributeTested, getWait5()),
+                expectedValueOfAttribute
+        );
+
+        Assert.assertEquals(
+                clickGetAttributeValue(getDriver(), getByWithNumber(WEATHER_ICON_IN_POP_UP, 6), attributeTested, getWait5()),
+                expectedValueOfAttribute
+        );
+
+        Assert.assertEquals(
+                clickGetAttributeValue(getDriver(), getByWithNumber(WEATHER_ICON_IN_POP_UP, 7), attributeTested, getWait5()),
+                expectedValueOfAttribute
+        );
+
+        Assert.assertEquals(
+                clickGetAttributeValue(getDriver(), getByWithNumber(WEATHER_ICON_IN_POP_UP, 8), attributeTested, getWait5()),
+                expectedValueOfAttribute
+        );
+
+        Assert.assertEquals(
+                clickGetAttributeValue(getDriver(), getByWithNumber(WEATHER_ICON_IN_POP_UP, 9), attributeTested, getWait5()),
+                expectedValueOfAttribute
+        );
     }
 }
