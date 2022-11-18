83620685ed67c914482f724d8e34d79f4973b178
OlegCernis <115178187+OlegCernis@users.noreply.github.com>
2022-11-18 11:19:04 +0200

    testVerifyChangingTempUnitInHeading_WhenSwitchTempUnitButton() was added (#710)
    Was Verify Changing Temp Unit In Heading When Switch TempUnitButton.

--- src/test/java/OlegCernisTest.java
+++ src/test/java/OlegCernisTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
@@ -15,16 +16,21 @@ public class OlegCernisTest extends BaseTest {
     final static By SEARCH_DROPDOWN_MENU = By.className("search-dropdown-menu");
     final static By MOLDOVA_MD_CHOICE_IN_DROPDOWN_MENU =
             By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Chisinau, MD ']");
+    final static By fahrenheit = By.xpath("//div[text()= 'Imperial: °F, mph']");
+    final static By metric = By.xpath("//div[text()= 'Metric: °C, m/s']");
+    final static By confirmTemp = By.xpath("//div[@class = 'current-temp']/span");
+    final static String imperialTemp = "Imperial: °F, mph";
+    final static String metricTemp = "Metric: °C, m/s";
+    final static String imperialSymbol ="°F";
+    final static String metricSymbol ="°C";
 
     private void openBaseURL() {
         getDriver().get(BASE_URL);
     }
-
     private void waitForGrayFrameDisappeared() {
         getWait20().until(ExpectedConditions.invisibilityOfElementLocated(
                 By.className("owm-loader-container")));
     }
-
     private String getText(By by, WebDriver driver) {
 
         return driver.findElement(by).getText();
@@ -34,7 +40,6 @@ public class OlegCernisTest extends BaseTest {
         wait.until(ExpectedConditions.visibilityOfElementLocated(by));
         wait.until(ExpectedConditions.elementToBeClickable(by)).click();
     }
-
     private void input(String text, By by, WebDriver driver) {
         driver.findElement(by).sendKeys(text);
     }
@@ -42,12 +47,16 @@ public class OlegCernisTest extends BaseTest {
     private void waitElementToBeVisible(By by, WebDriverWait wait) {
         wait.until(ExpectedConditions.visibilityOfElementLocated(by));
     }
-
     private void waitTextToBeChanged(By by, String text, WebDriver driver, WebDriverWait wait) {
         wait.until(ExpectedConditions
                 .not(ExpectedConditions.textToBePresentInElement(driver.findElement(by), text)));
     }
+    private boolean compareSymbol(WebDriver driver, String temp, String symbolTemp) {
+        click(By.xpath(String.format("//div[text()='%s']", temp)), getWait5());
+//        waitForGrayFrameDisappeared();
 
+        return getText(confirmTemp, getDriver()).contains(symbolTemp);
+    }
     @Test
     public void testVerifyThatLocationIsChangedInCurrentWeatherBlockAfterInputNewCity() {
 
@@ -66,7 +75,39 @@ public class OlegCernisTest extends BaseTest {
         waitTextToBeChanged(H2_CITY_COUNTRY_HEADER, oldH2Header, getDriver(), getWait10());
 
         Assert.assertEquals(expectedResult, newCity);
-
     }
+    @Test
+    public void testVerifyChangingTempUnitInHeading_WhenSwitchTempUnitButton() {
 
+        String expectedResult1 = "°F";
+
+        openBaseURL();
+        getDriver().manage().window().maximize();
+        waitForGrayFrameDisappeared();
+        click(fahrenheit, getWait5());
+
+        String fahrenheitConfirm = getText(confirmTemp, getDriver());
+
+        String actualResult = fahrenheitConfirm.substring((fahrenheitConfirm.length() - 2));
+        Assert.assertEquals(actualResult, expectedResult1);
+        compareSymbol(getDriver(), imperialTemp, imperialSymbol );
+    }
+    @Ignore
+    @Test
+    public void testVerifyMetricSymbolIsShownInCurrentTempWhenChangingUnitToMetric() {
+
+        String expectedResult2 = "°C";
+
+        openBaseURL();
+        getDriver().manage().window().maximize();
+        waitForGrayFrameDisappeared();
+
+        click(metric, getWait5());
+
+        String metricConfirm = getText(confirmTemp,getDriver());
+
+        String actualResult2 = metricConfirm.substring(metricConfirm.length() -2);
+        Assert.assertEquals(actualResult2, expectedResult2);
+        compareSymbol(getDriver(), metricTemp, metricSymbol);
+    }
 }
