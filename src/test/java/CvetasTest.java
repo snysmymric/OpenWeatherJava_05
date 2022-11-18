e1b4526b94e9ae7fd2a3660ed23bf8b9cf138578
Светлана Тишина <tisch1na.s@yandex.ru>
2022-11-18 14:00:16 +0300

    RFStartWeatherCurrentWeatherh2_testH2TagText_WhenSearchingCityCountry_Cvetas (#726)

--- src/test/java/CvetasTest.java
+++ src/test/java/CvetasTest.java
@@ -5,42 +5,8 @@ import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-@Ignore
+
 public class CvetasTest extends BaseTest {
 
-    @Test
-    public void testH2TegText_WhenSearchingCityCountry() throws InterruptedException {
-        String url = "https://openweathermap.org/";
-        String cityName = "Paris";
-        String expectedResult = "Paris, FR";
 
-        getDriver().get(url);
-        Thread.sleep(10000);
-
-        WebElement searchCityField = getDriver().findElement(
-                By.xpath("//div[@id='weather-widget']//input[@placeholder='Search city']")
-        );
-
-        searchCityField.click();
-        searchCityField.sendKeys(cityName);
-
-        WebElement searchButton = getDriver().findElement(
-                By.xpath("//div[@id='weather-widget']//button[@type='submit']")
-        );
-        searchButton.click();
-        Thread.sleep(1000);
-
-        WebElement parisFRChoiceInDropdownMenu = getDriver().findElement(
-                By.xpath("//ul[@class=\"search-dropdown-menu\"]//li//span[text() = 'Paris, FR ']")
-        );
-        parisFRChoiceInDropdownMenu.click();
-
-        WebElement h2CityCountryHeader = getDriver().findElement(
-                By.xpath("//div[@id='weather-widget']//h2")
-        );
-        Thread.sleep(2000);
-        String actualResult = h2CityCountryHeader.getText();
-
-        Assert.assertEquals(actualResult,expectedResult);
-    }
 }
