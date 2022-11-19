7fff1cae92d62cd13f24cca765d2672ab3d79b90
ElenaHouston <115054183+ElenaHouston@users.noreply.github.com>
2022-11-19 11:56:51 -0600

    [Start][Weather][CurrentWeather][h2]testConfirmTheLinkAndTitleOnThePage_OpenWeatherMap <ElenaHouston> (#799)
    * added
    
    * testConfirmTheLinkAndTitleOnThePage_OpenWeatherMap changed

--- src/test/java/ElenaHoustonTest.java
+++ src/test/java/ElenaHoustonTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
@@ -14,29 +15,40 @@ public class ElenaHoustonTest extends BaseTest {
     final static By SEARCH_DROPDOWN_MENU = By.className("search-dropdown-menu");
     final static By PARIS_CHOICE_IN_DROP_DOWN_MENU = By.xpath(
             "//ul[@class ='search-dropdown-menu']/li/span[text() = 'Paris, FR ']");
-    private void openBaseURL(){
+    final static By GUIDE_MENU = By.xpath("//div[@id = 'desktop-menu']//a[@href = '/guide']");
+
+    private void openBaseURL() {
         getDriver().get(Base_URL);
     }
-    private void waitForGrayFrameDisappeared(){
+    private void waitForGrayFrameDisappeared() {
         getWait20().until(ExpectedConditions.invisibilityOfElementLocated(By.className("owm-loader-container")));
     }
-    private String getText(By by, WebDriver driver){
+
+    private String getText(By by, WebDriver driver) {
         return driver.findElement(by).getText();
     }
-    private void click(By by, WebDriver driver){
-        getWait5().until(ExpectedConditions.visibilityOfElementLocated(by));
-        getWait5().until(ExpectedConditions.elementToBeClickable(by)).click();
+
+    private void click(By by, WebDriverWait wait) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
     }
-    private void input(String text, By by, WebDriver driver){
+    private void input(String text, By by, WebDriver driver) {
         driver.findElement(by).sendKeys(text);
     }
-    private void waitElementToBeVisible(By by, WebDriverWait wait){
+
+    private void waitElementToBeVisible(By by, WebDriverWait wait) {
         wait.until(ExpectedConditions.visibilityOfElementLocated(by));
     }
-    private void waitTextToBeChanged(By by, String text, WebDriver driver, WebDriverWait wait){
+
+    private void waitTextToBeChanged(By by, String text, WebDriver driver, WebDriverWait wait) {
         wait.until(ExpectedConditions
                 .not(ExpectedConditions.textToBePresentInElement(driver.findElement(by), text)));
     }
+
+    private void findGuideMenuByLink(String text){
+     getDriver().findElement(By.linkText(text));
+    }
+
     @Test
     public void testH2TagText_WhenSearchingCityCountry() {
         String cityName = "Paris";
@@ -47,40 +59,34 @@ public class ElenaHoustonTest extends BaseTest {
 
         String oldH2Header = getText(H2_CITY_COUNTRY_HEADER, getDriver());
 
-        click(SEARCH_CITY_FIELD, getDriver());
+        click(SEARCH_CITY_FIELD, getWait5());
         input(cityName, SEARCH_CITY_FIELD, getDriver());
-        click(SEARCH_BUTTON, getDriver());
+        click(SEARCH_BUTTON, getWait5());
         waitElementToBeVisible(SEARCH_DROPDOWN_MENU, getWait10());
-        click(PARIS_CHOICE_IN_DROP_DOWN_MENU, getDriver());
+        click(PARIS_CHOICE_IN_DROP_DOWN_MENU, getWait5());
         waitTextToBeChanged(H2_CITY_COUNTRY_HEADER, oldH2Header, getDriver(), getWait10());
         String actualResult = getText(H2_CITY_COUNTRY_HEADER, getDriver());
 
         Assert.assertEquals(actualResult, expectedResult);
     }
-}
-//
-//    @Test
-//    public void testConfirmTheLinkAndTitleOnThePage_OpenWeatherMap() throws InterruptedException {
-//        String url = "https://openweathermap.org/";
-//        String guideMenuText = "Guide";
-//        String expectedResult1 = "https://openweathermap.org/guide";
-//
-//        String expectedResult2 = "OpenWeatherMap API guide - OpenWeatherMap";
-//
-//        getDriver().get(url);
-//        Thread.sleep(10000);
-//
-//        WebElement guideMenu = getDriver().findElement(By.linkText(guideMenuText));
-//        guideMenu.click();
-//        Thread.sleep(2000);
-//
-//        String actualResult1 = getDriver().getCurrentUrl();
-//        String actualResult2 = getDriver().getTitle();
-//
-//        Assert.assertEquals(actualResult1, expectedResult1);
-//        Assert.assertEquals(actualResult2, expectedResult2);
-//    }
-//
+    @Test
+    public void testConfirmTheLinkAndTitleOnThePage_OpenWeatherMap() throws InterruptedException {
+        String expectedResult1 = "https://openweathermap.org/guide";
+
+        String expectedResult2 = "OpenWeatherMap API guide - OpenWeatherMap";
+
+        openBaseURL();
+        waitForGrayFrameDisappeared();
+
+       click(GUIDE_MENU, getWait5());
+
+        String actualResult1 = getDriver().getCurrentUrl();
+        String actualResult2 = getDriver().getTitle();
+
+        Assert.assertEquals(actualResult1, expectedResult1);
+        Assert.assertEquals(actualResult2, expectedResult2);
+    }
+//    @Ignore
 //    @Test
 //    public void testUnitsOfMeasurement_ImperialFMphConfirmFLondon() throws InterruptedException {
 //        String url = "https://openweathermap.org/";
@@ -99,11 +105,12 @@ public class ElenaHoustonTest extends BaseTest {
 //        WebElement tempInF = getDriver().findElement(By.xpath("//div[@class='current-temp']/span")
 //        );
 //        String tempF = tempInF.getText();
-//        String actualResult = tempF.substring((tempF.length()-2));
+//        String actualResult = tempF.substring((tempF.length() - 2));
 //
 //        Assert.assertTrue(tempInF.getText().contains(fTempSymbol));
 //        Assert.assertEquals(actualResult, expectedResult);
-//    }
+//}
+//    @Ignore
 //    @Test
 //    public void testCheckPresenceOfTreeSubMenusInTheHead () throws InterruptedException {
 //        String url = "https://openweathermap.org/";
@@ -142,4 +149,4 @@ public class ElenaHoustonTest extends BaseTest {
 //        Assert.assertEquals(actualResult2, expectedResult2);
 //        Assert.assertEquals(actualResult3, expectedResult3);
 //    }
-//}
+}
