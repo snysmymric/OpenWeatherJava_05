83d47732d91211f736f5d2540a9d8285d5163b0d
Yan Dadaelov <sirota92@bk.ru>
2022-11-19 23:45:09 +0300

    testStartPageUrl() test added (#800)

--- src/test/java/YanDadaelovTest.java
+++ src/test/java/YanDadaelovTest.java
@@ -1,10 +1,29 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
+
 public class YanDadaelovTest extends BaseTest {
 
+    final static String BASE_URL = "https://openweathermap.org/";
+
+
+    private void openBaseURL() {
+        getDriver().get(BASE_URL);
+    }
+
+    private String getCurrentUrl() {
+
+        return getDriver().getCurrentUrl();
+    }
+
+
+    @Test
+    public void testStartPageUrl() {
+        openBaseURL();
+        getWait5();
+        String actualResult_currentURl = getCurrentUrl();
+
+        Assert.assertEquals(actualResult_currentURl, BASE_URL);
+    }
 }
