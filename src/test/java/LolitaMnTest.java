2933c764c29ad6effa0921765a850b498c288dc6
LolitaMn <87144131+LolitaMn@users.noreply.github.com>
2022-11-19 20:33:01 +0400

    AT_Start_Menu_SignIn_testSignInButtonNavigateToLogInPage_LolitaMn (#766)
    * AT_Start_Menu_SignIn_testSignInButtonNavigateToLogInPage_LolitaMn
    
    * EmptyLinesWasRemoved

--- src/test/java/LolitaMnTest.java
+++ src/test/java/LolitaMnTest.java
@@ -1,10 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
@@ -18,11 +16,19 @@ public class LolitaMnTest extends BaseTest {
     final static By TEXT_PANEL =  By.xpath("//p[@class='stick-footer-panel__description']");
     final static By ALLOW_ALL_BUTTON = By.xpath("//button[@type='button' and text()='Allow all']");
     final static By MANAGE_BUTTON = By.xpath("//a[@href='/cookies-settings' and text()=' Manage cookies ']");
+    final static By SIGN_IN_BUTTON = By.xpath(
+            "//div[@id='desktop-menu']//a[@href='https://openweathermap.org/home/sign_in']");
+    final static By SIGN_IN_FORM = By.className("sign-form");
 
 
     private void openBaseURL() {
         getDriver().get(URL);
 
+    }
+
+    private String getCurrentUrl() {
+
+        return getDriver().getCurrentUrl();
     }
 
     private void waitForGrayFrameDisappeared() {
@@ -36,6 +42,11 @@ public class LolitaMnTest extends BaseTest {
     private String getText(By by, WebDriver driver) {
 
         return driver.findElement(by).getText();
+    }
+
+    private void click(By by, WebDriverWait wait) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
     }
 
 
@@ -58,6 +69,23 @@ public class LolitaMnTest extends BaseTest {
         String actualResultTextPanel = getText(TEXT_PANEL, getDriver());
 
         Assert.assertEquals(actualResultTextPanel, expectedResult);
+    }
 
+    @Test
+    public void testSignInButtonNavigateToLogInPage() {
+        String expectedResultUrl = "https://home.openweathermap.org/users/sign_in";
+
+        openBaseURL();
+        waitForGrayFrameDisappeared();
+
+        String oldCurrentUrl = getCurrentUrl();
+
+        click(SIGN_IN_BUTTON, getWait5());
+        waitForElementGetVisible(SIGN_IN_FORM,getWait10());
+
+        String actualResultUrl = getCurrentUrl();
+
+        Assert.assertNotEquals(oldCurrentUrl,actualResultUrl);
+        Assert.assertEquals(expectedResultUrl,actualResultUrl);
     }
 }
