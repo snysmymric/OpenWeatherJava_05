fce19bab2df58aa3dbae18fa7a97eaf339362703
OlegCernis <115178187+OlegCernis@users.noreply.github.com>
2022-11-20 17:55:53 +0200

    Was modificated way to get xpath and way to confirm elements (#811)
    Co-authored-by: Irina Z Java <66705803+ArinaJur@users.noreply.github.com>

--- src/test/java/OlegCernisTest.java
+++ src/test/java/OlegCernisTest.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
@@ -19,18 +18,16 @@ public class OlegCernisTest extends BaseTest {
     final static By fahrenheit = By.xpath("//div[text()= 'Imperial: °F, mph']");
     final static By metric = By.xpath("//div[text()= 'Metric: °C, m/s']");
     final static By confirmTemp = By.xpath("//div[@class = 'current-temp']/span");
-    final static String imperialTemp = "Imperial: °F, mph";
-    final static String metricTemp = "Metric: °C, m/s";
-    final static String imperialSymbol ="°F";
-    final static String metricSymbol ="°C";
 
     private void openBaseURL() {
         getDriver().get(BASE_URL);
     }
+
     private void waitForGrayFrameDisappeared() {
         getWait20().until(ExpectedConditions.invisibilityOfElementLocated(
                 By.className("owm-loader-container")));
     }
+
     private String getText(By by, WebDriver driver) {
 
         return driver.findElement(by).getText();
@@ -40,6 +37,7 @@ public class OlegCernisTest extends BaseTest {
         wait.until(ExpectedConditions.visibilityOfElementLocated(by));
         wait.until(ExpectedConditions.elementToBeClickable(by)).click();
     }
+
     private void input(String text, By by, WebDriver driver) {
         driver.findElement(by).sendKeys(text);
     }
@@ -47,13 +45,14 @@ public class OlegCernisTest extends BaseTest {
     private void waitElementToBeVisible(By by, WebDriverWait wait) {
         wait.until(ExpectedConditions.visibilityOfElementLocated(by));
     }
+
     private void waitTextToBeChanged(By by, String text, WebDriver driver, WebDriverWait wait) {
         wait.until(ExpectedConditions
                 .not(ExpectedConditions.textToBePresentInElement(driver.findElement(by), text)));
     }
+
     private boolean compareSymbol(WebDriver driver, String temp, String symbolTemp) {
         click(By.xpath(String.format("//div[text()='%s']", temp)), getWait5());
-//        waitForGrayFrameDisappeared();
 
         return getText(confirmTemp, getDriver()).contains(symbolTemp);
     }
@@ -83,34 +82,25 @@ public class OlegCernisTest extends BaseTest {
     @Test
     public void testVerifyChangingTempUnitInHeading_WhenSwitchTempUnitButton() {
 
-        String expectedResult1 = "°F";
-
         openBaseURL();
-        getDriver().manage().window().maximize();
         waitForGrayFrameDisappeared();
         click(fahrenheit, getWait5());
 
-        String fahrenheitConfirm = getText(confirmTemp, getDriver());
+        String actualResult = getText(confirmTemp, getDriver());
 
-        String actualResult = fahrenheitConfirm.substring((fahrenheitConfirm.length() - 2));
-        Assert.assertEquals(actualResult, expectedResult1);
-        compareSymbol(getDriver(), imperialTemp, imperialSymbol );
+        Assert.assertTrue(actualResult.contains("°F"));
+
     }
+
     @Test
     public void testVerifyMetricSymbolIsShownInCurrentTempWhenChangingUnitToMetric() {
 
-        String expectedResult2 = "°C";
-
         openBaseURL();
-        getDriver().manage().window().maximize();
         waitForGrayFrameDisappeared();
-
         click(metric, getWait5());
 
-        String metricConfirm = getText(confirmTemp,getDriver());
+        String actualResult2 = getText(confirmTemp,getDriver());
 
-        String actualResult2 = metricConfirm.substring(metricConfirm.length() -2);
-        Assert.assertEquals(actualResult2, expectedResult2);
-        compareSymbol(getDriver(), metricTemp, metricSymbol);
+        Assert.assertTrue(actualResult2.contains("°C"));
     }
 }
