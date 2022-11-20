1b17b55b2229b609ccd85e7be3aadd3f1b47a423
Galina Kukleva <qakuklevagalina@gmail.com>
2022-11-20 15:11:07 -0700

    [RF] Multiply RF<GALINA-KUKLEVA> (#854)

--- src/test/java/GalinaKuklevaTest.java
+++ src/test/java/GalinaKuklevaTest.java
@@ -1,194 +1,130 @@
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.support.ui.ExpectedConditions;
-//import org.openqa.selenium.support.ui.WebDriverWait;
-//import org.testng.Assert;
-//import org.testng.annotations.Ignore;
-//import org.testng.annotations.Test;
-//
-//import base.BaseTest;
-//
-//@Ignore
-//public class GalinaKuklevaTest extends BaseTest {
-//    final static String BASE_URL = "https://openweathermap.org/";
-//    final static String FAHRENHEIT_SIGN = "°F";
-//    final static By H2_CITY_COUNTRY_HEADER = By.xpath("//div[@id = 'weather-widget']//h2");
-//    final static By SEARCH_CITY_FIELD = By.xpath("//div[@id = 'weather-widget']//input[@placeholder = 'Search city']");
-//    final static By SEARCH_BUTTON = By.xpath("//div[@id = 'weather-widget']//button[@type = 'submit']");
-//    final static By SEARCH_DROPDOWN_MENU = By.className("search-dropdown-menu");
-//    final static By PARIS_FR_CHOICE_IN_DROPDOWN_MENU = By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Paris, FR ']");
-//    final static By GUIDE_BUTTON = By.xpath("//div[@id = 'desktop-menu']//li/a [@href='/guide']");
-//    final static By IMPERIAL_DATA = By.xpath("//div[@id = 'weather-widget']//div[text() = 'Imperial: °F, mph']");
-//    final static By TEMP_IN_THE_CITY = By.xpath("//div[@id = 'weather-widget']//span[@class = 'heading']");
-//
-//    private void openBaseURL() {
-//        getDriver().get(BASE_URL);
-//    }
-//
-//    private void waitForGrayFrameDisappeared() {
-//        getWait20().until(ExpectedConditions.invisibilityOfElementLocated(
-//                By.className("owm-loader-container")));
-//    }
-//
-//    private String getText(By by, WebDriver driver) {
-//
-//        return driver.findElement(by).getText();
-//    }
-//
-//    private void click(By by,  WebDriverWait wait) {
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
-//        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
-//    }
-//
-//    private void input(String text, By by, WebDriver driver) {
-//        driver.findElement(by).sendKeys(text);
-//    }
-//
-//    private void waitElementToBeVisible(By by, WebDriverWait wait) {
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
-//    }
-//
-//    private void waitTextToBeChanged(By by, String text, WebDriver driver, WebDriverWait wait) {
-//        wait.until(ExpectedConditions
-//                .not(ExpectedConditions.textToBePresentInElement(driver.findElement(by), text)));
-//    }
-//
-//    @Test
-//    public void testH2TagText_WhenSearchingCityCountry() {
-//        String cityName = "Paris";
-//        String expectedResult = "Paris, FR";
-//
-//        openBaseURL();
-//        waitForGrayFrameDisappeared();
-//
-//        String oldH2Header = getText(H2_CITY_COUNTRY_HEADER, getDriver());
-//
-//        click(SEARCH_CITY_FIELD, getWait5());
-//        input(cityName, SEARCH_CITY_FIELD, getDriver());
-//        click(SEARCH_BUTTON, getWait5());
-//        waitElementToBeVisible(SEARCH_DROPDOWN_MENU, getWait10());
-//        click(PARIS_FR_CHOICE_IN_DROPDOWN_MENU, getWait10());
-//        waitTextToBeChanged(H2_CITY_COUNTRY_HEADER, oldH2Header, getDriver(), getWait10());
-//
-//        String actualResult = getText(H2_CITY_COUNTRY_HEADER, getDriver());
-//
-//        Assert.assertEquals(actualResult, expectedResult);
-//    }
-//
-//    @Test
-//    public void testOpenWeatherMapGuidePageTitle() {
-//
-//        String expectedResult_1 = "https://openweathermap.org/guide";
-//        String expectedResult_2 = "OpenWeatherMap API guide - OpenWeatherMap";
-//
-//        openBaseURL();
-//        waitForGrayFrameDisappeared();
-//        click(GUIDE_BUTTON, getWait5());
-//
-//        String actualResult_1 = getDriver().getCurrentUrl();
-//        String actualResult_2 = getDriver().getTitle();
-//
-//        Assert.assertEquals(actualResult_1, expectedResult_1);
-//        Assert.assertEquals(actualResult_2, expectedResult_2);
-//    }
-//
-//    @Test
-//    public void testTemperatureInFahrenheits() {
-//
-//        String expectedResult = FAHRENHEIT_SIGN;
-//
-//        openBaseURL();
-//        waitForGrayFrameDisappeared();
-//        click(IMPERIAL_DATA, getWait20());
-//
-//        getText(TEMP_IN_THE_CITY, getDriver());
-//
-//        Boolean actualResult = getText(TEMP_IN_THE_CITY, getDriver()).contains(expectedResult);
-//
-//        Assert.assertTrue(actualResult, expectedResult);
-//    }
-//
-//    @Ignore
-//    @Test
-//    public void testNonCaptchaVerification() throws InterruptedException {
-//
-//        String baseUrl = "https://openweathermap.org/";
-//        String eMail = "tester@test.com";
-//        String message = "Hello, World!";
-//        String expectedResult = "reCAPTCHA verification failed, please try again.";
-//
-//        getDriver().get(baseUrl);
-//        getDriver().manage().window().maximize();
-//        Thread.sleep(10000);
-//        WebElement supportButton = getDriver().findElement(
-//                By.xpath("//div[@id='support-dropdown']")
-//        );
-//        supportButton.click();
-//        Thread.sleep(5000);
-//        WebElement subMenuAskAQuestion = getDriver().findElement(
-//                By.xpath("//div[@id = 'desktop-menu']//a[text() = 'Ask a question']")
-//        );
-//        subMenuAskAQuestion.click();
-//        Thread.sleep(10000);
-//        String originalWindow = getDriver().getWindowHandle();
-//        assert getDriver().getWindowHandles().size() != 1;
-//        Thread.sleep(10000);
-//        for (String windowHandle : getDriver().getWindowHandles()) {
-//            if (!originalWindow.contentEquals(windowHandle)) {
-//                getDriver().switchTo().window(windowHandle);
-//                break;
-//            }
-//        }
-//        WebElement emailField = getDriver().findElement(
-//                By.id("question_form_email")
-//        );
-//        emailField.sendKeys(eMail);
-//        Thread.sleep(5000);
-//        WebElement subjectField = getDriver().findElement(
-//                By.id("question_form_subject")
-//        );
-//        subjectField.click();
-//        Thread.sleep(5000);
-//        WebElement optionTech = getDriver().findElement(
-//                By.xpath("//select[@id = 'question_form_subject']/option[@value = 'Tech Issue']")
-//        );
-//        optionTech.click();
-//        Thread.sleep(5000);
-//        WebElement messageField = getDriver().findElement(
-//                By.id("question_form_message")
-//        );
-//        messageField.sendKeys(message);
-//        Thread.sleep(5000);
-//        WebElement submitField = getDriver().findElement(
-//                By.xpath("//form[@id = 'new_question_form']//input[@value = 'Submit']")
-//        );
-//        submitField.click();
-//        Thread.sleep(5000);
-//        WebElement submitVerification = getDriver().findElement(
-//                By.xpath("//div[text() = 'reCAPTCHA verification failed, please try again.']")
-//        );
-//        String actualResult = submitVerification.getText();
-//
-//        Assert.assertEquals(actualResult, expectedResult);
-//    }
-//
-//    @Ignore
-//    @Test
-//    public void testDeskTopMenuClickAPIFind30Buttons1() throws InterruptedException {
-//
-//        String baseUrl = "https://openweathermap.org/";
-//        getDriver().get(baseUrl);
-//        getDriver().manage().window().maximize();
-//        int expectedResult = 30;
-//        Thread.sleep(10000);
-//        WebElement element = getDriver().findElement(
-//                By.xpath("//div[@id = 'desktop-menu']//a[text()= 'API']"));
-//        element.click();
-//        int actualResult = getDriver().findElements(
-//                By.xpath("//a[contains(@class, 'btn_block orange round') " +
-//                        "or contains(@class, 'ow-btn round btn-orange') ]")).size();
-//
-//        Assert.assertEquals(actualResult, expectedResult);
-//    }
-//}
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+
+public class GalinaKuklevaTest extends BaseTest {
+    final static String FAHRENHEIT_SIGN = "°F";
+    final static By H2_CITY_COUNTRY_HEADER = By.xpath("//div[@id = 'weather-widget']//h2");
+    final static By SEARCH_CITY_FIELD = By.xpath("//div[@id = 'weather-widget']//input[@placeholder = 'Search city']");
+    final static By SEARCH_BUTTON = By.xpath("//div[@id = 'weather-widget']//button[@type = 'submit']");
+    final static By SEARCH_DROPDOWN_MENU = By.className("search-dropdown-menu");
+    final static By PARIS_FR_CHOICE_IN_DROPDOWN_MENU = By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Paris, FR ']");
+    final static By GUIDE_BUTTON = By.xpath("//div[@id = 'desktop-menu']//li/a [@href='/guide']");
+    final static By IMPERIAL_DATA = By.xpath("//div[@id = 'weather-widget']//div[text() = 'Imperial: °F, mph']");
+    final static By TEMP_IN_THE_CITY = By.xpath("//div[@id = 'weather-widget']//span[@class = 'heading']");
+    final static By SUPPORT_BUTTON = By.xpath("//div[@id='support-dropdown']");
+    final static By SUB_MENU_ASK_A_QUESTION = By.xpath("//div[@id = 'desktop-menu']//a[text() = 'Ask a question']");
+    final static By EMAIL_FIELD = By.id("question_form_email");
+    final static By SUBJECT_FIELD = By.id("question_form_subject");
+    final static By OPTION_TECH = By.xpath("//select[@id = 'question_form_subject']/option[@value = 'Tech Issue']");
+    final static By MESSAGE_FIELD = By.id("question_form_message");
+    final static By SUBMIT_FIELD = By.xpath("//form[@id = 'new_question_form']//input[@value = 'Submit']");
+    final static By SUBMIT_VERIFICATION = By.xpath("//div[text() = 'reCAPTCHA verification failed, please try again.']");
+    final static By ELEMENT_ORANGE = By.xpath("//div[@id = 'desktop-menu']//a[text()= 'API']");
+
+    @Test
+    public void testH2TagText_WhenSearchingCityCountry() {
+        final String cityName = "Paris";
+        final String expectedCityCountryName = "Paris, FR";
+
+        openBaseURL();
+
+        String oldH2Header = getText(H2_CITY_COUNTRY_HEADER);
+
+        click(SEARCH_CITY_FIELD);
+        input(cityName, SEARCH_CITY_FIELD);
+        click(SEARCH_BUTTON);
+        waitElementToBeVisible(SEARCH_DROPDOWN_MENU);
+        click(PARIS_FR_CHOICE_IN_DROPDOWN_MENU);
+        waitTextToBeChanged(H2_CITY_COUNTRY_HEADER, oldH2Header);
+
+        String actualResult = getText(H2_CITY_COUNTRY_HEADER);
+
+        Assert.assertEquals(actualResult, expectedCityCountryName);
+    }
+
+    @Test
+    public void testOpenWeatherMapGuidePageTitle() {
+
+        String expectedResult_1 = "https://openweathermap.org/guide";
+        String expectedResult_2 = "OpenWeatherMap API guide - OpenWeatherMap";
+
+        openBaseURL();
+
+        click(GUIDE_BUTTON);
+
+        String actualResult_1 = getDriver().getCurrentUrl();
+        String actualResult_2 = getDriver().getTitle();
+
+        Assert.assertEquals(actualResult_1, expectedResult_1);
+        Assert.assertEquals(actualResult_2, expectedResult_2);
+    }
+
+    @Test
+    public void testTemperatureInFahrenheits() {
+
+        String expectedResult = FAHRENHEIT_SIGN;
+
+        openBaseURL();
+
+        click(IMPERIAL_DATA);
+        getText(TEMP_IN_THE_CITY);
+        Boolean actualResult = getText(TEMP_IN_THE_CITY).contains(expectedResult);
+
+        Assert.assertTrue(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testNonCaptchaVerification() {
+
+        final String eMail = "tester@test.com";
+        final String message = "Hello, World!";
+        final String expectedResult = "reCAPTCHA verification failed, please try again.";
+
+        openBaseURL();
+
+        click(SUPPORT_BUTTON);
+        click(SUB_MENU_ASK_A_QUESTION);
+
+        String originalWindow = getDriver().getWindowHandle();
+        assert getDriver().getWindowHandles().size() != 1;
+        getWait10();
+        for (String windowHandle : getDriver().getWindowHandles()) {
+            if (!originalWindow.contentEquals(windowHandle)) {
+                getDriver().switchTo().window(windowHandle);
+                break;
+            }
+        }
+
+        input(eMail, EMAIL_FIELD);
+        getWait10();
+        click(SUBJECT_FIELD);
+        click(OPTION_TECH);
+        input(message, MESSAGE_FIELD);
+        getWait10();
+        click(SUBMIT_FIELD);
+
+        String actualResult = getText(SUBMIT_VERIFICATION);
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testDeskTopMenuClickAPIFind30Buttons1() {
+
+        openBaseURL();
+
+        int expectedResult = 30;
+
+        waitElementToBeVisible(ELEMENT_ORANGE);
+        click(ELEMENT_ORANGE);
+
+        int actualResult = getDriver().findElements(
+                By.xpath("//a[contains(@class, 'btn_block orange round') " +
+                        "or contains(@class, 'ow-btn round btn-orange') ]")).size();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+}
