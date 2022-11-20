078f833fdd6c64d3bd4bbed8153ce33b98487be1
Marina J <marina.jtest@gmail.com>
2022-11-20 10:04:06 -0600

    ATMainNavBarMenuBlog_testVerifyBlogPageArticleCategoriesCount_added (#831)

--- src/test/java/MariJernTest.java
+++ src/test/java/MariJernTest.java
@@ -1,22 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MariJernTest extends BaseTest {
 
     final static String BASE_URL = "https://openweathermap.org/";
     final static String BLOG_URL = "https://openweather.co.uk/blog/category/weather";
     final static By MENU_BLOG = By.xpath("//div[@id='desktop-menu']//a[@href='https://openweather.co.uk/blog/category/weather']");
+    final static By BLOG_CATEGORIES = By.xpath("//li[@class='post-filters__item']");
 
     private void openBaseURL() {
         getDriver().get(BASE_URL);
     }
 
-    private void clickMenu_Blog() { getDriver().findElement(MENU_BLOG).click(); }
+    private void clickMenu_Blog() {
+        getDriver().findElement(MENU_BLOG)
+                .click();
+    }
 
     private void waitForGrayFrameDisappeared() {
         getWait20().until(ExpectedConditions.invisibilityOfElementLocated(
@@ -33,10 +39,25 @@ public class MariJernTest extends BaseTest {
 
         openBaseURL();
         waitForGrayFrameDisappeared();
-
         clickMenu_Blog();
         swithToUKWindow();
 
-        Assert.assertEquals(getDriver().getCurrentUrl(),BLOG_URL);
+        Assert.assertEquals(getDriver().getCurrentUrl(), BLOG_URL);
+    }
+
+    @Test
+    public void testVerifyBlogPageArticleCategoriesCount() {
+        int expectedArticleCategoriesCount = 6;
+
+        openBaseURL();
+        waitForGrayFrameDisappeared();
+        clickMenu_Blog();
+        swithToUKWindow();
+
+        List<WebElement> ArticleCategories = getDriver().findElements(BLOG_CATEGORIES);
+        for (int i = 0; i < ArticleCategories.size(); i++) {
+
+            Assert.assertEquals((ArticleCategories.size()), expectedArticleCategoriesCount);
+        }
     }
 }
