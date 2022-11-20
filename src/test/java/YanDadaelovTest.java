b010f0470a28971edbf5ab6acaf247782683e7a2
Yan Dadaelov <sirota92@bk.ru>
2022-11-20 13:01:13 +0300

    testStartPageTitleText()test added (#835)

--- src/test/java/YanDadaelovTest.java
+++ src/test/java/YanDadaelovTest.java
@@ -6,6 +6,7 @@ import runner.BaseTest;
 public class YanDadaelovTest extends BaseTest {
 
     final static String BASE_URL = "https://openweathermap.org/";
+    final static String START_PAGE_TITLE = "Сurrent weather and forecast - OpenWeatherMap";
 
 
     private void openBaseURL() {
@@ -17,13 +18,27 @@ public class YanDadaelovTest extends BaseTest {
         return getDriver().getCurrentUrl();
     }
 
+    private String getCurrentTitle() {
+
+        return getDriver().getTitle();
+    }
+
 
     @Test
     public void testStartPageUrl() {
         openBaseURL();
         getWait5();
-        String actualResult_currentURl = getCurrentUrl();
+        String actualResult_currentUrl = getCurrentUrl();
 
-        Assert.assertEquals(actualResult_currentURl, BASE_URL);
+        Assert.assertEquals(actualResult_currentUrl, BASE_URL);
+    }
+
+    @Test
+    public void testStartPageTitleText() {
+        openBaseURL();
+        getWait5();
+        String actualResult_startPageTitleText = getCurrentTitle();
+
+        Assert.assertEquals(actualResult_startPageTitleText, START_PAGE_TITLE);
     }
 }
