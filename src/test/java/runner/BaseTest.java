3806d7c7f4d7da299d647a8974d20ac3257e2e95
Irina Z Java <66705803+ArinaJur@users.noreply.github.com>
2022-11-19 21:31:40 -0500

    [AT][Structure][Settings] Create Base setting to print customized logs <Lesson> (#812)
    * Customized logs added

--- src/test/java/runner/BaseTest.java
+++ src/test/java/runner/BaseTest.java
@@ -2,8 +2,12 @@ package runner;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.Reporter;
 import org.testng.annotations.*;
 
+import java.lang.reflect.Method;
 import java.time.Duration;
 
 public abstract class BaseTest {
@@ -13,13 +17,23 @@ public abstract class BaseTest {
     private WebDriverWait webDriverWait10;
     private WebDriverWait webDriverWait5;
 
+    @BeforeSuite
+    protected void beforeSuite(ITestContext context) {
+        Reporter.log(ReportUtils.getReportHeader(context), true);
+    }
+
     @BeforeMethod
-    protected void beforeMethod() {
+    protected void beforeMethod(Method method, ITestResult result) {
         driver = BaseUtils.createDriver();
+        Reporter.log(ReportUtils.END_LINE, true);
+        Reporter.log("TEST RUN", true);
+        Reporter.log(ReportUtils.getClassNameTestName(method, result), true);
     }
 
     @AfterMethod
-    protected void afterMethod() {
+    protected void afterMethod(Method method, ITestResult result) {
+        Reporter.log(ReportUtils.getTestStatistics(method, result), true);
+
         driver.quit();
         webDriverWait20 = null;
         webDriverWait10 = null;
---
+++ src/test/java/runner/TestUtils.java
@@ -0,0 +1,10 @@
+package runner;
+
+public class TestUtils {
+    private static final String BASE_URL = "https://openweathermap.org/";
+
+    public static String getBaseUrl() {
+
+        return BASE_URL;
+    }
+}
