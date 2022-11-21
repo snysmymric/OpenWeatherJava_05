ad29104a5af1e2f74e4d17db6de79f740bdac81e
Olena Rysina <olenarysina@gmail.com>
2022-11-21 15:06:37 -0500

    [RF] AllTestsRefactored <Lenochkanik80> (#900)
    * RFAllTestsRefactored_Lenochkanik80

--- src/test/java/Lenochkanik80Test.java
+++ src/test/java/Lenochkanik80Test.java
@@ -1,97 +1,59 @@
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.support.ui.ExpectedConditions;
-//import org.openqa.selenium.support.ui.WebDriverWait;
-//import org.testng.Assert;
-//import org.testng.annotations.Ignore;
-//import org.testng.annotations.Test;
-//import base.BaseTest;
-//
-//@Ignore
-//public class Lenochkanik80Test extends BaseTest {
-//
-//    final static String BASE_URL = "https://openweathermap.org/";
-//
-//    final static By H2_CITY_COUNTRY_HEADER = By.xpath("//div[@id = 'weather-widget']//h2");
-//    final static By SEARCH_CITY_FIELD = By.xpath("//div[@id = 'weather-widget']//input[@placeholder= 'Search city']");
-//    final static By SEARCH_BUTTON = By.xpath("//div[@id = 'weather-widget']//button[@type = 'submit']");
-//    final static By SEARCH_DROPDOWN_MENU = By.className("search-dropdown-menu");
-//    final static By PARIS_FR_CHOICE_IN_DROPDOWN_MENU = By.xpath("//ul[@class='search-dropdown-menu']/li/span[text() = 'Paris, FR ']");
-//    final static By SEARCH_API_MENU = By.xpath("//div[@id='desktop-menu']//a[@href='/api']");
-//    final static By API_TITLE = By.className("breadcrumb-title");
-//    private void openBaseURL() {
-//        getDriver().get(BASE_URL);
-//    }
-//
-//    private void waitForGrayFrameDisappeared() {
-//        getWait20().until(ExpectedConditions.invisibilityOfElementLocated(By.className("owm-loader-container")));
-//    }
-//
-//    private String getText(By by, WebDriver driver) {
-//        return driver.findElement(by).getText();
-//    }
-//
-//    private void click(By by, WebDriverWait wait) {
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
-//        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
-//    }
-//
-//    private void input(String text, By by, WebDriver driver) {
-//        driver.findElement(by).sendKeys(text);
-//    }
-//
-//    private void waitElementToBeVisible(By by, WebDriverWait wait) {
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
-//    }
-//
-//    private void waitTextToBeChanged(By by, String text, WebDriver driver, WebDriverWait wait) {
-//        wait.until(ExpectedConditions.not(ExpectedConditions.textToBePresentInElement(driver.findElement(by), text)));
-//    }
-//    @Test
-//    public void testH2TagText_WhenSearchingCityCountry()  {
-//        String cityName = "Paris";
-//        String expectedResult = "Paris, FR";
-//
-//        openBaseURL();
-//        waitForGrayFrameDisappeared();
-//
-//        String oldH2Header = getText(H2_CITY_COUNTRY_HEADER, getDriver());
-//
-//        click(SEARCH_CITY_FIELD, getWait5());
-//        input(cityName, SEARCH_CITY_FIELD, getDriver());
-//        click(SEARCH_BUTTON, getWait5());
-//        waitElementToBeVisible(SEARCH_DROPDOWN_MENU, getWait10());
-//        click(PARIS_FR_CHOICE_IN_DROPDOWN_MENU, getWait10());
-//        waitTextToBeChanged(H2_CITY_COUNTRY_HEADER, oldH2Header, getDriver(), getWait10());
-//
-//        String actualResult = getText(H2_CITY_COUNTRY_HEADER, getDriver());
-//
-//        Assert.assertEquals(actualResult, expectedResult);
-//    }
-//
-//    @Test
-//    public void testAPIMenuIsClickable() {
-//        String expectedResult = "https://openweathermap.org/api";
-//
-//        openBaseURL();
-//        waitForGrayFrameDisappeared();
-//
-//        click(SEARCH_API_MENU, getWait5());
-//
-//        String actualResult = getDriver().getCurrentUrl();
-//        Assert.assertEquals(actualResult, expectedResult);
-//    }
-//
-//    @Test
-//    public void testTitle_WhenOpenAPIMenuPage() {
-//        String expectedResult = "Weather API";
-//
-//        openBaseURL();
-//        waitForGrayFrameDisappeared();
-//        click(SEARCH_API_MENU, getWait5());
-//
-//        String actualResult = getText(API_TITLE, getDriver());
-//
-//        Assert.assertEquals(actualResult, expectedResult);
-//    }
-//}
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import base.BaseTest;
+
+public class Lenochkanik80Test extends BaseTest {
+    private final By H2_CITY_COUNTRY_HEADER = By.xpath("//div[@id = 'weather-widget']//h2");
+    private final By SEARCH_CITY_FIELD = By.xpath("//div[@id = 'weather-widget']//input[@placeholder= 'Search city']");
+    private final By SEARCH_BUTTON = By.xpath("//div[@id = 'weather-widget']//button[@type = 'submit']");
+    private final By SEARCH_DROPDOWN_MENU = By.className("search-dropdown-menu");
+    private final By PARIS_FR_CHOICE_IN_DROPDOWN_MENU = By.xpath("//ul[@class='search-dropdown-menu']/li/span[text() = 'Paris, FR ']");
+    private final By SEARCH_API_MENU = By.xpath("//div[@id='desktop-menu']//a[@href='/api']");
+    private final By API_TITLE = By.className("breadcrumb-title");
+
+    @Test
+    public void testH2TagText_WhenSearchingCityCountry()  {
+        final String cityName = "Paris";
+        final String expectedResult = "Paris, FR";
+
+        openBaseURL();
+
+        final String oldH2Header = getText(H2_CITY_COUNTRY_HEADER);
+
+        click(SEARCH_CITY_FIELD);
+        input(cityName, SEARCH_CITY_FIELD);
+        click(SEARCH_BUTTON);
+        waitElementToBeVisible(SEARCH_DROPDOWN_MENU);
+        click(PARIS_FR_CHOICE_IN_DROPDOWN_MENU);
+        waitTextToBeChanged(H2_CITY_COUNTRY_HEADER, oldH2Header);
+
+        String actualResult = getText(H2_CITY_COUNTRY_HEADER);
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testAPIMenu() {
+        final String expectedResult = "https://openweathermap.org/api";
+
+        openBaseURL();
+        click(SEARCH_API_MENU);
+
+        String actualResult = getCurrentURL();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testTitle_WhenOpenAPIMenuPage() {
+        final String expectedResult = "Weather API";
+
+        openBaseURL();
+        click(SEARCH_API_MENU);
+
+        String actualResult = getText(API_TITLE);
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+}
