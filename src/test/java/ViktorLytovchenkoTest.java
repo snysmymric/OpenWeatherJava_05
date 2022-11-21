8b6612f78df43e6761ec91e83806056e5d3feb2c
Viktor Lytovchenko <vktrlitovchenko@gmail.com>
2022-11-21 18:19:30 +0200

    [RF]Refactoring test<ViktorLytovchenko> (#888)
    * Refactoring test and delete old.
    
    * Change on final.....

--- src/test/java/ViktorLytovchenkoTest.java
+++ src/test/java/ViktorLytovchenkoTest.java
@@ -1,87 +1,32 @@
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.Keys;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.support.ui.ExpectedConditions;
-//import org.openqa.selenium.support.ui.WebDriverWait;
-//import org.testng.Assert;
-//import org.testng.annotations.Ignore;
-//import org.testng.annotations.Test;
-//import base.BaseTest;
-//
-//@Ignore
-//public class ViktorLytovchenkoTest extends BaseTest {
-//
-//        final static String BASE_URL = "https://openweathermap.org/";
-//
-//        final static By WEATHER_IN_YOUR_CITY_FIELD = By.xpath("//div[@id = 'desktop-menu']/form/input[1]");
-//
-//        final static By CITY_IN_VALUE = By.xpath("//div[@class = 'form-group']//input[@id = 'search_str']");
-//
-//        private void openBaseURL() {getDriver().get(BASE_URL);}
-//
-//        private void waitForGrayFrameDisappeared() {
-//            getWait20().until(ExpectedConditions.invisibilityOfElementLocated(
-//                    By.className("owm-loader-container")));}
-//
-//        private void click(By by,  WebDriverWait wait) {
-//            wait.until(ExpectedConditions.visibilityOfElementLocated(by));
-//            wait.until(ExpectedConditions.elementToBeClickable(by)).click();
-//        }
-//
-//        private void input(String text, By by, WebDriver driver) {
-//            driver.findElement(by).sendKeys(text);
-//        }
-//        private void enter(By by, WebDriver driver) {
-//            driver.findElement(by).sendKeys(Keys.ENTER);;
-//        }
-//
-//        private String getText(By by, WebDriver driver) {
-//
-//            return driver.findElement(by).getAttribute("value");
-//        }
-//
-//
-//
-//        @Test
-//        public void testTypeInSearchBarRome() {
-//            String cityName = "Rome";
-//            String expectedResult = "https://openweathermap.org/find?q=Rome";
-//            String expectedResult1 = "Rome";
-//
-//            openBaseURL();
-//            waitForGrayFrameDisappeared();
-//
-//            click(WEATHER_IN_YOUR_CITY_FIELD, getWait5());
-//            input(cityName, WEATHER_IN_YOUR_CITY_FIELD, getDriver());
-//            enter(WEATHER_IN_YOUR_CITY_FIELD, getDriver());
-//
-//            String actualResult = getDriver().getCurrentUrl();
-//            String actualResult1 = getText(CITY_IN_VALUE, getDriver());
-//
-//            Assert.assertEquals(actualResult, expectedResult);
-//            Assert.assertEquals(actualResult1, expectedResult1);
-//        }
-//
-//        @Ignore
-//        @Test
-//        public void checkSiteReload() throws InterruptedException {
-//            String url = "https://openweathermap.org/";
-//            getDriver().get(url);
-//            Thread.sleep(10000);
-//
-//            WebElement imageLogoSite = getDriver().findElement(
-//                    By.xpath("//body[@class = 'body-orange']//ul[@id = 'first-level-nav']//a/img")
-//            );
-//            imageLogoSite.click();
-//            Thread.sleep(10000);
-//
-//            String expectedResult = "https://openweathermap.org/";
-//            String actualResult = getDriver().getCurrentUrl();
-//
-//            Assert.assertEquals(actualResult, expectedResult);
-//        }
-//
-//    }
-//
-//
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import base.BaseTest;
+
+
+public class ViktorLytovchenkoTest extends BaseTest {
+
+        private final By WEATHER_IN_YOUR_CITY_FIELD = By.xpath("//div[@id = 'desktop-menu']/form/input[1]");
+        private final By CITY_IN_VALUE = By.xpath("//input[@id='search_str']");
+
+        @Test
+        public void testTypeInSearchBarRome() {
+            final String cityName = "Rome";
+            final String expectedResult = "https://openweathermap.org/find?q=Rome";
+            final String expectedResult1 = "Rome";
+
+            openBaseURL();
+
+            click(WEATHER_IN_YOUR_CITY_FIELD);
+            inputAndEnter(WEATHER_IN_YOUR_CITY_FIELD, cityName);
+
+            String actualResult = getCurrentURL();
+            String actualResult1 = getTextByAttribute(CITY_IN_VALUE, "value");
+
+            Assert.assertEquals(actualResult, expectedResult);
+            Assert.assertEquals(actualResult1, expectedResult1);
+        }
+
+    }
+
+
