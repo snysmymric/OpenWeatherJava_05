644cf0f0f35fdfee69aeaa68d690088885b70bae
BugorDmitriy <116024901+BugorDmitriy@users.noreply.github.com>
2022-11-21 13:37:45 -0500

    Multiply refactoring tests in class - BugorDmitriyTest (#898)

--- src/test/java/BugorDmitriyTest.java
+++ src/test/java/BugorDmitriyTest.java
@@ -1,154 +1,106 @@
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.support.ui.ExpectedConditions;
-//import org.openqa.selenium.support.ui.WebDriverWait;
-//import org.testng.Assert;
-//import org.testng.annotations.Ignore;
-//import org.testng.annotations.Test;
-//import base.BaseTest;
-//
-//import java.util.List;
-//
-//@Ignore
-//public class BugorDmitriyTest extends BaseTest {
-//    final static String BASE_URL = "https://openweathermap.org/";
-//    final static By H2_CITY_COUNTRY_HEADER = By.xpath("//div[@id = 'weather-widget']//h2");
-//    final static By SEARCH_CITY_FIELD = By.xpath("//div[@id='weather-widget']//input[@placeholder='Search city']");
-//    final static By SEARCH_BUTTON = By.xpath("//div[@id = 'weather-widget']//button[@type = 'submit']");
-//    final static By SEARCH_DROPDOWN_MENU = By.className("search-dropdown-menu");
-//    final static By PARIS_FR_CHOICE_DROPDOWN_MENU = By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Paris, FR ']");
-//    final static By WE_USE_COOKIES = By.xpath("//div[@class='stick-footer-panel__container']//p[text()='We use cookies which are essential "
-//            + "for the site to work. We also use non-essential cookies to help us improve our services. Any data collected is anonymised. "
-//            + "You can allow all cookies or manage them individually.']");
-//    final static By ALLOW_ALL = By.xpath("//button[@class='stick-footer-panel__link']");
-//    final static By MANAGE_COOKIES = By.xpath("//div[@class='stick-footer-panel__btn-container']//a");
-//    final static By HEADING_SUPPORT_DROPDOWN = By.xpath("//div[@id='support-dropdown']");
-//    final static By FAQ = By.xpath("//ul[@class='dropdown-menu dropdown-visible']//a [@href='/faq']");
-//    final static By HOW_TO_START = By.xpath("//ul[@class='dropdown-menu dropdown-visible']//li/a[@href='/appid']");
-//    final static By ASK_A_QUESTION = By.xpath("//ul[@class='dropdown-menu dropdown-visible']//a[text()='Ask a question']");
-//    final static By SUPPORT_DROPDOWN_MENU = By.xpath("//ul[@id='support-dropdown-menu']/*");
-//    final static By DASHBOARD_MENU = By.xpath("//div[@id='desktop-menu']/ul/li[3]/a");
-//    private void openBaseURL(){
-//        getDriver().get(BASE_URL);
-//    }
-//
-//    private void waitForGreyFrameDisappeared(){
-//        getWait20().until(ExpectedConditions.invisibilityOfElementLocated(By.className("owm-loader-container")));
-//    }
-//
-//    private String getText(By by, WebDriver driver) {
-//
-//        return driver.findElement(by).getText();
-//    }
-//
-//    private void click(By by, WebDriverWait wait) {
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
-//        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
-//    }
-//
-//    private void input(String text, By by, WebDriver driver) {
-//        driver.findElement(by).sendKeys(text);
-//    }
-//
-//    private void waitElementToBeVisible(By by, WebDriverWait wait){
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
-//    }
-//
-//    private void waitTextChanged(By by, String text, WebDriver driver, WebDriverWait wait) {
-//        wait.until(ExpectedConditions.not(ExpectedConditions.textToBePresentInElement(driver.findElement(by), text)));
-//    }
-//
-//    private int seeAllElementAndCount(String whichElement, WebDriverWait wait) {
-//        wait.until(ExpectedConditions.visibilityOfAllElements(getDriver().findElement(By.xpath(whichElement))));
-//        List<WebElement> allElements = getDriver().findElements(By.xpath(whichElement));
-//        int count = allElements.size();
-//        for (WebElement checkedElement : allElements) {
-//            wait.until(ExpectedConditions.elementToBeClickable(checkedElement));
-//        }
-//        return count;
-//    }
-//
-//        @Test
-//        public void testH2TextWhenSearchingCityCountry() {
-//
-//            String cityName = "Paris";
-//            String expectedResult = "Paris, FR";
-//
-//            openBaseURL();
-//            waitForGreyFrameDisappeared();
-//
-//            String oldH2Header = getText(H2_CITY_COUNTRY_HEADER, getDriver());
-//            click(SEARCH_CITY_FIELD, getWait5());
-//            input(cityName, SEARCH_CITY_FIELD, getDriver());
-//            click(SEARCH_BUTTON, getWait10());
-//            waitElementToBeVisible(SEARCH_DROPDOWN_MENU, getWait10());
-//            click(PARIS_FR_CHOICE_DROPDOWN_MENU, getWait5());
-//            waitTextChanged(H2_CITY_COUNTRY_HEADER, oldH2Header, getDriver(), getWait10());
-//
-//            String actualResult = getText(H2_CITY_COUNTRY_HEADER, getDriver());
-//
-//            Assert.assertEquals(actualResult, expectedResult);
-//    }
-//
-//            @Test
-//        public void testConfirmTextAboutCookies() {
-//
-//            String expectedResult1 = "We use cookies which are essential for the site to work. We also use non-essential"
-//                    + " cookies to help us improve our services. Any data collected is anonymised. You can allow all "
-//                    + "cookies or manage them individually.";
-//            String expectedResult2 = "Allow all";
-//            String expectedResult3 = "Manage cookies";
-//
-//            openBaseURL();
-//            waitForGreyFrameDisappeared();
-//
-//            String actualResult1WeUseCookies = getText(WE_USE_COOKIES, getDriver());
-//            String actualResult2AllowAll = getText(ALLOW_ALL, getDriver());
-//            String actualResult3ManageCookies = getText(MANAGE_COOKIES, getDriver());
-//
-//            Assert.assertEquals(actualResult1WeUseCookies, expectedResult1);
-//            Assert.assertEquals(actualResult2AllowAll, expectedResult2);
-//            Assert.assertEquals(actualResult3ManageCookies, expectedResult3);
-//    }
-//
-//            @Ignore
-//            @Test
-//        public void testMenuSupport() {
-//
-//            int expectedResult = 3;
-//            String expectedResult1 = "FAQ";
-//            String expectedResult2 = "How to start";
-//            String expectedResult3 = "Ask a question";
-//
-//            openBaseURL();
-//            waitForGreyFrameDisappeared();
-//
-//            click(HEADING_SUPPORT_DROPDOWN, getWait5());
-//            int actualResult = seeAllElementAndCount ("//ul[@id='support-dropdown-menu']/*", getWait5());
-//
-//            String actualResult1FAQ = getText(FAQ, getDriver());
-//            String actualResult2HowToStart = getText(HOW_TO_START, getDriver());
-//            String actualResult3AskAQuestion = getText(ASK_A_QUESTION, getDriver());
-//
-//            Assert.assertEquals(actualResult, expectedResult);
-//            Assert.assertEquals(actualResult1FAQ, expectedResult1);
-//            Assert.assertEquals(actualResult2HowToStart, expectedResult2);
-//            Assert.assertEquals(actualResult3AskAQuestion, expectedResult3);
-//    }
-//
-//            @Test
-//        public void testDashboardMenu(){
-//
-//            String expectedResult = "https://openweathermap.org/weather-dashboard";
-//
-//            openBaseURL();
-//            waitForGreyFrameDisappeared();
-//
-//            click(DASHBOARD_MENU, getWait5());
-//
-//            String actualResult = getDriver().getCurrentUrl();
-//
-//            Assert.assertEquals(actualResult, expectedResult);
-//    }
-//   }
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+import base.BaseTest;
+
+import java.util.List;
+
+
+public class BugorDmitriyTest extends BaseTest {
+    final static By H2_CITY_COUNTRY_HEADER = By.xpath("//div[@id = 'weather-widget']//h2");
+    final static By SEARCH_CITY_FIELD = By.xpath("//div[@id='weather-widget']//input[@placeholder='Search city']");
+    final static By SEARCH_BUTTON = By.xpath("//div[@id = 'weather-widget']//button[@type = 'submit']");
+    final static By SEARCH_DROPDOWN_MENU = By.className("search-dropdown-menu");
+    final static By PARIS_FR_CHOICE_DROPDOWN_MENU = By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Paris, FR ']");
+    final static By WE_USE_COOKIES = By.xpath("//div[@class='stick-footer-panel__container']//p[text()='We use cookies which are essential "
+            + "for the site to work. We also use non-essential cookies to help us improve our services. Any data collected is anonymised. "
+            + "You can allow all cookies or manage them individually.']");
+    final static By ALLOW_ALL = By.xpath("//button[@class='stick-footer-panel__link']");
+    final static By MANAGE_COOKIES = By.xpath("//div[@class='stick-footer-panel__btn-container']//a");
+    final static By HEADING_SUPPORT_DROPDOWN = By.xpath("//div[@id='support-dropdown']");
+    final static By FAQ = By.xpath("//ul[@class='dropdown-menu dropdown-visible']//a [@href='/faq']");
+    final static By HOW_TO_START = By.xpath("//ul[@class='dropdown-menu dropdown-visible']//li/a[@href='/appid']");
+    final static By ASK_A_QUESTION = By.xpath("//ul[@class='dropdown-menu dropdown-visible']//a[text()='Ask a question']");
+    final static By SUPPORT_DROPDOWN_MENU = By.xpath("//ul[@id='support-dropdown-menu']/*");
+    final static By DASHBOARD_MENU = By.xpath("//div[@id='desktop-menu']/ul/li[3]/a");
+
+           @Test
+        public void testH2TextWhenSearchingCityCountry() {
+
+            String cityName = "Paris";
+            String expectedResult = "Paris, FR";
+
+            openBaseURL();
+
+            String oldH2Header = getText(H2_CITY_COUNTRY_HEADER);
+            click(SEARCH_CITY_FIELD);
+            input(cityName, SEARCH_CITY_FIELD);
+            click(SEARCH_BUTTON);
+            waitElementToBeVisible(SEARCH_DROPDOWN_MENU);
+            click(PARIS_FR_CHOICE_DROPDOWN_MENU);
+            waitTextToBeChanged(H2_CITY_COUNTRY_HEADER, oldH2Header);
+
+            String actualResult = getText(H2_CITY_COUNTRY_HEADER);
+
+            Assert.assertEquals(actualResult, expectedResult);
+    }
+
+            @Test
+        public void testConfirmTextAboutCookies() {
+
+            String expectedResult1 = "We use cookies which are essential for the site to work. We also use non-essential"
+                    + " cookies to help us improve our services. Any data collected is anonymised. You can allow all "
+                    + "cookies or manage them individually.";
+            String expectedResult2 = "Allow all";
+            String expectedResult3 = "Manage cookies";
+
+            openBaseURL();
+
+            String actualResult1WeUseCookies = getText(WE_USE_COOKIES);
+            String actualResult2AllowAll = getText(ALLOW_ALL);
+            String actualResult3ManageCookies = getText(MANAGE_COOKIES);
+
+            Assert.assertEquals(actualResult1WeUseCookies, expectedResult1);
+            Assert.assertEquals(actualResult2AllowAll, expectedResult2);
+            Assert.assertEquals(actualResult3ManageCookies, expectedResult3);
+    }
+
+            @Ignore
+            @Test
+        public void testMenuSupport() {
+
+            int expectedResult = 3;
+            String expectedResult1 = "FAQ";
+            String expectedResult2 = "How to start";
+            String expectedResult3 = "Ask a question";
+
+            openBaseURL();
+
+            click(HEADING_SUPPORT_DROPDOWN);
+            int actualResult = seeAllElementAndCount (SUPPORT_DROPDOWN_MENU);
+
+            String actualResult1FAQ = getText(FAQ);
+            String actualResult2HowToStart = getText(HOW_TO_START);
+            String actualResult3AskAQuestion = getText(ASK_A_QUESTION);
+
+            Assert.assertEquals(actualResult, expectedResult);
+            Assert.assertEquals(actualResult1FAQ, expectedResult1);
+            Assert.assertEquals(actualResult2HowToStart, expectedResult2);
+            Assert.assertEquals(actualResult3AskAQuestion, expectedResult3);
+    }
+
+            @Test
+        public void testDashboardMenu(){
+
+            String expectedResult = "https://openweathermap.org/weather-dashboard";
+
+            openBaseURL();
+
+            click(DASHBOARD_MENU);
+
+            String actualResult = getDriver().getCurrentUrl();
+
+            Assert.assertEquals(actualResult, expectedResult);
+    }
+   }
