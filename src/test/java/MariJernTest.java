c459cd9ad624bd882992a388381039c92e017e83
Marina J <marina.jtest@gmail.com>
2022-11-21 02:04:00 -0600

    [RF]Multiple Tests Refactored in my class<MariJern> (#883)

--- src/test/java/MariJernTest.java
+++ src/test/java/MariJernTest.java
@@ -3,7 +3,6 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import base.BaseTest;
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -13,17 +12,12 @@ public class MariJernTest extends BaseTest {
     private final By MENU_BLOG = By.xpath("//div[@id='desktop-menu']//a[@href='https://openweather.co.uk/blog/category/weather']");
     private final By BLOG_CATEGORIES = By.xpath("//li[@class='post-filters__item']");
 
-    private void switchToUKWindow() {
-        ArrayList<String> UK_Window = new ArrayList<>(getDriver().getWindowHandles());
-        getDriver().switchTo().window(UK_Window.get(1));
-    }
-
     @Test
     public void testMenuBlogIsClickable() {
 
         openBaseURL();
         click(MENU_BLOG);
-        switchToUKWindow();
+        switchWindow();
 
         Assert.assertEquals(getDriver().getCurrentUrl(), BLOG_URL);
     }
@@ -34,7 +28,7 @@ public class MariJernTest extends BaseTest {
 
         openBaseURL();
         click(MENU_BLOG);
-        switchToUKWindow();
+        switchWindow();
 
         List<WebElement> ArticleCategories = getDriver().findElements(BLOG_CATEGORIES);
         for (int i = 0; i < ArticleCategories.size(); i++) {
