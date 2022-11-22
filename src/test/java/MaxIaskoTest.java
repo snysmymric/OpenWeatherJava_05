9565b43c135861dc29a100e9aae1df648237d000
Max Iasko <115347581+maksymiasko@users.noreply.github.com>
2022-11-22 20:19:16 +0000

    added test6LinksPresentEnable() (#972)

--- src/test/java/MaxIaskoTest.java
+++ src/test/java/MaxIaskoTest.java
@@ -3,6 +3,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import base.BaseTest;
 
+import java.util.List;
+
 public class MaxIaskoTest extends BaseTest {
     final static By SEARCH_API_PAGE = By.xpath("//div[@id = 'desktop-menu']//a[text()= 'API']");
     final static By SEARCH_ORANGE_ELEMENT = By.xpath("//a[contains(@class, 'btn_block orange round')" +
@@ -11,6 +13,9 @@ public class MaxIaskoTest extends BaseTest {
     final static By SEARCH_WHETHER_IN_YOUR_CITY_FIELD = By.xpath(
             "//div[@id='desktop-menu']/form/input[@type ='text']");
     final static By SEARCH_FIELD_TEXT = By.xpath("//div/input[@id='search_str']");
+    final static By SEARCH_BLOG_FOOTER_LINK = By.xpath(
+            "//div[@class='footer-section not-foldable']/div/ul//li/a[@target = '_blank']");
+    final static By SEARCH_ELEMENTS_CONTAINER = By.xpath("//ul[@id= 'blog-categories']/li");
 
     @Test
     public void testSearch30OrangeButtons() {
@@ -46,6 +51,24 @@ public class MaxIaskoTest extends BaseTest {
         jumpToNextWindow();
 
         Assert.assertEquals(getTextByAttribute(SEARCH_FIELD_TEXT, name), expectedResult0);
+    }
+
+    @Test
+    public void test6LinksPresentEnable() {
+        final List<String> expectedLinkList = List.of(
+                "ALL", "WEATHER", "AGRO", "PLATFORM", "TECHNOLOGIES", "TEAM&COMPANY");
+
+        openBaseURL();
+
+        scrollByVisibleElement(SEARCH_BLOG_FOOTER_LINK);
+        click(SEARCH_BLOG_FOOTER_LINK);
+        jumpToNextWindow();
+
+        Assert.assertEquals(getListSize(SEARCH_ELEMENTS_CONTAINER), expectedLinkList.size());
+
+        Assert.assertEquals(getListText(SEARCH_ELEMENTS_CONTAINER), expectedLinkList);
+
+        Assert.assertTrue(isElementsListActive(SEARCH_ELEMENTS_CONTAINER));
     }
 }
 
