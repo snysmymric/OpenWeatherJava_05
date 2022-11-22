c25e631c0bbf1719f681a88a5d2024982b03e2ea
Anastassia-I <115176063+Anastassia-I@users.noreply.github.com>
2022-11-22 14:11:53 -0600

    testH2TagText_WhenSearchingCityCountry()_refactored (#970)

--- src/test/java/AnastassiaITest.java
+++ src/test/java/AnastassiaITest.java
@@ -1,64 +1,33 @@
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.support.ui.ExpectedConditions;
-//import org.openqa.selenium.support.ui.WebDriverWait;
-//import org.testng.Assert;
-//import org.testng.annotations.Ignore;
-//import org.testng.annotations.Test;
-//import base.BaseTest;
-//
-//@Ignore
-//public class AnastassiaITest extends BaseTest {
-//    final static String BASE_URL = "https://openweathermap.org/";
-//
-//    final static By H_2_CITY_COUNTRY_HEADER = By.xpath("//div[@id = 'weather-widget']//h2");
-//    final static By SEARCH_CITY_FIELD = By.xpath("//div[@id = 'weather-widget']/div/div/div//div/div/input[@placeholder = 'Search city' ]");
-//    final static By SEARCH_BUTTON = By.xpath("//div[@id = 'weather-widget']//button[@type = 'submit']");
-//    final static By SEARCH_DROPDOWN_MENU = By.className("search-dropdown-menu");
-//    final static By PARIS_FR_CHOICE_IN_DROPDOWN_MENU = By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Paris, FR ']");
-//
-//    private void openBaseUrl() {
-//        getDriver().get(BASE_URL);
-//    }
-//    private void waitForGreyFrameDisappeared() {
-//        getWait20().until(ExpectedConditions.invisibilityOfElementLocated(By.className("owm-loader-container")));
-//    }
-//    private String getText(By by, WebDriver driver) {
-//        return driver.findElement(by).getText();
-//    }
-//    private void click(By by, WebDriverWait wait) {
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
-//        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
-//    }
-//    private void input(String text, By by, WebDriver driver) {
-//        driver.findElement(by).sendKeys(text);
-//    }
-//    private void waitElementToBeVisible(By by, WebDriverWait wait) {
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
-//    }
-//    private void waitTextToBeChanged(By by, String text, WebDriver driver, WebDriverWait wait) {
-//        wait.until(ExpectedConditions.
-//                not(ExpectedConditions.textToBePresentInElement(driver.findElement(by), text)));
-//    }
-//    @Test
-//    public void testH2TagText_WhenSearchingCityCountry() {
-//        String cityName = "Paris";
-//        String expectedResult = "Paris, FR";
-//
-//        openBaseUrl();
-//        waitForGreyFrameDisappeared();
-//
-//        String oldH2Header = getText(H_2_CITY_COUNTRY_HEADER, getDriver());
-//
-//        click(SEARCH_CITY_FIELD, getWait5());
-//        input(cityName, SEARCH_CITY_FIELD, getDriver());
-//        click(SEARCH_BUTTON, getWait5());
-//        waitElementToBeVisible(SEARCH_DROPDOWN_MENU, getWait10());
-//        click(PARIS_FR_CHOICE_IN_DROPDOWN_MENU, getWait10());
-//        waitTextToBeChanged(H_2_CITY_COUNTRY_HEADER, oldH2Header, getDriver(), getWait10());
-//
-//        String actualResult = getText(H_2_CITY_COUNTRY_HEADER, getDriver());
-//
-//        Assert.assertEquals(actualResult, expectedResult);
-//    }
-//}
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import base.BaseTest;
+public class AnastassiaITest extends BaseTest {
+
+    private final  By H_2_CITY_COUNTRY_HEADER = By.xpath("//div[@id = 'weather-widget']//h2");
+    private final  By SEARCH_CITY_FIELD = By.xpath("//div[@id = 'weather-widget']/div/div/div//div/div/input[@placeholder = 'Search city' ]");
+    private final  By SEARCH_BUTTON = By.xpath("//div[@id = 'weather-widget']//button[@type = 'submit']");
+    private final  By SEARCH_DROPDOWN_MENU = By.className("search-dropdown-menu");
+    private final  By PARIS_FR_CHOICE_IN_DROPDOWN_MENU = By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Paris, FR ']");
+
+    @Test
+    public void testH2TagText_WhenSearchingCityCountry() {
+        final String cityName = "Paris";
+        final String expectedCityCountryNames= "Paris, FR";
+
+        openBaseURL();
+
+        final String oldH2Header = getText(H_2_CITY_COUNTRY_HEADER);
+
+        click(SEARCH_CITY_FIELD);
+        input(cityName, SEARCH_CITY_FIELD);
+        click(SEARCH_BUTTON);
+        waitElementToBeVisible(SEARCH_DROPDOWN_MENU);
+        click(PARIS_FR_CHOICE_IN_DROPDOWN_MENU);
+        waitTextToBeChanged(H_2_CITY_COUNTRY_HEADER, oldH2Header);
+
+        String actualCityCountryNames = getText(H_2_CITY_COUNTRY_HEADER);
+
+        Assert.assertEquals(actualCityCountryNames,expectedCityCountryNames );
+    }
+}
