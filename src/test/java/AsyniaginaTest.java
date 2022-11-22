40547ed8e0fb40609066aed561af735a0898cd88
asyniagina <56331881+asyniagina@users.noreply.github.com>
2022-11-22 11:09:46 -0700

    [AT][Main][FooterWebsite][Company] testCompanyTextIsExist() <asyniagina> (#907)
    * testCompanyTextIsExist added

--- src/test/java/AsyniaginaTest.java
+++ src/test/java/AsyniaginaTest.java
@@ -11,6 +11,7 @@ public class AsyniaginaTest extends BaseTest {
     private final By SEARCH_DROPDOWN_MENU = By.className("search-dropdown-menu");
     private final By PARIS_FR_CHOICE_IN_DROPDOWN_MENU = By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Paris, FR ']");
     private final By SUBSCRIBE_FOR_FREE = By.xpath("//a[@href='https://home.openweathermap.org/users/sign_up']");
+    private final By COMPANY_TEXT = By.xpath("//div[@class='footer-section']//div[@class='section-content']/p");
 
     @Test
     public void testH2TagText_WhenSearchingCityCountry() {
@@ -49,5 +50,17 @@ public class AsyniaginaTest extends BaseTest {
 
         Assert.assertTrue(actualSubscribeForFreeLinkExists
                 .contains(expectedSubscribeForFreeLinkExists));
+    }
+
+    @Test
+    public void testCompanyTextIsExist() {
+        String expectedCompanyText = "OpenWeather is a team of IT experts and data scientists that has been practising deep weather data science. For each point on the globe, OpenWeather provides historical, current and forecasted weather data via light-speed APIs. Headquarters in London, UK.";
+
+        openBaseURL();
+        scrollByVisibleElement(COMPANY_TEXT);
+        String actualCompanyText = getText(COMPANY_TEXT);
+
+        Assert.assertTrue(isDisplayed(COMPANY_TEXT));
+        Assert.assertEquals(actualCompanyText, expectedCompanyText);
     }
 }
