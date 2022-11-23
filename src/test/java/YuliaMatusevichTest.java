2679a1879c2482da320344157c39caa40f531a47
YuliaMatusevich <115679291+YuliaMatusevich@users.noreply.github.com>
2022-11-23 12:48:38 -0600

    testPaginationCountPages_WeatherCategoryBlogMenu added (#994)

--- src/test/java/YuliaMatusevichTest.java
+++ src/test/java/YuliaMatusevichTest.java
@@ -20,20 +20,23 @@ public class YuliaMatusevichTest extends BaseTest {
             By.xpath("//div[@id = 'desktop-menu']//a[@href = 'https://openweather.co.uk/blog/category/weather']");
 
     final static By BLOG_CATEGORIES_PANEL = By.xpath("//ul[@id ='blog-categories']");
+    final static By WEATHER_BLOG_CATEGORY = By.xpath(
+            "//ul[@class = 'post-filters__items']//a[@href = '/blog/category/weather']");
 
     @Test
-    public void testAllSocialMediaIconsVisibleAndClickable (){
+    public void testAllSocialMediaIconsVisibleAndClickable() {
         int expectedResult = 6;
 
         openBaseURL();
 
-        int actualResult = findAllVisibleElements("//div[@class = 'social']/a",getWait10());
+        int actualResult = findAllVisibleElements("//div[@class = 'social']/a", getWait10());
 
-        Assert.assertEquals(actualResult,expectedResult);
+        Assert.assertEquals(actualResult, expectedResult);
     }
+
     @Test
-    public void testFacebookIcon_IfCorrespondingFacebookWebpageOpens(){
-        String expectedResult ="facebook.com";
+    public void testFacebookIcon_IfCorrespondingFacebookWebpageOpens() {
+        String expectedResult = "facebook.com";
 
         openBaseURL();
         scrollToPageBottom();
@@ -44,9 +47,10 @@ public class YuliaMatusevichTest extends BaseTest {
 
         Assert.assertTrue((actualResult.contains(expectedResult)));
     }
+
     @Test
-    public void testTwitterIcon_OnHomePage_WhenClickIcon(){
-        String expectedResult ="twitter.com";
+    public void testTwitterIcon_OnHomePage_WhenClickIcon() {
+        String expectedResult = "twitter.com";
 
         openBaseURL();
         scrollToPageBottom();
@@ -57,9 +61,10 @@ public class YuliaMatusevichTest extends BaseTest {
 
         Assert.assertTrue((actualResult.contains(expectedResult)));
     }
+
     @Test
-    public void testLinkedInIcon_OnHomePage_WhenClickIcon(){
-        String expectedResult ="linkedin.com";
+    public void testLinkedInIcon_OnHomePage_WhenClickIcon() {
+        String expectedResult = "linkedin.com";
 
         openBaseURL();
         scrollToPageBottom();
@@ -70,9 +75,10 @@ public class YuliaMatusevichTest extends BaseTest {
 
         Assert.assertTrue((actualResult.contains(expectedResult)));
     }
+
     @Test
-    public void testMediumIcon_OnHomePage_WhenClickIcon(){
-        String expectedResult ="medium.com";
+    public void testMediumIcon_OnHomePage_WhenClickIcon() {
+        String expectedResult = "medium.com";
 
         openBaseURL();
         scrollToPageBottom();
@@ -83,9 +89,10 @@ public class YuliaMatusevichTest extends BaseTest {
 
         Assert.assertTrue((actualResult.contains(expectedResult)));
     }
+
     @Test
-    public void testTelegramIcon_OnHomePage_WhenClickIcon(){
-        String expectedResult ="t.me";
+    public void testTelegramIcon_OnHomePage_WhenClickIcon() {
+        String expectedResult = "t.me";
 
         openBaseURL();
         scrollToPageBottom();
@@ -96,10 +103,11 @@ public class YuliaMatusevichTest extends BaseTest {
 
         Assert.assertTrue((actualResult.contains(expectedResult)));
     }
+
     @Test
-    public void testBlogGategoriesName_whenSwitchToBlogPage(){
+    public void testBlogGategoriesName_whenSwitchToBlogPage() {
         final List<String> expectedCategoriesName =
-                new ArrayList<> (Arrays.asList("ALL", "WEATHER", "AGRO", "PLATFORM", "TECHNOLOGIES", "TEAM&COMPANY"));
+                new ArrayList<>(Arrays.asList("ALL", "WEATHER", "AGRO", "PLATFORM", "TECHNOLOGIES", "TEAM&COMPANY"));
 
         openBaseURL();
         click20(BLOG_BUTTON);
@@ -107,13 +115,27 @@ public class YuliaMatusevichTest extends BaseTest {
         waitElementToBeVisible(BLOG_CATEGORIES_PANEL);
         List<WebElement> categories = getListOfElements(By.xpath("//a[@class = 'post-filters__link']"));
         List<String> actualCategoriesName = new ArrayList<>();
-        for(WebElement category : categories){
+        for (WebElement category : categories) {
             actualCategoriesName.add(category.getText());
         }
 
         Assert.assertEquals(actualCategoriesName, expectedCategoriesName);
     }
 
-}
+    @Test
+    public void testPaginationCountPages_WeatherCategoryBlogMenu() {
+        final int navigationButtons = 2;
+        final int expectedPagesQuantity = 10;
 
+        openBaseURL();
+        click20(BLOG_BUTTON);
+        jumpToNextWindow();
+        click20(WEATHER_BLOG_CATEGORY);
+        scrollToPageBottom();
+        int actualPagesQuantity =
+                findAllVisibleElements(" //ul[@class = 'pagination']/li/*", getWait20()) - navigationButtons;
+
+        Assert.assertEquals(actualPagesQuantity, expectedPagesQuantity);
+    }
+}
 
