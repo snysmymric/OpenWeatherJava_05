d1674645d4d9094b1f5faa2c783835de2957e5ff
elviraKh1 <115122344+elviraKh1@users.noreply.github.com>
2022-11-23 02:13:01 -0500

    [AT][Widgets-Constructor]  testWidgetsThreeColorTypeExists <ElviraKh1> (#997)

--- src/test/java/ElviraKh1Test.java
+++ src/test/java/ElviraKh1Test.java
@@ -3,12 +3,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 import java.util.List;
 
 public class ElviraKh1Test extends BaseTest {
 
     private final By SUPPORT_MENU_ITEM = By.xpath("//div[@id='support-dropdown']");
     private final By SUB_SUPPORT_MENU_ITEMS = By.xpath("//ul[@id='support-dropdown-menu']//li");
+    private final By BY_WIDGET_CONTROL_LINK_ON_MAIN_PAGE = By.xpath("//div[@id='footer-website']//a[@href='/widgets-constructor']");
+    private final By BY_LIST_OF_TYPE_WIDGETS_RADIOBTN = By.xpath("//ul[@class='widget-choose']/li//input");
+    private final By BY_LIST_OF_TYPE_WIDGETS_IMG = By.xpath("//ul[@class='widget-choose']/li//img");
 
 
     @Test
@@ -29,6 +33,24 @@ public class ElviraKh1Test extends BaseTest {
         for (WebElement actualSubMenu : subSupportMenus) {
             Assert.assertTrue(actualSubMenu.isDisplayed());
             Assert.assertEquals(actualSubMenu.getText(), expectedSupportSubMenus[i++]);
+        }
+    }
+
+    @Test
+    public void testWidgetsThreeColorTypeExists() {
+        final String[] expectedWidgetsThreeColor = {"brown", "blue", "gray"};
+
+        openBaseURL();
+        scrollToPageBottom();
+        click20(BY_WIDGET_CONTROL_LINK_ON_MAIN_PAGE);
+
+        waitElementToBeVisible(BY_LIST_OF_TYPE_WIDGETS_IMG);
+        List<WebElement> actualListWebElementsTypeWidgetsRbtn = getListOfElements(BY_LIST_OF_TYPE_WIDGETS_RADIOBTN);
+
+        int i = 0;
+        for (WebElement actualWebElementTypeWidgetsRbtn : actualListWebElementsTypeWidgetsRbtn) {
+            Assert.assertEquals(actualWebElementTypeWidgetsRbtn.getAttribute("id"),
+                                 String.format("widget-%s", expectedWidgetsThreeColor[i++]));
         }
     }
 }
