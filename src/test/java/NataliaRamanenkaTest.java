5238b7dcc30ca3f706190e5f83e18fe8813e0046
NataliaRamanenka <4yzheuskaya@gmail.com>
2022-11-23 06:30:57 +0300

    [AT][Start][Weather][TimeContaner] testMapOpened_WhenClickOnTimeContaner <NataliaRamanenka> (#977)
    * testMapOpened_WhenClickOnTimeContanerAdded
    
    * testMapOpened_WhenClickOnTimeContanerIsModified

--- src/test/java/NataliaRamanenkaTest.java
+++ src/test/java/NataliaRamanenkaTest.java
@@ -9,12 +9,13 @@ public class NataliaRamanenkaTest extends BaseTest {
     final static By SEARCH_HEAD_OF_PAGE_GUIDE = By.xpath(
             "//div[@class = 'col-sm-7']/h1");
     final static By ABOUT_US = By.xpath("//a[@href = '/about-us']");
-
     final static By SEARCH_HEAD_OF_ABOUT_US = By.xpath("//div[@class = 'about-us']//h1");
-
     final static By ASK_A_QUESTION = By.xpath("//div[@class = 'footer-section not-foldable']" +
             "//a[@href = 'https://home.openweathermap.org/questions']");
     final static By SEARCH_HEAD_OF_ASK_A_QUESTION = By.xpath("//h4[@class ='headline']");
+    final static By WIDGET_MAP = By.xpath("//div[@id = 'widget-map']");
+    final static By TIME_CONTANER = By.xpath("//a[@class = 'map-info-block']");
+    final static By MAP = By.xpath("//div[@id = 'map']");
 
     @Test
     public void testGuideLinkAndTitle()  {
@@ -22,39 +23,50 @@ public class NataliaRamanenkaTest extends BaseTest {
         final String expectedTitle = "OpenWeatherMap API guide - OpenWeatherMap";
 
         openBaseURL();
-
         click(SEARCH_GUIDE_BUTTON);
         waitElementToBeVisible(SEARCH_HEAD_OF_PAGE_GUIDE);
 
         Assert.assertEquals(getCurrentURL(), expectedUrl);
         Assert.assertEquals(getTitle(), expectedTitle);
     }
+
     @Test
     public void testAboutUsVisibleAndClickable(){
-        String expectedUrl = "https://openweathermap.org/about-us";
+        final String expectedUrl = "https://openweathermap.org/about-us";
 
         openBaseURL();
-
         scrollByVisibleElement(ABOUT_US);
         click(ABOUT_US);
-
         switchToAnotherWindow(getDriver());
         waitElementToBeVisible(SEARCH_HEAD_OF_ABOUT_US);
 
         Assert.assertEquals(getCurrentURL(),expectedUrl);
     }
+
     @Test
     public void testAskAQuestionVisibleAndClickable(){
-        String expectedUrl = "https://home.openweathermap.org/questions";
+        final String expectedUrl = "https://home.openweathermap.org/questions";
 
         openBaseURL();
-
         scrollByVisibleElement(ASK_A_QUESTION);
         click(ASK_A_QUESTION);
-
         switchToAnotherWindow(getDriver());
         waitElementToBeVisible(SEARCH_HEAD_OF_ASK_A_QUESTION);
 
         Assert.assertEquals(getCurrentURL(),expectedUrl);
+    }
+
+    @Test
+    public void testMapOpened_WhenClickOnTimeContaner(){
+        final String expectedPartialUrl = "https://openweathermap.org/weathermap?basemap=" +
+                "map&cities=true&layer=radar";
+
+        openBaseURL();
+        scrollByVisibleElement(WIDGET_MAP);
+        click(TIME_CONTANER);
+        switchToAnotherWindow(getDriver());
+        waitElementToBeVisible(MAP);
+
+        Assert.assertTrue(isContainsTextInUrl(expectedPartialUrl));
     }
 }
