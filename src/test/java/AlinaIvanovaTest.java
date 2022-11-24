dc7bf4b92f3bf6d2d990be7a446a46e492507e33
Alina Ivanova <alina.ivanova.cernis@gmail.com>
2022-11-24 20:49:06 +0200

    [AT][Guide][URL][Title]testGuidePageTitleAndURL<AlinaIvanova> (#1064)
    * test Guide Page title and url added
    
    * Update AlinaIvanovaTest.java
    
    getWait10() was added before Assert.assertEquals(actualURL, expectedURL);

--- src/test/java/AlinaIvanovaTest.java
+++ src/test/java/AlinaIvanovaTest.java
@@ -5,6 +5,7 @@ import base.BaseTest;
 
 public class AlinaIvanovaTest extends BaseTest {
     private final By MENU_OUR_INITIATIVES = By.xpath("//div[@id= 'desktop-menu']//a[@href= '/our-initiatives']");
+    private final By MENU_GUIDE = By.xpath("//div[@id= 'desktop-menu']//a[@href= '/guide']");
 
     @Test
     public void testOurInitiativesMenu() {
@@ -17,5 +18,21 @@ public class AlinaIvanovaTest extends BaseTest {
         String actualLink = getCurrentURL();
 
         Assert.assertEquals(actualLink, expectedLink);
+    }
+
+    @Test
+    public void testGuidePageTitleAndURL(){
+        final String expectedURL = "https://openweathermap.org/guide";
+        final String expectedPageTitle = "OpenWeatherMap API guide - OpenWeatherMap";
+
+        openBaseURL();
+        click(MENU_GUIDE);
+
+        String actualURL= getCurrentURL();
+        String actualPageTitle = getTitle();
+        
+        getWait10();
+        Assert.assertEquals(actualURL, expectedURL);
+        Assert.assertEquals(actualPageTitle, expectedPageTitle);
     }
 }
