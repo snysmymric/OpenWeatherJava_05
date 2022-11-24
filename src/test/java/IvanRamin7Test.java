10731d471230d72eb137c7820429028185cde182
John Doe <johndoe@example.com>
2022-11-24 16:24:50 +0400

    [RF] Homework tests instead mine deleted class_Ivan_Ramin7

--- src/test/java/IvanRamin7Test.java
+++ src/test/java/IvanRamin7Test.java
@@ -1,7 +1,5 @@
 import base.BaseTest;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,44 +7,37 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class IvanRamin7Test extends BaseTest {
-    final static By H_2_CITY_COUNTRY_HEADER = By.xpath("//div[@id = 'weather-widget']//h2");
-    final static By SEARCH_CITY_FIELD = By.xpath("//div[@id = 'weather-widget']//input[@placeholder = 'Search city']");
-    final static By SEARCH_BUTTON = By.xpath("//button[@type = 'submit']");
-    final static By SEARCH_DROPDOWN_MENU = By.className("search-dropdown-menu");
-    final static By PARIS_FR_CHOICE_FROM_DROPDOWN_MENU = By.xpath("//div[@id = 'weather-widget']//ul[@class = 'search-dropdown-menu']//span[text() = 'Paris, FR ']");
-    final static By GUIDE_DESKTOP_MENU = By.xpath("//nav[@id = 'nav-website']//div[@id = 'desktop-menu']//a[@href = '/guide']");
-    final static By CELSIUS_FAHRENHEITS_SWITCH_FAHRENHEITS =  By.xpath("//div[@class='page-container']//div[text() = 'Imperial: °F, mph']");
-    final static By CELSIUS_FAHRENHEITS_SWITCH_CELSIUS = By.xpath("//div[@class='page-container']//div[text() = 'Metric: °C, m/s']");
-    final static By TEMPERATURE_UNDER_COUNTRY_NAME = By.xpath("//div[@class='section-content']//span[@class = 'heading']");
-    final static By COOKIE_WARNING_TEXT = By.xpath("//div[@class = 'stick-footer-panel']//p[@class = 'stick-footer-panel__description']");
-    final static By COOKIE_WARNING_ALLOW_ALL_BUTTON = By.xpath("//div[@class = 'stick-footer-panel']//button");
-    final static By COOKIE_WARNING_MANAGE_COOKIES_BUTTON = By.xpath("//div[@class = 'stick-footer-panel']//a[@rel = 'noopener noreferrer']");
-    final static By SUPPORT_DROPDOWN =  By.xpath("//div[@id = 'support-dropdown']");
-    final static By SUPPORT_DROPDOWN_FAQ = By.xpath("//ul[@class = 'dropdown-menu dropdown-visible']//a[@href='/faq']");
-    final static By SUPPORT_DROPDOWN_HOW_TO_START =  By.xpath("//ul[@class = 'dropdown-menu dropdown-visible']/li/a[@href = '/appid']");
-    final static By SUPPORT_DROPDOWN_ASK_QUESTION = By.xpath("//ul[@class = 'dropdown-menu dropdown-visible']/li/a[@href = 'https://home.openweathermap.org/questions']");
-    final static By EMAIL_FIELD_FROM_ASK_QUESTION = By.xpath("//input[@id = 'question_form_email']");
-    final static By SUBJECT_FIELD_FROM_ASK_QUESTION = By.xpath("//div[@class = 'form-group select required question_form_subject']//select[@id = 'question_form_subject']/option[@value = 'Other']");
-    final static By MESSAGE_FIELD_FROM_ASK_QUESTION = By.xpath("//div[@class = 'form-group text required question_form_message']//textarea");
-    final static By SUBMIT_BUTTON_FROM_ASK_QUESTION = By.xpath("//input[@value = 'Submit']");
-    final static By ERROR_CAPTCHA = By.xpath("//div[@class = 'help-block']");
-    final static By LOGO_MAIN = By.xpath("//img[@src='/themes/openweathermap/assets/img/logo_white_cropped.png']");
-
-
-    private void waitForGreyFrameDisappeared(){
-        getWait20().until(ExpectedConditions
-                .invisibilityOfElementLocated(
-                        By.className("owm-loader-container")));
-    }
-
+    private final By H_2_CITY_COUNTRY_HEADER = By.xpath("//div[@id = 'weather-widget']//h2");
+    private final By SEARCH_CITY_FIELD = By.xpath("//div[@id = 'weather-widget']//input[@placeholder = 'Search city']");
+    private final By SEARCH_BUTTON = By.xpath("//button[@type = 'submit']");
+    private final By SEARCH_DROPDOWN_MENU = By.className("search-dropdown-menu");
+    private final By PARIS_FR_CHOICE_FROM_DROPDOWN_MENU = By.xpath("//div[@id = 'weather-widget']//ul[@class = 'search-dropdown-menu']//span[text() = 'Paris, FR ']");
+    private final By GUIDE_DESKTOP_MENU = By.xpath("//nav[@id = 'nav-website']//div[@id = 'desktop-menu']//a[@href = '/guide']");
+    private final By CELSIUS_FAHRENHEITS_SWITCH_FAHRENHEITS =  By.xpath("//div[@class='page-container']//div[text() = 'Imperial: °F, mph']");
+    private final By CELSIUS_FAHRENHEITS_SWITCH_CELSIUS = By.xpath("//div[@class='page-container']//div[text() = 'Metric: °C, m/s']");
+    private final By TEMPERATURE_UNDER_COUNTRY_NAME = By.xpath("//div[@class='section-content']//span[@class = 'heading']");
+    private final By COOKIE_WARNING_TEXT = By.xpath("//div[@class = 'stick-footer-panel']//p[@class = 'stick-footer-panel__description']");
+    private final By COOKIE_WARNING_ALLOW_ALL_BUTTON = By.xpath("//div[@class = 'stick-footer-panel']//button");
+    private final By COOKIE_WARNING_MANAGE_COOKIES_BUTTON = By.xpath("//div[@class = 'stick-footer-panel']//a[@rel = 'noopener noreferrer']");
+    private final By SUPPORT_DROPDOWN =  By.xpath("//div[@id = 'support-dropdown']");
+    private final By SUPPORT_DROPDOWN_FAQ = By.xpath("//ul[@class = 'dropdown-menu dropdown-visible']//a[@href='/faq']");
+    private final By SUPPORT_DROPDOWN_HOW_TO_START =  By.xpath("//ul[@class = 'dropdown-menu dropdown-visible']/li/a[@href = '/appid']");
+    private final By SUPPORT_DROPDOWN_ASK_QUESTION = By.xpath("//ul[@class = 'dropdown-menu dropdown-visible']/li/a[@href = 'https://home.openweathermap.org/questions']");
+    private final By EMAIL_FIELD_FROM_ASK_QUESTION = By.xpath("//input[@id = 'question_form_email']");
+    private final By SUBJECT_CHOOSE = By.xpath("//option[@value = 'Initiatives']");
+    private final By SUBJECT_FIELD_FROM_ASK_QUESTION = By.xpath("//div[@class = 'form-group select required question_form_subject']//select[@id = 'question_form_subject']/option[@value = 'Other']");
+    private final By MESSAGE_FIELD_FROM_ASK_QUESTION = By.xpath("//div[@class = 'form-group text required question_form_message']//textarea");
+    private final By SUBMIT_BUTTON_FROM_ASK_QUESTION = By.xpath("//input[@value = 'Submit']");
+    private final By ERROR_CAPTCHA = By.xpath("//div[@class = 'help-block']");
+    private final By LOGO_MAIN = By.xpath("//img[@src='/themes/openweathermap/assets/img/logo_white_cropped.png']");
 
     @Test
         public void testH2TagText_WhenSearchingCityCountry(){
-            String cityName = "Paris";
-            String expectedResult = "Paris, FR";
+            final String cityName = "Paris";
+            final String expectedResult = "Paris, FR";
 
             openBaseURL();
-            waitForGreyFrameDisappeared();
+
             String oldH2Header = getText(H_2_CITY_COUNTRY_HEADER);
             click(SEARCH_CITY_FIELD);
             input(cityName, SEARCH_CITY_FIELD);
@@ -54,19 +45,19 @@ public class IvanRamin7Test extends BaseTest {
             waitElementToBeVisible(SEARCH_DROPDOWN_MENU);
             click(PARIS_FR_CHOICE_FROM_DROPDOWN_MENU);
             waitTextToBeChanged(H_2_CITY_COUNTRY_HEADER, oldH2Header);
-            String actualResult = getText(H_2_CITY_COUNTRY_HEADER);
 
+            String actualResult = getText(H_2_CITY_COUNTRY_HEADER);
 
             Assert.assertEquals(actualResult, expectedResult);
         }
 
     @Test
     public void test_ConfirmGuidePage() {
-        String urlGuidePageExpectedResult = "https://openweathermap.org/guide";
-        String titleGuideExpectedResult = "OpenWeatherMap API guide - OpenWeatherMap";
+        final String urlGuidePageExpectedResult = "https://openweathermap.org/guide";
+        final String titleGuideExpectedResult = "OpenWeatherMap API guide - OpenWeatherMap";
 
         openBaseURL();
-        waitForGreyFrameDisappeared();
+
         click(GUIDE_DESKTOP_MENU);
         String urlGuidePageActualResult = getDriver().getCurrentUrl();
         String titleGuideActualResult = getDriver().getTitle();
@@ -77,28 +68,24 @@ public class IvanRamin7Test extends BaseTest {
 
     @Test
     public void test_ConfirmFarenheits() {
-        String fahrenheitExpectedResult = "°F";
+        final String fahrenheitExpectedResult = "°F";
 
         openBaseURL();
-        waitForGreyFrameDisappeared();
-        click(CELSIUS_FAHRENHEITS_SWITCH_FAHRENHEITS);
-        getWait10();
-        boolean visibilityOfFahrenheits = getDriver()
-                .findElement(TEMPERATURE_UNDER_COUNTRY_NAME).getText().contains(fahrenheitExpectedResult);
 
-        Assert.assertTrue(visibilityOfFahrenheits);
+        click(CELSIUS_FAHRENHEITS_SWITCH_FAHRENHEITS);
+
+        Assert.assertTrue(getText(TEMPERATURE_UNDER_COUNTRY_NAME).contains(fahrenheitExpectedResult));
     }
 
     @Test
     public void test_Cookies() {
-        String textExpectedResult = "We use cookies which are essential for the site to work." +
+        final String textExpectedResult = "We use cookies which are essential for the site to work." +
                 " We also use non-essential cookies to help us improve our services. Any data collected is anonymised." +
                 " You can allow all cookies or manage them individually.";
-        String allowAllButtonExpectedResult = "Allow all";
-        String manageCookiesButtonExpectedResult = "Manage cookies";
+        final String allowAllButtonExpectedResult = "Allow all";
+        final String manageCookiesButtonExpectedResult = "Manage cookies";
 
         openBaseURL();
-        waitForGreyFrameDisappeared();
 
         Assert.assertEquals(getDriver().findElement(COOKIE_WARNING_TEXT).getText(), textExpectedResult);
         Assert.assertEquals(getDriver().findElement(COOKIE_WARNING_ALLOW_ALL_BUTTON).getText(), allowAllButtonExpectedResult);
@@ -110,8 +97,7 @@ public class IvanRamin7Test extends BaseTest {
         ArrayList<String> expectedResult = new ArrayList<>(Arrays.asList("FAQ", "How to start", "Ask a question"));
 
         openBaseURL();
-        waitForGreyFrameDisappeared();
-        getDriver().manage().window().maximize();
+
         click(SUPPORT_DROPDOWN);
         ArrayList<String> actualResult = new ArrayList<>(Arrays.asList(
                 getDriver().findElement(SUPPORT_DROPDOWN_FAQ).getText(),
@@ -124,24 +110,19 @@ public class IvanRamin7Test extends BaseTest {
 
     @Test
     public void test_ErrorHandlingCaptcha() {
-        String userEmail = "user@gmail.com";
-        String textMessage = "Hi, i like to walk in the morning";
-        String captchaErrorTextExpectedResult = "reCAPTCHA verification failed, please try again.";
+        final String userEmail = "user@gmail.com";
+        final String textMessage = "Hi, i like to walk in the morning";
+        final String captchaErrorTextExpectedResult = "reCAPTCHA verification failed, please try again.";
 
         openBaseURL();
-        waitForGreyFrameDisappeared();
-        getDriver().manage().window().maximize();
+
         click(SUPPORT_DROPDOWN);
         click(SUPPORT_DROPDOWN_ASK_QUESTION);
-        String oldTab = getDriver().getWindowHandle();
-        ArrayList<String> newTab = new ArrayList<>(getDriver().getWindowHandles());
-        newTab.remove(oldTab);
-        getDriver().switchTo().window(newTab.get(0));
+        jumpToNextWindow();
         click(EMAIL_FIELD_FROM_ASK_QUESTION);
         input(userEmail, EMAIL_FIELD_FROM_ASK_QUESTION);
         click(SUBJECT_FIELD_FROM_ASK_QUESTION);
-        WebElement subjectChoose = getDriver().findElement(By.xpath("//option[@value = 'Initiatives']"));
-        subjectChoose.click();
+        click(SUBJECT_CHOOSE);
         click(MESSAGE_FIELD_FROM_ASK_QUESTION);
         input(textMessage, MESSAGE_FIELD_FROM_ASK_QUESTION);
         click(SUBMIT_BUTTON_FROM_ASK_QUESTION);
@@ -151,11 +132,11 @@ public class IvanRamin7Test extends BaseTest {
 
     @Test
     public void test_ConfirmUnitsAreSwitched(){
-        String expectedFahrenheits = "°F";
-        String expectedCelsius = "°C";
+        final String expectedFahrenheits = "°F";
+        final String expectedCelsius = "°C";
 
         openBaseURL();
-        waitForGreyFrameDisappeared();
+
         click(CELSIUS_FAHRENHEITS_SWITCH_FAHRENHEITS);
 
         Assert.assertTrue(getDriver().findElement(CELSIUS_FAHRENHEITS_SWITCH_FAHRENHEITS).getText().contains(expectedFahrenheits));
@@ -168,9 +149,9 @@ public class IvanRamin7Test extends BaseTest {
     @Test
     public void test_urlNotChanged() {
         openBaseURL();
-        click(LOGO_MAIN);
-        String actualResultURL = getCurrentURL();
 
-        Assert.assertEquals(actualResultURL, BASE_URL);
+        click(LOGO_MAIN);
+
+        Assert.assertEquals(getCurrentURL(), BASE_URL);
     }
 }
