2edb334df327e5664fb8a9ce72b21acc9b016ef0
Lara Bee <115852420+larisabratukhina@users.noreply.github.com>
2022-11-24 00:52:27 -0800

    [RF][Start][Weather][CurrentWeather][h2] testH2TagText_WhenSearchingCityCountry() <LaraBee> (#1045)

--- src/test/java/LaraBeeTest.java
+++ src/test/java/LaraBeeTest.java
@@ -5,12 +5,6 @@ import base.BaseTest;
 
 public class LaraBeeTest extends BaseTest {
 
-    private final By H2_CITY_COUNTRY_HEADER = By.xpath("//div[@id = 'weather-widget']//h2");
-    private final By SEARCH_CITY_FIELD = By.xpath("//div[@id = 'weather-widget']//input[@placeholder = 'Search city']");
-    private final By SEARCH_BUTTON = By.xpath("//div[@id = 'weather-widget']//button[@type = 'submit']");
-    private final By SEARCH_DROPDOWN_MENU = By.className("search-dropdown-menu");
-    private final By PARIS_FR_CHOICE_IN_DROPDOWN_MENU = By.xpath(
-            "//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Paris, FR ']");
     private final By LOGO = By.xpath(
             "//a[@href]//img[@src='/themes/openweathermap/assets/img/logo_white_cropped.png']");
     private final By DOWNLOAD_ON_THE_APP_BUTTON = By.xpath(
@@ -24,27 +18,6 @@ public class LaraBeeTest extends BaseTest {
     private final By TEXT_DOWNLOAD_OPEN_WEATHER_APP = By.xpath(".//p[text()='Download OpenWeather app']");
     private final By ALL_STORE_BUTTONS = By.xpath(
             "//div[@class='my-5']/div[@style='display: flex; flex-direction: row;']/a");
-
-    @Test
-    public void testH2TagText_WhenSearchingCityCountry() {
-        final String cityName = "Paris";
-        final String expectedCityCountryNames = "Paris, FR";
-
-        openBaseURL();
-
-        final String oldH2Header = getText(H2_CITY_COUNTRY_HEADER);
-
-        click(SEARCH_CITY_FIELD);
-        input(cityName, SEARCH_CITY_FIELD);
-        click(SEARCH_BUTTON);
-        waitElementToBeVisible(SEARCH_DROPDOWN_MENU);
-        click(PARIS_FR_CHOICE_IN_DROPDOWN_MENU);
-        waitTextToBeChanged(H2_CITY_COUNTRY_HEADER, oldH2Header);
-
-        String actualCityCountryNames = getText(H2_CITY_COUNTRY_HEADER);
-
-        Assert.assertEquals(actualCityCountryNames, expectedCityCountryNames);
-    }
 
     @Test
     public void testCheckUrlAfterReloadMainPage() {
