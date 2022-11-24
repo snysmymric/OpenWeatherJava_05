6214a404b31023867caf4b446df94ec7ff099467
IraKalinka <90063648+IraKalinka@users.noreply.github.com>
2022-11-23 16:52:48 -0800

    testSupportMenuHasLinks added (#1031)

--- src/test/java/IraKalinkaTest.java
+++ src/test/java/IraKalinkaTest.java
@@ -1,14 +1,20 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import base.BaseTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class IraKalinkaTest extends BaseTest {
-     @Test
+
+    private final By SUPPORT_LINK = By.id("support-dropdown");
+    private final By DROPDOWN_MENU  = By.xpath("//li[@class='with-dropdown']/ul[@class='dropdown-menu dropdown-visible']");
+    private final By TAG_NAME = By.tagName("a");
+
+    @Test
     public void testTitleTagAndUrL() {
         final String pageTitle = "Сurrent weather and forecast - OpenWeatherMap";
         openBaseURL();
@@ -16,4 +22,29 @@ public class IraKalinkaTest extends BaseTest {
         Assert.assertEquals(getTitle(), pageTitle);
         Assert.assertEquals(getCurrentURL(), BASE_URL);
     }
+
+    @Test
+    public void testSupportMenuHasLinks() {
+       final List<String> expectedList = List.of("FAQ", "How to start", "Ask a question");
+       openBaseURL();
+       click(SUPPORT_LINK);
+       Assert.assertEquals(getLinksList(DROPDOWN_MENU, TAG_NAME), expectedList);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
