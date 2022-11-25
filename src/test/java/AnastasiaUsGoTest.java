2df1e4668698c168dd2966e36bf8d8e5b04cfbb2
AnastasiaUsGo <115109523+AnastasiaUsGo@users.noreply.github.com>
2022-11-25 21:30:21 +0300

    [AT][Main][NavBar][Menu][SignIn] testSignInLink_IsClickable() <AnastasiaUsGo> (#1107)
    * testSignInLink_IsClickable() added

--- src/test/java/AnastasiaUsGoTest.java
+++ src/test/java/AnastasiaUsGoTest.java
@@ -1,74 +1,26 @@
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.support.ui.ExpectedConditions;
-//import org.openqa.selenium.support.ui.WebDriverWait;
-//import org.testng.Assert;
-//import org.testng.annotations.Ignore;
-//import org.testng.annotations.Test;
-//import base.BaseTest;
-//
-//@Ignore
-//public class AnastasiaUsGoTest extends BaseTest {
-//    final static String BASE_URL = "https://openweathermap.org/";
-//
-//    final static By H2_CITY_COUNTRY_HEADER = By.xpath("//div[@id='weather-widget']//h2");
-//    final static By SEARCH_CITY_FIELD = By.
-//            xpath("//div[@id='weather-widget']//input[@placeholder='Search city']");
-//    final static By SEARCH_BUTTON = By.xpath("//div[@id='weather-widget']//button[@type='submit']");
-//    final static By SEARCH_DROPDOWN_MENU= By.className("search-dropdown-menu");
-//    final static By PARIS_FR_CHOICE_IN_DROPDOWN_MENU = By.
-//            xpath("//ul[@class='search-dropdown-menu']/li/span[text()='Paris, FR ']");
-//
-//    private void openBaseURL() {
-//        getDriver().get(BASE_URL);
-//    }
-//
-//    private void waitForGrayFrameDisappeared() {
-//        getWait20().until(ExpectedConditions.invisibilityOfElementLocated(
-//                By.className("owm-loader-container")));
-//    }
-//
-//    private String getText(By by, WebDriver driver) {
-//        return driver.findElement(by).getText();
-//    }
-//
-//    private void click(By by, WebDriverWait wait) {
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
-//        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
-//    }
-//
-//    private void input(String text, By by, WebDriver driver) {
-//        driver.findElement(by).sendKeys(text);
-//    }
-//
-//    private void waitElementToBeVisible(By by, WebDriverWait wait) {
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
-//    }
-//
-//    private void waitTextToBeChanged(By by, String text, WebDriver driver, WebDriverWait wait) {
-//        wait.until(ExpectedConditions.not(ExpectedConditions.
-//                textToBePresentInElement(driver.findElement(by), text)));
-//    }
-//
-//    @Test
-//    public void testH2TagText_WhenSearchCityCountry() {
-//        String cityName = "Paris";
-//        String expectedResult = "Paris, FR";
-//
-//        openBaseURL();
-//        waitForGrayFrameDisappeared();
-//
-//        String oldH2Header = getText(H2_CITY_COUNTRY_HEADER, getDriver());
-//
-//        click(SEARCH_CITY_FIELD, getWait5());
-//        input(cityName, SEARCH_CITY_FIELD, getDriver());
-//        click(SEARCH_BUTTON, getWait10());
-//        waitElementToBeVisible(SEARCH_DROPDOWN_MENU, getWait10());
-//        click(PARIS_FR_CHOICE_IN_DROPDOWN_MENU, getWait10());
-//        waitTextToBeChanged(H2_CITY_COUNTRY_HEADER, oldH2Header, getDriver(), getWait10());
-//
-//        String actualResult = getText(H2_CITY_COUNTRY_HEADER, getDriver());
-//
-//        Assert.assertEquals(actualResult, expectedResult);
-//    }
-//}
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+import base.BaseTest;
+
+public class AnastasiaUsGoTest extends BaseTest {
+
+    private final By SIGN_IN_LINK = By.xpath(
+            "//a[@href='https://openweathermap.org/home/sign_in' and contains(text(), \"Sign in\")]"
+    );
+
+    @Test
+    public void testSignInLink_IsClickable() {
+        final String expectedSignInLink = "https://home.openweathermap.org/users/sign_in";
+
+        openBaseURL();
+        click(SIGN_IN_LINK);
+        waitURLToBeChanged(BASE_URL);
+
+        Assert.assertEquals(getCurrentURL(), expectedSignInLink);
+    }
+}
