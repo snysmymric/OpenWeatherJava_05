e0d97344fa55abfaf020c0be1b7f20d6271043a8
Maria Dymsha <103815224+dymshamaria@users.noreply.github.com>
2022-11-25 15:21:12 +0300

    [AT][Main][NavBar] testVerifyAllElementsOfNavigationBarAreVisible <MariaDymsha> #1093

--- src/test/java/Maria_Dymsha_Test.java
+++ src/test/java/Maria_Dymsha_Test.java
@@ -11,7 +11,21 @@ public class Maria_Dymsha_Test extends BaseTest {
     private final By SEARCH_BUTTON = By.xpath("//div[@id = 'weather-widget']//button[@type = 'submit']");
     private final By SEARCH_DROPDOWN_MENU = By.className("search-dropdown-menu");
     private final By PARIS_FR_CHOICE_IN_DROPDOWN_MENU = By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Paris, FR ']");
-
+    private final By NAVIGATION_BAR = By.xpath("//nav[@id='nav-website']");
+    private final By LOGO_IMAGE = By.xpath("//img[@src='/themes/openweathermap/assets/img/logo_white_cropped.png']");
+    private final By SEARCH_FIELD = By.xpath("//div//form[@role='search']//input[@placeholder='Weather in your city']");
+    private final By GUID_BUTTON = By.xpath("//div//a[@href='/guide']");
+    private final By API_BUTTON = By.xpath("//div//li//a[@href='/api']");
+    private final By DASHBOARD_BUTTON = By.xpath("//div//ul//li//a[text()='Dashboard']");
+    private final By MARKETPLACE_BUTTON = By.xpath("//div//ul//li//a[text()='Marketplace']");
+    private final By PRICING_BUTTON = By.xpath("//div[@id='desktop-menu']//ul//li//a[@href='/price']");
+    private final By MAPS_BUTTON = By.xpath("//div[@id='desktop-menu']//ul//li//a[@href='/weathermap']");
+    private final By OUR_INITIATIVES_BUTTON = By.xpath("//div[@id='desktop-menu']//ul//li//a[@href='/our-initiatives']");
+    private final By PARTNERS_BUTTON = By.xpath("//div[@id='desktop-menu']//ul//li//a[@href='/examples']");
+    private final By BLOG_BUTTON = By.xpath("//div[@id='desktop-menu']//ul//li//a[@href='https://openweather.co.uk/blog/category/weather']");
+    private final By FOR_BUSINESS_BUTTON = By.xpath("//div[@id='desktop-menu']//ul//li//a[@href='https://openweather.co.uk']");
+    private final By SING_IN_BUTTON = By.xpath("//div[@id='desktop-menu']//ul//li//a[@href='https://openweathermap.org/home/sign_in']");
+    private final By SUPPORT_BUTTON = By.xpath("//div[@id='support-dropdown']");
 
     @Test
     public void testH2TagText_WhenSearchingCityCountry() {
@@ -35,7 +49,7 @@ public class Maria_Dymsha_Test extends BaseTest {
     }
 
     @Test
-    public void testOpenWeatherStartPageOpened(){
+    public void testOpenWeatherStartPageOpened() {
         String expectedResultTitle = "Сurrent weather and forecast - OpenWeatherMap";
         String expectedResultUrl = "https://openweathermap.org/";
 
@@ -46,5 +60,27 @@ public class Maria_Dymsha_Test extends BaseTest {
 
         Assert.assertEquals(actualResultTitle, expectedResultTitle);
         Assert.assertEquals(actualResultUrl, expectedResultUrl);
+    }
+
+    @Test
+    public void testVerifyAllElementsOfNavigationBarAreVisible() {
+
+        openBaseURL();
+
+        Assert.assertTrue(isElementExists(NAVIGATION_BAR));
+        Assert.assertTrue(isElementExists(LOGO_IMAGE));
+        Assert.assertTrue(isElementExists(SEARCH_FIELD));
+        Assert.assertTrue(isElementExists(GUID_BUTTON));
+        Assert.assertTrue(isElementExists(API_BUTTON));
+        Assert.assertTrue(isElementExists(DASHBOARD_BUTTON));
+        Assert.assertTrue(isElementExists(MARKETPLACE_BUTTON));
+        Assert.assertTrue(isElementExists(PRICING_BUTTON));
+        Assert.assertTrue(isElementExists(MAPS_BUTTON));
+        Assert.assertTrue(isElementExists(OUR_INITIATIVES_BUTTON));
+        Assert.assertTrue(isElementExists(PARTNERS_BUTTON));
+        Assert.assertTrue(isElementExists(BLOG_BUTTON));
+        Assert.assertTrue(isElementExists(FOR_BUSINESS_BUTTON));
+        Assert.assertTrue(isElementExists(SING_IN_BUTTON));
+        Assert.assertTrue(isElementExists(SUPPORT_BUTTON));
     }
 }
