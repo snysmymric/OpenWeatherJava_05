5200c9913a595be8cd760aaf1ef14fb891b4e69a
Vadim Kachanovich <vad.kachanovich@gmail.com>
2022-11-26 22:48:25 +0300

    testAllElementsExistsAndVisibleGreyContainer() added (#1148)

--- src/test/java/VadimKachanovichTest.java
+++ src/test/java/VadimKachanovichTest.java
@@ -1,8 +1,10 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import base.BaseTest;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class VadimKachanovichTest extends BaseTest {
     private final By H2_CITY_COUNTRY_HEADER = By.xpath("//div[@id = 'weather-widget']//h2");
@@ -14,13 +16,15 @@ public class VadimKachanovichTest extends BaseTest {
     private final By POPUP_MENU_NAME = By.xpath("//div[@class = 'pop-up-container']/div/h3[@id = 'dialogTitle']");
     private final By IMPERIAL_UOM = By.xpath("//div[@id='weather-widget']//div[@class = 'switch-container']/div[contains(text(),'°F')]");
     private final By HEADING_UOM = By.xpath("//div[@id='weather-widget']//div[@class = 'section-content']//span[@class='heading']");
-
+    private final By GREY_CONTAINER_SEARCH_FIELD = By.xpath("//div[@id='weather-widget']//input");
+    private final By GREY_CONTAINER_SEARCH_BUTTON = By.xpath("//div[@id='weather-widget']//div[@class = 'search-block']//button");
+    private final By GREY_CONTAINER_ICON_CURRENT_LOCATION = By.xpath("//div[@id='weather-widget']//div[@class = 'controls']//*[local-name()= 'svg']");
+    private final By METRIC_UOM = By.xpath("//div[@id='weather-widget']//div[@class = 'switch-container']/div[contains(text(),'°C')]");
+    private final By GREY_CONTAINER_AREA = By.xpath("//div[@id= 'weather-widget']/div[@class='grey-container']/*");
     @Test
     public void testWhenSearchingCityCountry()  {
-
         final String cityName = "Paris";
         final String expectedResult = "Paris, FR";
-
         openBaseURL();
         String startH2Header = getText(H2_CITY_COUNTRY_HEADER);
         click(SEARCH_CITY_FIELD);
@@ -32,7 +36,6 @@ public class VadimKachanovichTest extends BaseTest {
         String actualResult = getText(H2_CITY_COUNTRY_HEADER);
         Assert.assertEquals(actualResult,expectedResult);
     }
-
     @Test
     public void testVerifyPopUpWindowShownWhenClickingDifferentWeatherButton () {
         final String expectedResult = "Different weather";
@@ -41,7 +44,6 @@ public class VadimKachanovichTest extends BaseTest {
         String actualResult = getText(POPUP_MENU_NAME);
         Assert.assertEquals(actualResult,expectedResult);
     }
-
     @Test
     public void testVerifyChangingTempUnitInHeadingWhenSwitchTempUnitButton () {
         final boolean expectedResult = true;
@@ -49,5 +51,25 @@ public class VadimKachanovichTest extends BaseTest {
         click(IMPERIAL_UOM);
         boolean actualResult = getText(HEADING_UOM).contains("°F");
         Assert.assertEquals(actualResult,expectedResult);
+    }
+    @Test
+    public void testAllElementsExistsAndVisibleGreyContainer() {
+        final int expectedDisplayed = 6;
+        int counter = 0;
+        openBaseURL();
+        getWait20().until(ExpectedConditions.visibilityOfAllElements(getDriver().findElement(GREY_CONTAINER_AREA)));
+        List<By> greyElements = new ArrayList<>();
+        greyElements.add(GREY_CONTAINER_SEARCH_FIELD);
+        greyElements.add(GREY_CONTAINER_SEARCH_BUTTON);
+        greyElements.add(GREY_CONTAINER_ICON_CURRENT_LOCATION);
+        greyElements.add(DIFFERENT_WEATHER_BUTTON);
+        greyElements.add(METRIC_UOM);
+        greyElements.add(IMPERIAL_UOM);
+        for (By webelement: greyElements) {
+            Assert.assertTrue(isElementEnabled(webelement));
+            Assert.assertTrue(isDisplayed(webelement));
+            counter++;
+        }
+        Assert.assertEquals(counter, expectedDisplayed);
     }
 }
