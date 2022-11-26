b32b39bc9c9e8b43ca1892f521cc6738649e5a42
Alina Ivanova <alina.ivanova.cernis@gmail.com>
2022-11-26 21:18:39 +0200

    testPageRoadRiskAPI added (#1146)

--- src/test/java/AlinaIvanovaTest.java
+++ src/test/java/AlinaIvanovaTest.java
@@ -6,6 +6,7 @@ import base.BaseTest;
 public class AlinaIvanovaTest extends BaseTest {
     private final By MENU_OUR_INITIATIVES = By.xpath("//div[@id= 'desktop-menu']//a[@href= '/our-initiatives']");
     private final By MENU_GUIDE = By.xpath("//div[@id= 'desktop-menu']//a[@href= '/guide']");
+    private final By ROAD_RISK_API = By.xpath("//p/a[@href='/api/road-risk']");
 
     @Test
     public void testOurInitiativesMenu() {
@@ -21,7 +22,7 @@ public class AlinaIvanovaTest extends BaseTest {
     }
 
     @Test
-    public void testGuidePageTitleAndURL(){
+    public void testGuidePageTitleAndURL() {
         final String expectedURL = "https://openweathermap.org/guide";
         final String expectedPageTitle = "OpenWeatherMap API guide - OpenWeatherMap";
 
@@ -32,10 +33,24 @@ public class AlinaIvanovaTest extends BaseTest {
         waitURLToBeChanged(BASE_URL);
         waitTitleToBeChanged(oldPageTitle);
 
-        String actualURL= getCurrentURL();
+        String actualURL = getCurrentURL();
         String actualPageTitle = getTitle();
 
         Assert.assertEquals(actualURL, expectedURL);
+        Assert.assertEquals(actualPageTitle, expectedPageTitle);
+    }
+
+    @Test
+    public void testPageRoadRiskAPI() {
+        final String expectedPageTitle = "Road Risk - OpenWeatherMap";
+
+        openBaseURL();
+        click(MENU_GUIDE);
+        waitURLToBeChanged(BASE_URL);
+        click(ROAD_RISK_API);
+
+        String actualPageTitle = getTitle();
+
         Assert.assertEquals(actualPageTitle, expectedPageTitle);
     }
 }
