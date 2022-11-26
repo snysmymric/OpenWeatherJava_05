8e831e0e02b8e63b0985d9f2286860650ca6dd49
Dasha1991 <72041132+Dasha1991@users.noreply.github.com>
2022-11-26 08:48:09 -0600

    RFMultiply changes (#1136)

--- src/test/java/Dasha1991Test.java
+++ src/test/java/Dasha1991Test.java
@@ -1,65 +1,38 @@
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.support.ui.ExpectedConditions;
-//import org.openqa.selenium.support.ui.WebDriverWait;
-//import org.testng.Assert;
-//import org.testng.annotations.Ignore;
-//import org.testng.annotations.Test;
-//import base.BaseTest;
-//
-//@Ignore
-//public class Dasha1991Test extends BaseTest {
-//
-//     final static String BASE_URL = "https://openweathermap.org/";
-//     final static  By NAME_WEBSITE_H1 = By.xpath
-//             ("//div[@class='mobile-padding']//span[@class = 'orange-text']");
-//     final static By NAME_H2_HEADER = By.xpath
-//             ("//div[@class = 'mobile-padding']//span[@class = 'white-text']");
-//
-//     private void openBaseURL() {
-//         getDriver().get(BASE_URL);
-//     }
-//    private void waitForGrayFrameDissappeared() {
-//        getWait20().until(ExpectedConditions.invisibilityOfElementLocated(By.className("owm-loader-container")));
-//    }
-//    private void click(By by, WebDriverWait wait) {
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
-//        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
-//    }
-//    private void waitUntilElementIsVisible(By by, WebDriverWait wait) {
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
-//    }
-//    private String getText(By by, WebDriver driver) {
-//        return driver.findElement(by).getText();
-//    }
-//
-//    @Ignore
-//    @Test
-//    public void testHeaderStartPage() {
-//         String expectedResultH1 = "OpenWeather";
-//         String expectedResultH2 = "Weather forecasts, nowcasts and history in a fast and elegant way";
-//
-//         openBaseURL();
-//         waitForGrayFrameDissappeared();
-//
-//         String actualResultH1 = getText(NAME_WEBSITE_H1,getDriver());
-//         String actualResultH2 = getText(NAME_H2_HEADER,getDriver());
-//
-//         Assert.assertEquals(actualResultH1,expectedResultH1);
-//         Assert.assertEquals(actualResultH2,expectedResultH2);
-//    }
-//    @Test
-//    public void testNameOfURL() {
-//         String expectedResultURL = "https://openweathermap.org/";
-//         String expectedResultTitle = "Сurrent weather and forecast - OpenWeatherMap";
-//
-//         openBaseURL();
-//         waitForGrayFrameDissappeared();
-//
-//         String actualResultURL = getDriver().getCurrentUrl();
-//         String actualResultTitle = getDriver().getTitle();
-//
-//         Assert.assertEquals(actualResultURL,expectedResultURL);
-//         Assert.assertEquals(actualResultTitle,expectedResultTitle);
-//    }
-//}
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import base.BaseTest;
+
+public class Dasha1991Test extends BaseTest {
+
+     final static  By NAME_WEBSITE_H1 = By.xpath
+             ("//div[@class='mobile-padding']//span[@class = 'orange-text']");
+     final static By NAME_H2_HEADER = By.xpath
+             ("//div[@class = 'mobile-padding']//span[@class = 'white-text']");
+
+    final static By LOGO = By.xpath("//img[@src='/themes/openweathermap/assets/img/logo_white_cropped.png']");
+    @Test
+    public void testHeaderStartPage() {
+        final String expectedResultH1 = "OpenWeather";
+        final String expectedResultH2 = "Weather forecasts, nowcasts and history in a fast and elegant way";
+
+         openBaseURL();
+
+         String actualResultH1 = getText(NAME_WEBSITE_H1);
+         String actualResultH2 = getText(NAME_H2_HEADER);
+
+         Assert.assertEquals(actualResultH1,expectedResultH1);
+         Assert.assertEquals(actualResultH2,expectedResultH2);
+    }
+    @Test
+    public void testNameOfURL() {
+        final String expectedResultURL = "https://openweathermap.org/";
+        final String expectedResultTitle = "Сurrent weather and forecast - OpenWeatherMap";
+
+         openBaseURL();
+         click(LOGO);
+
+         Assert.assertEquals(getCurrentURL(),expectedResultURL);
+         Assert.assertEquals(getTitle(),expectedResultTitle);
+    }
+}
