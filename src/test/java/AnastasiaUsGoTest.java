97b63994c1317f2bc14fc3b565491903219ee7f3
AnastasiaUsGo <115109523+AnastasiaUsGo@users.noreply.github.com>
2022-11-26 11:54:00 +0300

    [AT][Main][FooterWebsite][Subscription][Pricing] testPrisingFooter_IsClickable() <AnastasiaUsGo> (#1138)
    * testPrisingFooter_IsClickable() added

--- src/test/java/AnastasiaUsGoTest.java
+++ src/test/java/AnastasiaUsGoTest.java
@@ -1,9 +1,5 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import base.BaseTest;
 
@@ -12,6 +8,8 @@ public class AnastasiaUsGoTest extends BaseTest {
     private final By SIGN_IN_LINK = By.xpath(
             "//a[@href='https://openweathermap.org/home/sign_in' and contains(text(), \"Sign in\")]"
     );
+    private final By PRICING_FOOTER_LINK = By.xpath("//div[@class='section-content']//a[@href='/price']");
+    private final By PRICING_TITLE = By.className("breadcrumb-title");
 
     @Test
     public void testSignInLink_IsClickable() {
@@ -22,5 +20,19 @@ public class AnastasiaUsGoTest extends BaseTest {
         waitURLToBeChanged(BASE_URL);
 
         Assert.assertEquals(getCurrentURL(), expectedSignInLink);
+    }
+
+    @Test
+    public void testPrisingFooter_IsClickable(){
+        String expectedPricingFooterLink = "https://openweathermap.org/price";
+        String expectedTextTitle = "Pricing";
+
+        openBaseURL();
+        scrollToPageBottom();
+        click(PRICING_FOOTER_LINK);
+        waitURLToBeChanged(BASE_URL);
+
+        Assert.assertEquals(getCurrentURL(), expectedPricingFooterLink);
+        Assert.assertEquals(getText(PRICING_TITLE), expectedTextTitle);
     }
 }
