b3caca6448ba3b701cea362da8a752a3dff1e5a7
Margarita Evd <115936846+emvrita@users.noreply.github.com>
2022-11-26 22:34:15 -0500

    Added_testGuideSolarRadiationApiIsClickableAndLinkIsOpened (#1174)

--- src/test/java/MargaritaEvdTest.java
+++ src/test/java/MargaritaEvdTest.java
@@ -1,5 +1,23 @@
-//import base.BaseTest;
-//
-//public class MargaritaEvdTest extends BaseTest {
-//
-//}
+import base.BaseTest;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MargaritaEvdTest extends BaseTest {
+
+    private final By GUIDE_MENU_BUTTON = By.xpath("//div[@id='desktop-menu']//a[@href = '/guide']");
+    private final By SOLAR_RADIATION_API_LINK = By.xpath(
+            "//p[text()='Our weather products are accessible via fast, reliable ']/a[.='Solar Radiation API']");
+
+    @Test
+    public void testGuideSolarRadiationApiIsClickableAndLinkIsOpened(){
+        String expectedLink = "https://openweathermap.org/api/solar-radiarion";
+        openBaseURL();
+        click(GUIDE_MENU_BUTTON);
+        click(SOLAR_RADIATION_API_LINK);
+
+        String actualLink = getCurrentURL();
+
+        Assert.assertEquals(actualLink,expectedLink);
+    }
+}
