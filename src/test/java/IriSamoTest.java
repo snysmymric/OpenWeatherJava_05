b426592db2e261c63666225e1c11e448b481605b
Irina Samo <113871651+IriSamo@users.noreply.github.com>
2022-11-27 10:27:03 -0500

    Added test_Main_NavBar_Menu_Maps_MenuIsClickable (#1188)

--- src/test/java/IriSamoTest.java
+++ src/test/java/IriSamoTest.java
@@ -10,6 +10,8 @@ import java.util.List;
 public class IriSamoTest extends BaseTest {
     private final By WEATHER_WIDGET_ELEMENTS =
             By.xpath("//div[@id='weather-widget']//*");
+    private final By MENU_MAPS =
+            By.xpath("//div[@id='desktop-menu']//a[text()='Maps']");
 
     @Test
     public void test_Start_Weather_AllElementsExistsAndVisible() {
@@ -45,5 +47,19 @@ public class IriSamoTest extends BaseTest {
         }
 
         Assert.assertEquals(expectedTexts.size(), count);
+    }
+
+    @Test
+    public void test_Main_NavBar_Menu_Maps_MenuIsClickable() {
+        final String mapsUrl = "https://openweathermap.org/weathermap?basemap=map&cities=true&layer=temperature&lat=45.4060&lon=-75.8009&zoom=5";
+        final String mapsTitle = "Interactive weather maps - OpenWeatherMap";
+
+        openBaseURL();
+        click(MENU_MAPS);
+
+        Assert.assertEquals(getCurrentURL().substring(0, 84), mapsUrl.substring(0, 84));
+
+        Assert.assertEquals(getTitle(), mapsTitle);
+
     }
 }
