09d7cef6a9d538d41bdda7373199fe18b834fcbb
Oleg Cernis <115178187+OlegCernis@users.noreply.github.com>
2022-12-02 04:56:46 +0200

    [[POM][Main][FooterWebsite][ProductCollections][WeatherDashboard]TestLinkWeatherDashboard <OlegCernis> (#1247)
    * ATStructurepackagesUtils_AddedMetodScrollByPixelInBaseTest_OlegCernis

--- src/test/java/pages/WeatherDashboardPage.java
+++ src/test/java/pages/WeatherDashboardPage.java
@@ -1,11 +1,22 @@
 package pages;
 
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.reporters.jq.Main;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class WeatherDashboardPage extends FooterMenuPage {
 
     public WeatherDashboardPage(WebDriver driver) {
         super(driver);
+    }
+
+    public WeatherDashboardPage logger_Info(String str) {
+        final Logger logger = Logger.getLogger(Main.class.getName());
+        logger.log(Level.INFO, String.valueOf(str));
+
+        return this;
     }
 }
--- src/test/java/tests/WeatherDashboardTest.java
+++ src/test/java/tests/WeatherDashboardTest.java
@@ -1,6 +1,31 @@
 package tests;
 import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.WeatherDashboardPage;
 
 public class WeatherDashboardTest extends BaseTest {
 
+    @Test
+    public void testDashboardLink() {
+        final String expectedResult = "https://openweathermap.org/weather-dashboard";
+
+        WeatherDashboardPage weatherDashboardPage = openBaseURL_ReturnMainPage()
+                .scrollToWeatherDashboardFooterMenu()
+                .clickWeatherDashboardFooterMenu();
+
+        Assert.assertEquals(weatherDashboardPage.getCurrentURL(), expectedResult);
+    }
+
+    @Test
+    public void testDashboardLinkWhitRapport() {
+
+//        WeatherDashboardPage weatherDashboardPage = openBaseURL_ReturnMainPage()
+//                .logger_Info("Link before click - " + getCurrentURL())
+//                .logger_Info("Title before click - " + getTitle())
+//                .scrollToDashboardLink()
+//                .clickDashboardMenu()
+//                .logger_Info("Link after click - " + getCurrentURL())
+//                .logger_Info("Title after click - " + getTitle());
+    }
 }
