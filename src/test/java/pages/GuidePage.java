33eed8a77d34bdbdfea293f07ff46e05b0c92705
marinarokhmanova <marinarokhmanova@gmail.com>
2022-12-04 08:22:03 -0600

    testHomeGuideNavigatesToBaseURL() added (#1317)

--- src/test/java/pages/GuidePage.java
+++ src/test/java/pages/GuidePage.java
@@ -1,11 +1,21 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class GuidePage extends FooterMenuPage {
 
+    @FindBy(xpath = "//div[@class='container']//li/a[@href = '/']")
+    private WebElement homeGuideButton;
 
     public GuidePage(WebDriver driver) {
         super(driver);
+    }
+
+    public GuidePage clickHomeGuide() {
+        click(homeGuideButton);
+
+        return this;
     }
 }
--- src/test/java/tests/GuideTest.java
+++ src/test/java/tests/GuideTest.java
@@ -1,7 +1,22 @@
 package tests;
 
 import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.GuidePage;
 
 public class GuideTest extends BaseTest {
 
+    @Test
+    public void testHomeGuideNavigatesToBaseURL() {
+        final String expectedURL = "https://openweathermap.org/";
+
+        GuidePage guidePage = openBaseURL_ReturnMainPage()
+                .clickGuideMenu()
+                .clickHomeGuide();
+
+        waitForGrayContainerDisappeared();
+
+        Assert.assertEquals(guidePage.getCurrentURL(), expectedURL);
+    }
 }
