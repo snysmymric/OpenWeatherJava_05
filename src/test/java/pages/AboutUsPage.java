847a1776f91ae0459944dfcd3f0b5a40b9384353
aika-l <67886657+aika-l@users.noreply.github.com>
2022-12-12 19:38:04 -0600

    [AT][Main][FooterWebsite][NotFoldable][AboutUs] testWhereToHasActiveFourOptions() <aika-l> (#1408)
    * ATMainFooterWebsiteNotFoldableAboutUs_testWhereToHasActiveFourOptions_aika
    
    * modified assert and removed unnessassary lines
    
    * removed extra not used import
    
    * removed unused method

--- src/test/java/pages/AboutUsPage.java
+++ src/test/java/pages/AboutUsPage.java
@@ -4,18 +4,43 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class AboutUsPage extends FooterMenuPage {
 
     @FindBy(xpath = "//div[@class = 'about-us']//h1")
     private WebElement aboutUsPageHeader;
 
+    @FindBy(xpath = "//h2[text()= 'Where-to']")
+    private WebElement whereToH2tag;
+
+    @FindBy(xpath = "//div[@class='section where-to']//div[1]/a")
+    private List<WebElement> optionsUnderWhereTo;
+
     public AboutUsPage(WebDriver driver) {
         super(driver);
     }
 
-    public AboutUsPage waitForAboutUsPageHeaderBeVisible(){
+    public AboutUsPage waitForAboutUsPageHeaderBeVisible() {
         wait10ElementToBeVisible(aboutUsPageHeader);
 
         return new AboutUsPage(getDriver());
+    }
+
+    public AboutUsPage scrollToWhereTo() {
+        scrollByVisibleElement(whereToH2tag);
+
+        return this;
+    }
+
+    public List<String> getOptionsText() {
+
+        return getTexts(optionsUnderWhereTo);
+    }
+
+    public AboutUsPage waitAllOptionsAreVisibleAndClickable() {
+        allElementsVisibleAndClickable(optionsUnderWhereTo);
+
+        return this;
     }
 }
--- src/test/java/tests/AboutUsTest.java
+++ src/test/java/tests/AboutUsTest.java
@@ -1,7 +1,26 @@
 package tests;
 
 import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class AboutUsTest extends BaseTest {
 
+    @Test
+    public void testWhereToHasActiveFourOptions() {
+
+        List <String> fourOptions = Arrays.asList("Buy by Subscription", "Buy in the Marketplace",
+                "Products documentation", "News and Updates");
+
+        List<String> actualOptionsUnderWhereTo =
+                openBaseURL().scrollToFooterMenu().clickOnAboutUsFooterMenu()
+                .scrollToWhereTo()
+                .waitAllOptionsAreVisibleAndClickable()
+                .getOptionsText();
+
+        Assert.assertEquals(actualOptionsUnderWhereTo, fourOptions);
+    }
 }
