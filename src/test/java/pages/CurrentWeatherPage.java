b05066bf8f0fdc4cc9ad4dafdf9f0f37268ceafd
Olga-Khliupina <115127136+Olga-Khliupina@users.noreply.github.com>
2022-12-13 15:25:54 -0600

    testCorrectAPICallCodeIsDisplayed() was created (#1422)

--- src/test/java/pages/CurrentWeatherPage.java
+++ src/test/java/pages/CurrentWeatherPage.java
@@ -1,10 +1,26 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class CurrentWeatherPage extends FooterMenuPage {
 
+    @FindBy(xpath = "//section[@id='geo']/div[@class='api']/code")
+    private WebElement apiCallCode;
+
     public CurrentWeatherPage(WebDriver driver) {
         super(driver);
+    }
+
+    public boolean isAPICallCodeDisplayed() {
+        wait10ElementToBeVisible(apiCallCode);
+
+        return isDisplayedElement(apiCallCode);
+    }
+
+    public String getAPICallCode() {
+
+        return getText(apiCallCode);
     }
 }
--- src/test/java/tests/CurrentWeatherTest.java
+++ src/test/java/tests/CurrentWeatherTest.java
@@ -1,6 +1,23 @@
 package tests;
 
 import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.CurrentWeatherPage;
 
 public class CurrentWeatherTest extends BaseTest {
+
+    @Test
+    public void testCorrectAPICallCodeIsDisplayed() {
+        String expectedAPICallCode = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+
+        CurrentWeatherPage currentWeatherPage = openBaseURL()
+                .scrollByOrangeBackground()
+                .clickCurrentWeatherIcon();
+
+        String actualAPICallCode = currentWeatherPage.getAPICallCode();
+
+        Assert.assertTrue(currentWeatherPage.isAPICallCodeDisplayed());
+        Assert.assertEquals(actualAPICallCode, expectedAPICallCode);
+    }
 }
